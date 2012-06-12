42f6f696f568da38aa26230c4643143833601a80
Cheng Chen <chengchendoc@gmail.com>
2012-06-12 11:13:04 +0800

    [snipmatch] add SnipMatch view class
    Change-Id: Ia9f08542f2ebea80e7727abb71aebe4f1a56dd49
    Signed-off-by: Cheng Chen <chengchendoc@gmail.com>

---
+++ plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/view/ViewConstants.java
@@ -0,0 +1,8 @@
+package org.eclipse.recommenders.snipmatch.view;
+
+public class ViewConstants {
+    public static final String dialogTitle = "CodeRecommenders - SnipMatch";
+    
+    public static final String deleteConfirm = "Do you confirm to delete this code snippet file?";
+    public static final String deleteSuccess = "Delete success, please refresh your index file.";
+}
