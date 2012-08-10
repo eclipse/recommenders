929825d3a6b0737adbce434d4b0777ed74137cc7
Marcel Bruch <bruch@st.informatik.tu-darmstadt.de>
2012-08-10 22:17:21 +0200

    [completion] draft of an extensible completion engine.
    This engine allows 'processors' to modify the list of JDT's
    proposal, to manipulate existing proposals, and to track which proposal
    have been proposed/applied etc.

---
+++ plugins/org.eclipse.recommenders.completion.rcp/src/org/eclipse/recommenders/internal/completion/rcp/proposals/ProposalProcessorManager.java
@@ -0,0 +1,62 @@
+package org.eclipse.recommenders.internal.completion.rcp.proposals;
+
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.jface.viewers.StyledString.Styler;
+import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.graphics.TextStyle;
+
+import com.google.common.collect.Sets;
+
+public class ProposalProcessorManager {
+
+    private Set<ProposalProcessor> processors = Sets.newHashSet();
+    private IProcessableProposal oProposal;
+    private StyledString oStyledString;
+    private int oRelevance;
+
+    public ProposalProcessorManager(IProcessableProposal oProposal) {
+        this.oProposal = oProposal;
+        this.oRelevance = oProposal.getRelevance();
+        this.oStyledString =  deepCopy(oProposal.getStyledDisplayString());
+    }
+
+    public void addProcessor(ProposalProcessor processor) {
+        processors.add(processor);
+    }
+
+    public boolean prefixChanged(String prefix) {
+        boolean discardProposal = false;
+        StyledString tmpStyledString = deepCopy(oStyledString);
+        AtomicInteger tmpRelevance = new AtomicInteger(oRelevance);
+
+        for (ProposalProcessor p : processors) {
+            discardProposal |= p.isPrefix(prefix);
+            p.modifyDisplayString(tmpStyledString);
+            p.modifyRelevance(tmpRelevance);
+        }
+        oProposal.setRelevance(tmpRelevance.get());
+        oProposal.setStyledDisplayString(tmpStyledString);
+        return discardProposal;
+    }
+
+    public static StyledString deepCopy(final StyledString displayString) {
+        final StyledString copy = new StyledString(displayString.getString());
+        for (final StyleRange range : displayString.getStyleRanges()) {
+            copy.setStyle(range.start, range.length, new Styler() {
+
+                @Override
+                public void applyStyles(final TextStyle textStyle) {
+                    textStyle.background = range.background;
+                    textStyle.borderColor = range.borderColor;
+                    textStyle.borderStyle = range.borderStyle;
+                    textStyle.font = range.font;
+                    textStyle.foreground = range.foreground;
+                }
+            });
+        }
+        return copy;
+    }
+}
---
+++ plugins/org.eclipse.recommenders.completion.rcp/src/org/eclipse/recommenders/internal/completion/rcp/proposals/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Classes to look at:
+ * 
+ *  Session and proposal processor related classes:
+ * <ul>
+ * <li> {@link SessionProcessor}
+ * <li> {@link ProposalProcessor}
+ * <li> {@link ProposalProcessorManager}
+ * </ul>
+ * Processable proposals related classes
+ * <ul>
+ * <li> {@link IProcessableProposal}
+ * <li> {@link IProcessableProposalFactory} and its default implementation {@link ProcessableProposalFactory}
+ * </ul>
+ */
+package org.eclipse.recommenders.internal.completion.rcp.proposals;
