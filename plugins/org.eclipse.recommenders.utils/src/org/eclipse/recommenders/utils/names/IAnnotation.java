b19710ddfed2540496e411fd3fa060234a2d9053
salsolatragus <info@sven-amann.de>
2012-11-23 14:09:40 +0100

    [core] basic annotation representation in name utils
    Change-Id: I04e7cd4de45847f393f9e7cf6ce7e34198e26f06

---
+++ plugins/org.eclipse.recommenders.utils/src/org/eclipse/recommenders/utils/names/IAnnotation.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2011 Sven Amann.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.recommenders.utils.names;
+
+public interface IAnnotation {
+
+    public ITypeName getAnnotationType();
+
+    // TODO information about the runtime entity's field values should be added here
+}
