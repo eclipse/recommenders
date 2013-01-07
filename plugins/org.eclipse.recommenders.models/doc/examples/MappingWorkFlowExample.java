59b7ca6e4aede0851f58ad3695141208b4ccff0f
Marcel Bruch <marcel.bruch@codetrails.com>
2013-01-07 17:40:33 +0100

    [models, draft] new, UI independent models API.
    Any comments and any API proposals are welcome - best with an example
    API usage snippet.
    
    Change-Id: I97428593b5028818d03e6b35b7dad048f33ff2ef

---
+++ plugins/org.eclipse.recommenders.models/doc/examples/MappingWorkFlowExample.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2010, 2013 Darmstadt University of Technology.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Olav Lenz - initial API and implementation
+ */
+package examples;
+
+import org.eclipse.recommenders.models.ProjectCoordinate;
+import org.eclipse.recommenders.models.mapping.IDependencyInfo;
+import org.eclipse.recommenders.models.mapping.IMappingProvider;
+import org.eclipse.recommenders.models.mapping.IMappingStrategy;
+import org.eclipse.recommenders.models.mapping.impl.MavenPomPropertiesStrategy;
+
+import com.google.common.base.Optional;
+
+public class MappingWorkFlowExample {
+
+    public static void useOfMapping(IMappingProvider mapping) {
+        IDependencyInfo ed = null;
+
+        mapping.addStrategy(new MavenPomPropertiesStrategy());
+
+        IMappingStrategy mappingStrategy = mapping;
+
+        Optional<ProjectCoordinate> optionalProjectCoordinate = mappingStrategy.extractProjectCoordinate(ed);
+
+        ProjectCoordinate projectCoordinate = null;
+        if (optionalProjectCoordinate.isPresent()) {
+            projectCoordinate = optionalProjectCoordinate.get();
+        }
+    }
+
+}
