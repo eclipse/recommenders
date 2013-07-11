6082a46fa5aaa569632c95184a3f025bc0abbdb6
Olav Lenz <olav.lenz@gmail.com>
2013-07-11 22:28:50 +0200

    [models] Import projects containing the new Models API
    Import the following projects: o.e.r.models, o.e.r.models.rcp,
    o.e.r.models.rcp.dependencymonitor, o.e.r.tests.models and
    o.e.r.tests.models.rcp.
    These projects contain the first step of the new Models API.
    
    Change-Id: I4c74bcd603ef1ff3585d04b1c95ee557983ca4e7
    Signed-off-by: Olav Lenz <olav.lenz@gmail.com>

---
+++ plugins/org.eclipse.recommenders.models/examples/org/eclipse/recommenders/examples/models/UsingModelArchiveCache.java
@@ -0,0 +1,74 @@
+package org.eclipse.recommenders.examples.models;
+
+import java.io.IOException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.recommenders.models.ModelArchiveCoordinate;
+import org.eclipse.recommenders.models.ModelRepository;
+import org.eclipse.recommenders.models.ModelRepository.ModelRepositoryEvents;
+import org.eclipse.recommenders.models.ProjectCoordinate;
+import org.eclipse.recommenders.utils.Pair;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+import com.google.common.eventbus.Subscribe;
+
+@SuppressWarnings("unused")
+public class UsingModelArchiveCache {
+
+    void downloadModelArchive(final ModelArchiveCoordinate model, final ModelRepository repository) throws IOException {
+        repository.resolve(model, newMonitor());
+    }
+
+    void findLocalModelArchive(final ModelArchiveCoordinate model, final ModelRepository repository) throws IOException {
+        if (!repository.getLocation(model).isPresent()) {
+            repository.resolve(model, newMonitor());
+        }
+    }
+
+    void deleteCachedModelArchive(final ModelArchiveCoordinate model, final ModelRepository repository)
+            throws IOException {
+        repository.delete(model, newMonitor());
+    }
+
+    void deleteIndex(final ModelRepository repository) throws IOException {
+        repository.delete(ModelRepository.INDEX, newMonitor());
+    }
+
+    void findAllModelArtifacts(final ProjectCoordinate[] gavs, final ModelRepository cache,
+            final IModelArchiveCoordinateProvider[] modelProviders) {
+
+        Table<ProjectCoordinate, String, Pair<ModelArchiveCoordinate, Boolean>> mappings = HashBasedTable.create();
+        for (ProjectCoordinate projectCoord : gavs) {
+            for (IModelArchiveCoordinateProvider modelProvider : modelProviders) {
+                ModelArchiveCoordinate modelCoord = modelProvider.find(projectCoord).orNull();
+                if (modelCoord != null) {
+                    boolean cached = cache.isCached(modelCoord);
+                    mappings.put(projectCoord, modelProvider.getType(), Pair.newPair(modelCoord, cached));
+                }
+            }
+        }
+        // update ui...
+    }
+
+    @Subscribe
+    void onEvent(final ModelRepositoryEvents.ModelArchiveCacheOpenedEvent e) {
+        // TODO check if a new index is available and download it
+
+    }
+
+    @Subscribe
+    void onEvent(final ModelRepositoryEvents.ModelArchiveCacheClosedEvent e) {
+        // TODO persists what needs to be persisted
+    }
+
+    @Subscribe
+    void onEvent(final ModelRepositoryEvents.ModelArchiveInstalledEvent e) {
+        // TODO delete old cached keys, and reload the models currently required
+    }
+
+    private IProgressMonitor newMonitor() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
