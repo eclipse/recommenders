13ca87fb20b4d6d0d40467bfb2e299c8801538c2
Timur Achmetow <achmetow84@googlemail.com>
2013-07-13 13:31:30 +0200

    bug 401851: [gsoc] Usage Data Collection for Code Completion
    New Tab integrated: Triggered Commands
    1. Shows the triggered Eclipse commands (all)
    2. Triggered commands will be persisted in the workspace
    3. The data will be presented in a TableViewer
    4. Sorter for each viewer column
    5. Included StyledCellLabelProvider
    
    Change-Id: Ia771d3d217618b2671d70b2ba744ada3d6d64919
    Signed-off-by: Timur Achmetow <achmetow84@googlemail.com>

---
+++ plugins/org.eclipse.recommenders.completion.rcp.sandbox/src/org/eclipse/recommenders/internal/completion/rcp/sandbox/TriggeredCommandCollector.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2013 Timur Achmetow
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Timur Achmetow - Initial API and implementation
+ */
+package org.eclipse.recommenders.internal.completion.rcp.sandbox;
+
+import java.io.File;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.eclipse.core.commands.Category;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IExecutionListener;
+import org.eclipse.core.commands.NotHandledException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import com.google.gson.Gson;
+
+public class TriggeredCommandCollector implements IStartup {
+
+    private final File file = getCommandLogLocation();
+
+    private final IExecutionListener listener = new IExecutionListener() {
+
+        @Override
+        public void preExecute(String commandId, ExecutionEvent event) {
+            try {
+                String name = event.getCommand().getName();
+                String description = event.getCommand().getDescription();
+                Category category = event.getCommand().getCategory();
+
+                TriggeredCommand triggeredCommand = new TriggeredCommand(commandId, name, description,
+                        System.currentTimeMillis(), category.getName(), category.getId());
+                Gson gson = new Gson();
+                Files.append(gson.toJson(triggeredCommand) + SystemUtils.LINE_SEPARATOR, file, Charsets.UTF_8);
+                System.out.println(triggeredCommand);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void postExecuteSuccess(String commandId, Object returnValue) {
+        }
+
+        @Override
+        public void postExecuteFailure(String commandId, ExecutionException exception) {
+        }
+
+        @Override
+        public void notHandled(String commandId, NotHandledException exception) {
+        }
+    };
+
+    @Override
+    public void earlyStartup() {
+        ICommandService commandService = (ICommandService) PlatformUI.getWorkbench().getAdapter(ICommandService.class);
+        if (commandService != null) {
+            commandService.addExecutionListener(listener);
+        }
+    }
+
+    public static File getCommandLogLocation() {
+        Bundle bundle = FrameworkUtil.getBundle(StatisticsSessionProcessor.class);
+        IPath location = Platform.getStateLocation(bundle);
+        return new File(location.toFile(), "triggered-commands.txt");
+    }
+}
