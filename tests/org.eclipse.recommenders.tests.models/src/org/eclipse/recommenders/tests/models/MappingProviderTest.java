5876e178b148b7f1f1c4012f0dcf42adda305eaf
Olav Lenz <olav.lenz@gmail.com>
2013-07-24 12:00:08 +0200

    [models] Move caching from MappingProvider to ProjectCoordinateProvider.
    Change-Id: Icba4e7c2d2d479d40898bd5df626633585cc1979
    Signed-off-by: Olav Lenz <olav.lenz@gmail.com>

--- tests/org.eclipse.recommenders.tests.models/src/org/eclipse/recommenders/tests/models/MappingProviderTest.java
+++ tests/org.eclipse.recommenders.tests.models/src/org/eclipse/recommenders/tests/models/MappingProviderTest.java
@@ -11,7 +11,9 @@
 package org.eclipse.recommenders.tests.models;
 
 import static com.google.common.base.Optional.fromNullable;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.List;
@@ -132,27 +134,6 @@ public class ProjectCoordinateProviderTest {
                 new File("example.jar"), DependencyType.JAR));
 
         assertEquals(EXPECTED_PROJECT_COORDINATE, optionalProjectCoordinate.get());
-    }
-
-    @Test
-    public void testMappingCacheMissAtFirstTime() {
-        MappingProvider sut = new MappingProvider();
-        sut.addStrategy(createMockedStrategy(EXPECTED_PROJECT_COORDINATE));
-        DependencyInfo dependencyInfo = new DependencyInfo(new File("example.jar"), DependencyType.JAR);
-        sut.searchForProjectCoordinate(dependencyInfo);
-        sut.getHitCount();
-        assertEquals(1, sut.getMissCount());
-    }
-
-    @Test
-    public void testMappingCacheHitAtSecondTime() {
-        MappingProvider sut = new MappingProvider();
-        sut.addStrategy(createMockedStrategy(EXPECTED_PROJECT_COORDINATE));
-        DependencyInfo dependencyInfo = new DependencyInfo(new File("example.jar"), DependencyType.JAR);
-        sut.searchForProjectCoordinate(dependencyInfo);
-        sut.searchForProjectCoordinate(dependencyInfo);
-
-        assertEquals(1, sut.getHitCount());
     }
 
     @Test
