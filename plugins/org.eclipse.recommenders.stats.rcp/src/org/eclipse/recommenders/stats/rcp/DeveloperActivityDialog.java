e191ba6fd1b93a8db7bb7ec358c496a263622067
Timur Achmetow <achmetow84@googlemail.com>
2013-08-04 22:24:06 +0200

    [gsoc] Usage Data Collection for Code Completion
    New Tab integrated: Triggered Commands
    1. Shows the triggered Eclipse commands (all)
    2. Triggered commands will be persisted in the workspace
    3. The data will be presented in a TableViewer
    4. Sorter for each viewer column
    5. Included StyledCellLabelProvider (Commands tab)
    6. StyledText through SWT Labels replaced
    7. Category column removed, Tree through Table replaced
    8. Persistence logic optimized
    9. Encoding problem fixed (NaN)
    10. TreeViewer integrated (for the new navigation)
    11. Configurable columns in each table
    12. Empty page for categories added
    
    Change-Id: Ia771d3d217618b2671d70b2ba744ada3d6d64919
    Signed-off-by: Timur Achmetow <achmetow84@googlemail.com>

---
+++ plugins/org.eclipse.recommenders.stats.rcp/src/org/eclipse/recommenders/stats/rcp/DeveloperActivityDialog.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2013 Timur Achmetow.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Timur Achmetow - Initial API and implementation
+ */
+package org.eclipse.recommenders.stats.rcp;
+
+import static java.lang.String.format;
+import static org.eclipse.recommenders.stats.rcp.TableViewerFactory.createWrapperComposite;
+
+import java.util.Date;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+import com.google.common.collect.Iterables;
+
+public class DeveloperActivityDialog extends TitleAreaDialog {
+
+    private StatisticsView statsView;
+
+    public DeveloperActivityDialog(Shell parentShell) {
+        super(parentShell);
+        setHelpAvailable(false);
+        statsView = new StatisticsView();
+    }
+
+    @Override
+    protected Control createContents(Composite parent) {
+        super.createContents(parent);
+        getShell().setText("Developer Activity Report");
+        getShell().setSize(600, 600);
+        setTitle("Developer Activities");
+        setMessage(getDescriptionText());
+        return parent;
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        parent.setLayout(new GridLayout());
+        SashForm masterSash = new SashForm(parent, SWT.HORIZONTAL);
+        masterSash.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+        final Composite masterCmp = createWrapperComposite(masterSash);
+        final Composite detailCmp = createWrapperComposite(masterSash);
+        final StackLayout stack = new StackLayout();
+        detailCmp.setLayout(stack);
+
+        final Composite statsCmp = createWrapperComposite(detailCmp);
+        final Composite commandsCmp = createWrapperComposite(detailCmp);
+        final Composite eclipseUsageCmp = createWrapperComposite(detailCmp);
+        NavigationViewer navigationViewer = new NavigationViewer();
+        navigationViewer.createViewer(masterCmp);
+        navigationViewer.addSelectionChangedListener(stack, detailCmp, statsCmp, commandsCmp, eclipseUsageCmp);
+        statsView.createContent(statsCmp);
+        new TriggeredCommandsView().createContent(commandsCmp);
+        new CategoryView().createContent(eclipseUsageCmp);
+        stack.topControl = eclipseUsageCmp;
+
+        masterSash.setWeights(new int[] { 30, 70 });
+        return parent;
+    }
+
+    @Override
+    protected void createButtonsForButtonBar(Composite parent) {
+        createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+    }
+
+    @Override
+    protected boolean isResizable() {
+        return true;
+    }
+
+    private String getDescriptionText() {
+        String date = "the beginning of recording";
+        if (statsView.getOkayEvents().size() > 0) {
+            Date start = new Date(Iterables.getFirst(statsView.getOkayEvents(), null).sessionStarted);
+            date = format("%tF", start);
+        }
+        return "Here is a summary of your activities since " + date;
+    }
+}
