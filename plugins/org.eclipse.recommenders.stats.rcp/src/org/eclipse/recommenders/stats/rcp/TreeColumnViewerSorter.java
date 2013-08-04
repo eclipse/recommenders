3ae92de5436753448222d09d7869f9e3f573827d
Marcel Bruch <marcel.bruch@codetrails.com>
2013-08-04 22:24:06 +0200

    [gsoc] Usage Data Collection for Code Completion
    New Tab integrated: Triggered Commands
    1. Shows the triggered Eclipse commands (all)
    2. Triggered commands will be persisted in the workspace
    3. The data will be presented in a TableViewer
    4. Sorter for each viewer column
    5. Included StyledCellLabelProvider (Commands tab)
    6. StyledText through SWT Labels replaced
    7. Category column removed, Tree through Table replaced
    
    Change-Id: Ia771d3d217618b2671d70b2ba744ada3d6d64919
    Signed-off-by: Timur Achmetow <achmetow84@googlemail.com>

---
+++ plugins/org.eclipse.recommenders.stats.rcp/src/org/eclipse/recommenders/stats/rcp/TreeColumnViewerSorter.java
@@ -0,0 +1,80 @@
+package org.eclipse.recommenders.stats.rcp;
+
+import org.eclipse.jface.viewers.ColumnViewer;
+import org.eclipse.jface.viewers.TreeViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerComparator;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public abstract class TreeColumnViewerSorter extends ViewerComparator {
+    public static final int ASC = 1;
+
+    public static final int NONE = 0;
+
+    public static final int DESC = -1;
+
+    private int direction = 0;
+
+    private final TreeViewerColumn column;
+
+    private final ColumnViewer viewer;
+
+    public TreeColumnViewerSorter(ColumnViewer viewer, TreeViewerColumn column) {
+        this.column = column;
+        this.viewer = viewer;
+        this.column.getColumn().addSelectionListener(new SelectionAdapter() {
+
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                if (TreeColumnViewerSorter.this.viewer.getComparator() != null) {
+                    if (TreeColumnViewerSorter.this.viewer.getComparator() == TreeColumnViewerSorter.this) {
+                        int tdirection = direction;
+
+                        if (tdirection == ASC) {
+                            setSorter(TreeColumnViewerSorter.this, DESC);
+                        } else if (tdirection == DESC) {
+                            setSorter(TreeColumnViewerSorter.this, ASC);
+                        }
+                    } else {
+                        setSorter(TreeColumnViewerSorter.this, ASC);
+                    }
+                } else {
+                    setSorter(TreeColumnViewerSorter.this, ASC);
+                }
+            }
+        });
+    }
+
+    public void setSorter(TreeColumnViewerSorter sorter, int direction) {
+        if (direction == NONE) {
+            column.getColumn().getParent().setSortColumn(null);
+            column.getColumn().getParent().setSortDirection(SWT.NONE);
+            viewer.setComparator(null);
+        } else {
+            column.getColumn().getParent().setSortColumn(column.getColumn());
+            sorter.direction = direction;
+
+            if (direction == ASC) {
+                column.getColumn().getParent().setSortDirection(SWT.UP);
+            } else {
+                column.getColumn().getParent().setSortDirection(SWT.DOWN);
+            }
+
+            if (viewer.getComparator() == sorter) {
+                viewer.refresh();
+            } else {
+                viewer.setComparator(sorter);
+            }
+
+        }
+    }
+
+    @Override
+    public int compare(Viewer viewer, Object e1, Object e2) {
+        return direction * doCompare(viewer, e1, e2);
+    }
+
+    protected abstract int doCompare(Viewer viewer, Object e1, Object e2);
+}
