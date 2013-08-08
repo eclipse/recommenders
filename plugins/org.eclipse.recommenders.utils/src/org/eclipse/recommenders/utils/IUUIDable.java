225a42ec5777f7d8386af3fec066255d6d8f4891
Andreas Sewe <andreas.sewe@codetrails.com>
2013-08-08 17:07:39 +0200

    [utils] Add IUUIDable interface
    Change-Id: I93d8012599338b2e5c6dd1f7f90dda7ff50ffb05

---
+++ plugins/org.eclipse.recommenders.utils/src/org/eclipse/recommenders/utils/IUUIDable.java
@@ -0,0 +1,11 @@
+package org.eclipse.recommenders.utils;
+
+import java.util.UUID;
+
+public interface IUUIDable {
+
+    /**
+     * @return a UUID stable for the entire lifetime of this object
+     */
+    UUID getUuid();
+}
