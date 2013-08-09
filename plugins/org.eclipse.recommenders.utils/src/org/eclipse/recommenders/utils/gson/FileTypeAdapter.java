4f2117ab7f1bf2cb3614da78e09f0cbd9cb87caf
Marcel Bruch <marcel.bruch@codetrails.com>
2013-08-09 14:20:46 +0200

    [utils] replaced gson de-serializers by TypeAdapters

---
+++ plugins/org.eclipse.recommenders.utils/src/org/eclipse/recommenders/utils/gson/FileTypeAdapter.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2010, 2013 Darmstadt University of Technology.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marcel Bruch - initial API and implementation.
+ */
+package org.eclipse.recommenders.utils.gson;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+public class FileTypeAdapter extends TypeAdapter<File> {
+
+    @Override
+    public void write(JsonWriter out, File value) throws IOException {
+        out.value(value.toString());
+    }
+
+    @Override
+    public File read(JsonReader in) throws IOException {
+        return new File(in.nextString());
+    }
+
+}
