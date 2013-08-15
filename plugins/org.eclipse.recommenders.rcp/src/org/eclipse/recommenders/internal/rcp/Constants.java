f3c4b6bcba6baa2fdb480cf989ab3080714413ef
Johannes Dorn <johannes.dorn@codetrails.com>
2013-08-15 12:51:47 +0200

    [preferences] Added survey dialog popup
    Dialog will open once if the CR plugin was activated at least 20 times AND
    if the first activation was at least seven days ago.
    
    Signed-off-by: Johannes Dorn <johannes.dorn@codetrails.com>

---
+++ plugins/org.eclipse.recommenders.rcp/src/org/eclipse/recommenders/internal/rcp/Constants.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2010 Darmstadt University of Technology.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Johannes Dorn - initial API and implementation.
+ */
+package org.eclipse.recommenders.internal.rcp;
+
+import static java.util.concurrent.TimeUnit.DAYS;
+import static org.eclipse.recommenders.rcp.utils.PreferencesHelper.createLinkLabelToPreferencePage;
+
+import java.net.URL;
+
+import org.eclipse.recommenders.utils.Urls;
+
+public class Constants {
+
+    /**
+     * The ID of the Survey preference page
+     */
+    protected static final String SURVEY_PREFERENCE_PAGE_ID = "org.eclipse.recommenders.rcp.survey.preferencepage";
+
+    /**
+     * How many milliseconds to wait to display the survey dialog after the conditions have been met.
+     */
+    public static final long SURVEY_DISPLAY_DELAY = 60000l;
+
+    /**
+     * The description of the survey.
+     */
+    public static final String SURVEY_DESCRIPTION = "To help us develop Code Recommenders to your needs, we ask you for a few moments of your time to fill out our user survey.";
+
+    /**
+     * A hint to inform the user to go to the preferences if he wants to take the survey later.
+     */
+    public static final String SURVEY_PREFERENCES_HINT = "This dialog will not appear again. If you want to take the survey at a later date, you can always go to <a>"
+            + createLinkLabelToPreferencePage(SURVEY_PREFERENCE_PAGE_ID) + "</a>. Do you want to take the survey now?";
+
+    /**
+     * The URL of the survey.
+     */
+    public static final URL SURVEY_URL = Urls
+            .toUrl("https://docs.google.com/a/codetrails.com/forms/d/1SqzZh1trpzS6UNEMjVWCvQTzGTBvjBFV-ZdwPuAwm5o/viewform");
+
+    /**
+     * Number of plugin activations before survey dialog may be displayed.
+     */
+    public static final int ACTIVATIONS_BEFORE_SURVEY = 20;
+
+    /**
+     * Number of minutes since first plugin activation before survey dialog may be displayed.
+     */
+    public static final long MILLIS_BEFORE_SURVEY = DAYS.toMillis(7);
+
+    /**
+     * Bundle symbolic name of the o.e.r.rcp bundle.
+     */
+    public static final String BUNDLE_NAME = "org.eclipse.recommenders.rcp";
+
+    /**
+     * Number of plugin activations. Used determine when the survey dialog may be displayed.
+     */
+    public static final String NUMBER_OF_ACTIVATIONS = "activation_count";
+
+    /**
+     * Date the plugin was first activated. Used determine the earliest time before the survey dialog may be displayed.
+     */
+    public static final String FIRST_ACTIVATION_DATE = "first_activation_date";
+
+    /**
+     * Whether the survey dialog has already been displayed to the user.
+     */
+    public static final String SURVEY_TAKEN = "survey_already_displayed";
+
+    /**
+     * Whether the user has elected not to take the survey.
+     */
+    public static final String SURVEY_OPT_OUT = "survey_opt_out";
+}
--- plugins/org.eclipse.recommenders.rcp/src/org/eclipse/recommenders/internal/rcp/RcpPlugin.java
+++ plugins/org.eclipse.recommenders.rcp/src/org/eclipse/recommenders/internal/rcp/RcpPlugin.java
@@ -7,16 +7,49 @@
  *
  * Contributors:
  *    Marcel Bruch - initial API and implementation.
+ *    Johannes Dorn - survey dialog logic
  */
 package org.eclipse.recommenders.internal.rcp;
 
+import static org.eclipse.recommenders.internal.rcp.Constants.ACTIVATIONS_BEFORE_SURVEY;
+import static org.eclipse.recommenders.internal.rcp.Constants.BUNDLE_NAME;
+import static org.eclipse.recommenders.internal.rcp.Constants.FIRST_ACTIVATION_DATE;
+import static org.eclipse.recommenders.internal.rcp.Constants.MILLIS_BEFORE_SURVEY;
+import static org.eclipse.recommenders.internal.rcp.Constants.NUMBER_OF_ACTIVATIONS;
+import static org.eclipse.recommenders.internal.rcp.Constants.SURVEY_DESCRIPTION;
+import static org.eclipse.recommenders.internal.rcp.Constants.SURVEY_DISPLAY_DELAY;
+import static org.eclipse.recommenders.internal.rcp.Constants.SURVEY_OPT_OUT;
+import static org.eclipse.recommenders.internal.rcp.Constants.SURVEY_PREFERENCES_HINT;
+import static org.eclipse.recommenders.internal.rcp.Constants.SURVEY_PREFERENCE_PAGE_ID;
+import static org.eclipse.recommenders.internal.rcp.Constants.SURVEY_TAKEN;
+import static org.eclipse.recommenders.internal.rcp.Constants.SURVEY_URL;
+
+import java.text.ParseException;
+
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.recommenders.rcp.utils.LoggingUtils;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWebBrowser;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
 import org.osgi.framework.BundleContext;
 
+import com.google.common.annotations.VisibleForTesting;
+
 public class RcpPlugin extends AbstractUIPlugin {
+
+    private static final String DIALOG_TITLE = "Help us improve Code Recommenders";
+
     private static RcpPlugin plugin;
     public static String P_UUID = "recommenders.uuid"; //$NON-NLS-1$
 
@@ -48,6 +81,7 @@ public class RcpPlugin extends AbstractUIPlugin {
     public void start(final BundleContext context) throws Exception {
         plugin = this;
         super.start(context);
+        handleSurveyDialog();
     }
 
     @Override
@@ -55,4 +89,92 @@ public class RcpPlugin extends AbstractUIPlugin {
         super.stop(context);
         plugin = null;
     }
+
+    private void handleSurveyDialog() throws ParseException {
+        IPreferenceStore preferences = new ScopedPreferenceStore(InstanceScope.INSTANCE, BUNDLE_NAME);
+        if (shouldDisplaySurveyDialog(preferences)) {
+            new WaitJob("Wait for Survey", SURVEY_DISPLAY_DELAY, preferences);
+        }
+    }
+
+    @VisibleForTesting
+    protected static boolean shouldDisplaySurveyDialog(IPreferenceStore preferences) {
+        if (preferences.getBoolean(SURVEY_TAKEN) || preferences.getBoolean(SURVEY_OPT_OUT))
+            return false;
+
+        if (enoughActivationsForSurvey(preferences)) {
+            if (enoughTimeForActiviation(preferences)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean enoughActivationsForSurvey(IPreferenceStore preferences) {
+        int numberOfActivations = preferences.getInt(NUMBER_OF_ACTIVATIONS);
+        preferences.setValue(NUMBER_OF_ACTIVATIONS, ++numberOfActivations);
+        return numberOfActivations >= ACTIVATIONS_BEFORE_SURVEY;
+    }
+
+    private static boolean enoughTimeForActiviation(IPreferenceStore preferences) {
+        long firstActivationDate = preferences.getLong(FIRST_ACTIVATION_DATE);
+        long currentTime = System.currentTimeMillis();
+        if (firstActivationDate != 0L) {
+            long timeSinceFirstActivation = currentTime - firstActivationDate;
+            return timeSinceFirstActivation > MILLIS_BEFORE_SURVEY;
+        } else {
+            preferences.setValue(FIRST_ACTIVATION_DATE, currentTime);
+            return false;
+        }
+    }
+
+    protected static void showSurveyDialog(final IPreferenceStore preferences) {
+        PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+            public void run() {
+                Shell activeShell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+
+                Dialog dialog = new PreferenceLinkDialog(activeShell, DIALOG_TITLE, null, SURVEY_DESCRIPTION,
+                        MessageDialog.QUESTION, new String[] { "Take the survey", "No, Thank you" }, 0,
+                        SURVEY_PREFERENCES_HINT, SURVEY_PREFERENCE_PAGE_ID);
+
+                if (dialog.open() == Dialog.OK) {
+                    try {
+                        IWebBrowser browser = PlatformUI.getWorkbench().getBrowserSupport().getExternalBrowser();
+                        browser.openURL(SURVEY_URL);
+                        preferences.setValue(SURVEY_TAKEN, true);
+                    } catch (PartInitException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    preferences.setValue(SURVEY_OPT_OUT, true);
+                }
+            }
+        });
+    }
+
+    private static class WaitJob extends Job {
+
+        private IPreferenceStore preferences;
+
+        public WaitJob(String name, long delay, IPreferenceStore preferences) {
+            super(name);
+            this.preferences = preferences;
+            setSystem(true);
+            schedule(delay);
+        }
+
+        @Override
+        public boolean shouldRun() {
+            return (!preferences.getBoolean(SURVEY_TAKEN) && !preferences.getBoolean(SURVEY_OPT_OUT));
+        }
+
+        @Override
+        protected IStatus run(IProgressMonitor monitor) {
+            showSurveyDialog(preferences);
+            return Status.OK_STATUS;
+        }
+
+    }
+
 }
