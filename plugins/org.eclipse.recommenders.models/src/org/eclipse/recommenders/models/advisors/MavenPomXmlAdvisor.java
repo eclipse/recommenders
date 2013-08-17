05ea2116fb23b6c2fcfc086a0c21120e903bf507
Olav Lenz <olav.lenz@gmail.com>
2013-08-17 11:58:37 +0200

    [models] Add UTF16 support to MavenPomXMLAdvisor
    Change-Id: I997a3a267a130349c86e660c173c571885636940
    Signed-off-by: Olav Lenz <olav.lenz@gmail.com>
    Signed-off-by: Marcel Bruch <marcel.bruch@codetrails.com>

--- plugins/org.eclipse.recommenders.models/src/org/eclipse/recommenders/models/advisors/MavenPomXmlAdvisor.java
+++ plugins/org.eclipse.recommenders.models/src/org/eclipse/recommenders/models/advisors/MavenPomXmlAdvisor.java
@@ -10,8 +10,7 @@
  */
 package org.eclipse.recommenders.models.advisors;
 
-import static com.google.common.base.Optional.absent;
-import static com.google.common.base.Optional.of;
+import static com.google.common.base.Optional.*;
 import static org.eclipse.recommenders.models.DependencyType.PROJECT;
 
 import java.io.File;
@@ -19,6 +18,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 import org.apache.maven.model.Model;
 import org.apache.maven.model.Parent;
@@ -36,7 +36,7 @@ import com.google.common.base.Optional;
 
 public class MavenPomXmlAdvisor extends AbstractProjectCoordinateAdvisor {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MavenPomXmlAdvisor.class);
+    private Logger log = LoggerFactory.getLogger(MavenPomXmlAdvisor.class);
 
     @Override
     protected Optional<ProjectCoordinate> doSuggest(DependencyInfo dependencyInfo) {
@@ -49,10 +49,10 @@ public class MavenPomXmlAdvisor extends AbstractProjectCoordinateAdvisor {
                 return extractProjectCoordinateFromModel(model);
             }
         } catch (IOException e) {
-            LOG.error("Could not read pom.xml file of dependency :" + dependencyInfo, e);
+            log.error("Could not read pom.xml file of dependency :" + dependencyInfo, e);
             return absent();
         } catch (XmlPullParserException e) {
-            LOG.error("Could not read pom.xml file of dependency :" + dependencyInfo, e);
+            log.error("Could not read pom.xml file of dependency :" + dependencyInfo, e);
             return absent();
         }
     }
@@ -61,11 +61,42 @@ public class MavenPomXmlAdvisor extends AbstractProjectCoordinateAdvisor {
         InputStreamReader pomInputStream = null;
         try {
             MavenXpp3Reader mavenReader = new MavenXpp3Reader();
-            pomInputStream = new InputStreamReader(new FileInputStream(pomfile), Charsets.UTF_8);
+            pomInputStream = new InputStreamReader(new FileInputStream(pomfile), detectFileEncoding(pomfile));
             return mavenReader.read(pomInputStream);
         } finally {
             IOUtils.closeQuietly(pomInputStream);
         }
+    }
+
+    private Charset detectFileEncoding(File file) {
+        FileInputStream input = null;
+        try {
+            input = new FileInputStream(file);
+            int first = input.read();
+            if (first == 0xEF) {
+                // look for the UTF-8 Byte Order Mark (BOM)
+                int second = input.read();
+                int third = input.read();
+                if (second == 0xBB && third == 0xBF) {
+                    return Charsets.UTF_8;
+                }
+            } else if (first == 0xFE) {
+                // look for the UTF-16 BOM
+                if (input.read() == 0xFF) {
+                    return Charsets.UTF_16BE;
+                }
+            } else if (first == 0xFF) {
+                if (input.read() == 0xFE) {
+                    return Charsets.UTF_16LE;
+                }
+            }
+        } catch (IOException e) {
+            log.error("Failed to determine charset for file " + file, e);
+        } finally {
+            IOUtils.closeQuietly(input);
+        }
+        log.debug("Detected neither UTF-8 not UTF-16. Returning default platform charset for file " + file);
+        return Charset.defaultCharset();
     }
 
     private Optional<ProjectCoordinate> extractProjectCoordinateFromModel(Model model) {
