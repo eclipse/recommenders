ffc8e3ed61cac84e8b33b2829bb8bae0c5fab207
Olav Lenz <olav.lenz@gmail.com>
2013-08-29 21:17:47 +0200

    [models] Ensure that version strings have the correct format.
    The version is checked in the constructor of ProjectCoordinate and
    ModelCoordinate. If the format is invalid an IllegalArgumentException
    will be thrown. Also Strategies add '.0' if the minor and/or micro
    version is missing.
    
    Change-Id: Ib63f7b4ca7a9ef215d55ad7132bbe745d69ae184
    Signed-off-by: Olav Lenz <olav.lenz@gmail.com>

--- plugins/org.eclipse.recommenders.models/src/org/eclipse/recommenders/models/advisors/MavenPomXmlAdvisor.java
+++ plugins/org.eclipse.recommenders.models/src/org/eclipse/recommenders/models/advisors/MavenPomXmlAdvisor.java
@@ -10,9 +10,10 @@
  */
 package org.eclipse.recommenders.models.advisors;
 
-import static com.google.common.base.Optional.*;
+import static com.google.common.base.Optional.absent;
 import static org.eclipse.recommenders.models.DependencyType.PROJECT;
 import static org.eclipse.recommenders.utils.IOUtils.closeQuietly;
+import static org.eclipse.recommenders.utils.Versions.canonicalizeVersion;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -118,7 +119,7 @@ public class MavenPomXmlAdvisor extends AbstractProjectCoordinateAdvisor {
 
         int indexOf = version.indexOf("-");
         version = version.substring(0, indexOf == -1 ? version.length() : indexOf);
-        return of(new ProjectCoordinate(groupId, artifactId, version));
+        return ProjectCoordinate.create(groupId, artifactId, canonicalizeVersion(version));
     }
 
     private boolean containsPropertyReference(String string) {
---
+++ plugins/org.eclipse.recommenders.utils/src/org/eclipse/recommenders/utils/Coordinates.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2010, 2013 Darmstadt University of Technology.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Olav Lenz - Initial implementation.
+ */
+package org.eclipse.recommenders.utils;
+
+import java.util.regex.Pattern;
+
+public class Coordinates {
+
+    private static final Pattern validCoordinatePart = Pattern.compile("[A-Za-z0-9_\\-.]+");
+
+    /**
+     * Check if a (non version) part of a coordinate like artifactId or groupId is not empty and contain no whitespace
+     * character.
+     */
+    public static boolean isCoordinateFieldValid(String string) {
+        return validCoordinatePart.matcher(string).matches();
+    }
+
+}
