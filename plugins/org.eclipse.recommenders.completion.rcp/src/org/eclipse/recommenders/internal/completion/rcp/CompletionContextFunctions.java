8b6c48369af4c8025a42642c98d70c84ba43347b
Marcel Bruch <marcel.bruch@codetrails.com>
2013-10-12 18:30:06 +0200

    [completion] added new API for extensible completion contexts
    The new API aims to provide a cleaner interface for computing values
    from the current completion context. It also provides the ability to
    cache computed values and thus minimize repeated computations of the
    same information. 
    
    In the long run, the code in RecommendersCompletionContext should be
    replaced by ICompletionContextFunctions soon.
    
    Change-Id: I487ec3ce06f65faed89c096c8f0f2f070c110574

---
+++ plugins/org.eclipse.recommenders.completion.rcp/src/org/eclipse/recommenders/internal/completion/rcp/CompletionContextFunctions.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2010, 2013 Darmstadt University of Technology.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marcel Bruch - initial API and implementation.
+ */
+package org.eclipse.recommenders.internal.completion.rcp;
+
+import static org.eclipse.recommenders.utils.Checks.cast;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.internal.codeassist.InternalCompletionContext;
+import org.eclipse.jdt.internal.compiler.lookup.MethodBinding;
+import org.eclipse.jdt.internal.compiler.util.ObjectVector;
+import org.eclipse.recommenders.completion.rcp.ICompletionContextFunction;
+import org.eclipse.recommenders.completion.rcp.IRecommendersCompletionContext;
+import org.eclipse.recommenders.completion.rcp.RecommendersCompletionContext;
+import org.eclipse.recommenders.rcp.utils.JdtUtils;
+import org.eclipse.recommenders.utils.names.ITypeName;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+
+public class CompletionContextFunctions {
+
+    public static final String CCTX_INTERNAL_COMPLETION_CONTEXT = "jdt-internal-completion-context";
+    public static final String CCTX_VISIBLE_METHODS = "visible-methods";
+
+    public static class ExpectedTypeContextFunction implements ICompletionContextFunction<IType> {
+
+        @Override
+        public IType compute(IRecommendersCompletionContext context, String key) {
+            IType value = context.getExpectedType().orNull();
+            context.set(key, value);
+            return value;
+        }
+    }
+
+    public static class ExpectedTypeNamesContextFunction implements ICompletionContextFunction<Set<ITypeName>> {
+
+        @Override
+        public Set<ITypeName> compute(IRecommendersCompletionContext context, String key) {
+            Set<ITypeName> value = context.getExpectedTypeNames();
+            context.set(key, value);
+            return value;
+        }
+    }
+
+    public static class InternalCompletionContextFunction implements
+            ICompletionContextFunction<InternalCompletionContext> {
+
+        @Override
+        public InternalCompletionContext compute(IRecommendersCompletionContext context, String key) {
+            InternalCompletionContext ctx = ((RecommendersCompletionContext) context).getCoreContext().orNull();
+            context.set(key, ctx);
+            return ctx;
+        }
+    }
+
+    public static class VisibleMethodsContextFunction implements ICompletionContextFunction<List<IMethod>> {
+
+        @Override
+        public List<IMethod> compute(IRecommendersCompletionContext context, String key) {
+            InternalCompletionContext ctx = context.get(CCTX_INTERNAL_COMPLETION_CONTEXT, null);
+            if (ctx == null || !ctx.isExtended()) {
+                return Collections.emptyList();
+            }
+            final ObjectVector v = ctx.getVisibleMethods();
+            final List<IMethod> res = Lists.newArrayListWithCapacity(v.size);
+            for (int i = v.size(); i-- > 0;) {
+                final MethodBinding b = cast(v.elementAt(i));
+                final Optional<IMethod> f = JdtUtils.createUnresolvedMethod(b);
+                if (f.isPresent()) {
+                    res.add(f.get());
+                }
+            }
+            context.set(key, res);
+            return res;
+        }
+    }
+}
