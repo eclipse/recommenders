26a983c761db624702b42d79b26133b2fd3d5698
Olav Lenz <olav.lenz@gmail.com>
2013-10-17 13:20:58 +0200

    [completion] Introduce ProposalTag and CompletionContextKey
    Change-Id: I984083bab99a6fd9e649426ea22978ce76b771b7
    Signed-off-by: Olav Lenz <olav.lenz@gmail.com>
    Signed-off-by: Marcel Bruch <marcel.bruch@codetrails.com>

---
+++ plugins/org.eclipse.recommenders.completion.rcp/src/org/eclipse/recommenders/completion/rcp/processable/IProposalTag.java
@@ -0,0 +1,10 @@
+package org.eclipse.recommenders.completion.rcp.processable;
+
+
+/**
+ * Marker interface for tags that may be used with {@link IProcessableProposal}s.
+ * 
+ * @see ProposalTag standard tags
+ */
+public interface IProposalTag {
+}
---
+++ plugins/org.eclipse.recommenders.completion.rcp/src/org/eclipse/recommenders/completion/rcp/processable/ProposalTag.java
@@ -0,0 +1,5 @@
+package org.eclipse.recommenders.completion.rcp.processable;
+
+public enum ProposalTag implements IProposalTag {
+    CONTEXT, RECOMMENDERS_SCORE, SUBWORDS_SCORE, IS_PREFIX_MATCH
+}
