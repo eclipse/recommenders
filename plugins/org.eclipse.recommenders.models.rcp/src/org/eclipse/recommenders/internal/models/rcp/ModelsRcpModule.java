4e251be6099f70db74d94d3e0621ea604bcff3f7
Olav Lenz <olav.lenz@gmail.com>
2013-11-06 15:17:11 +0100

    [models] Add preference page for advisors.
    Advisors can be dis-enabled in a preference page.
    An event is posted when the advisor configuration is changed.
    
    Change-Id: I3b7b6a9894dd135715d73af23d142613983bf7bf
    Signed-off-by: Olav Lenz <olav.lenz@gmail.com>

--- plugins/org.eclipse.recommenders.models.rcp/src/org/eclipse/recommenders/internal/models/rcp/ModelsRcpModule.java
+++ plugins/org.eclipse.recommenders.models.rcp/src/org/eclipse/recommenders/internal/models/rcp/ModelsRcpModule.java
@@ -85,6 +85,7 @@ public class ModelsRcpModule extends AbstractModule implements Module {
         bind(ManualProjectCoordinateAdvisor.class).in(SINGLETON);
         createAndBindNamedFile("caches/manual-mappings.json", MANUAL_MAPPINGS);
         createAndBindNamedFile("caches/identified-project-coordinates.json", IDENTIFIED_PACKAGE_FRAGMENT_ROOTS);
+
     }
 
     private void createAndBindNamedFile(String fileName, String name) {
