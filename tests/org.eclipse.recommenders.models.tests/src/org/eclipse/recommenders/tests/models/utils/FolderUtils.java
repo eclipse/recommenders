671e43b9648f8a29d52da01cba5830b9fb04d718
Andreas Sewe <andreas.sewe@codetrails.com>
2013-11-22 17:14:30 +0100

    [models] Improvements to SharedManualMappingsAdvisor
    * More tests
    * Marked as "experimental"
    * Moved right after "Manual mappings" (but disabled by default)
    
    Change-Id: Ib3895fe3ab3a69308e3cd02aedd382a9984e509e

---
+++ tests/org.eclipse.recommenders.models.tests/src/org/eclipse/recommenders/tests/models/utils/FolderUtils.java
@@ -0,0 +1,14 @@
+package org.eclipse.recommenders.tests.models.utils;
+
+import java.io.File;
+
+public class FolderUtils {
+
+    public static File dir(String... dirs) {
+        File file = File.listRoots()[0];
+        for (String dir : dirs) {
+            file = new File(file, dir);
+        }
+        return file;
+    }
+}
