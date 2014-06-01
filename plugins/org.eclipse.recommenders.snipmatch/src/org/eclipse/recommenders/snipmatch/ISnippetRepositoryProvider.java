970e7918da7aa351448707118f7fee5747f8d5ac
Olav Lenz <olav.lenz@gmail.com>
2014-06-01 15:15:30 +0200

    [snipmatch] Introduce ISnippetRepositoryConfiguration.
    Changed the way snippet repositories are created. All needed infos are
    stored in configurations (ISnippetRepositoryConfiguration) and stored in
    the preferences. Providers (ISnippetRepositoryProviders) are able to
    create concrete ISnippetRepository instance for a given configuration.
    The snippet view and preference page are able to work with the setup.
    This change might break thrid-party plugins because of the new extension
    point mechanism.
    
    Change-Id: I840e0ae32d61ad26bb1a951f682ae77e1be336ca
    Signed-off-by: Olav Lenz <olav.lenz@gmail.com>

---
+++ plugins/org.eclipse.recommenders.snipmatch/src/org/eclipse/recommenders/snipmatch/ISnippetRepositoryProvider.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2010, 2014 Darmstadt University of Technology.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Olav Lenz - initial API and implementation.
+ */
+package org.eclipse.recommenders.snipmatch;
+
+import java.io.File;
+
+import com.google.common.base.Optional;
+
+public interface ISnippetRepositoryProvider {
+
+    boolean isApplicable(String identifier);
+
+    ISnippetRepositoryConfiguration convert(String stringRepresentation);
+
+    String convert(ISnippetRepositoryConfiguration configuration);
+
+    Optional<ISnippetRepository> create(ISnippetRepositoryConfiguration configuration, File basedir);
+
+}
