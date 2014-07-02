d29e88515a0ec46a94b514c85e9e4334ee44eb8c
Olav Lenz <olav.lenz@gmail.com>
2014-07-02 13:24:31 +0200

    [snipmatch] Bug 438711: Configurations are not loaded correctly.
    Change-Id: I7e26b73fa1ef7566388e665e5cb13300a617540f
    Signed-off-by: Olav Lenz <olav.lenz@gmail.com>

--- plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/internal/snipmatch/rcp/RepositoryConfigurations.java
+++ plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/internal/snipmatch/rcp/RepositoryConfigurations.java
@@ -46,31 +46,39 @@ public class RepositoryConfigurations {
     public static final File LOCATION = InjectionService.getInstance().requestAnnotatedInstance(File.class,
             Names.named(SnipmatchRcpModule.REPOSITORY_CONFIGURATION_FILE));
 
-    public static SnippetRepositoryConfigurations loadConfigurations() {
-        Resource resource = provideResource();
+    protected static SnippetRepositoryConfigurations loadConfigurations(File file) {
+        Resource resource = provideResource(file);
+        SnippetRepositoryConfigurations configurations = SnipmatchFactory.eINSTANCE
+                .createSnippetRepositoryConfigurations();
 
-        SnippetRepositoryConfigurations configurations;
-        if (!resource.getContents().isEmpty()) {
-            configurations = (SnippetRepositoryConfigurations) resource.getContents().get(0);
-        } else {
-            configurations = SnipmatchFactory.eINSTANCE.createSnippetRepositoryConfigurations();
+        try {
+            resource.load(Collections.EMPTY_MAP);
+            if (!resource.getContents().isEmpty()) {
+                configurations = (SnippetRepositoryConfigurations) resource.getContents().get(0);
+            }
+        } catch (IOException e) {
+            LOG.error("Exception while loading repository configurations.", e); //$NON-NLS-1$
         }
 
         return configurations;
     }
 
-    private static Resource provideResource() {
+    public static SnippetRepositoryConfigurations loadConfigurations() {
+        return loadConfigurations(LOCATION);
+    }
+
+    private static Resource provideResource(File file) {
         Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
         Map<String, Object> m = reg.getExtensionToFactoryMap();
         m.put("snipmatch", new XMIResourceFactoryImpl()); //$NON-NLS-1$
 
         ResourceSet resSet = new ResourceSetImpl();
-        Resource resource = resSet.createResource(URI.createFileURI(LOCATION.getAbsolutePath()));
+        Resource resource = resSet.createResource(URI.createFileURI(file.getAbsolutePath()));
         return resource;
     }
 
-    public static void storeConfigurations(SnippetRepositoryConfigurations configurations) {
-        Resource resource = provideResource();
+    public static void storeConfigurations(SnippetRepositoryConfigurations configurations, File file) {
+        Resource resource = provideResource(file);
         resource.getContents().add(configurations);
 
         try {
@@ -80,7 +88,11 @@ public class RepositoryConfigurations {
         }
     }
 
-    public static List<SnippetRepositoryConfiguration> fetchDefaultConfigurations() {
+    public static void storeConfigurations(SnippetRepositoryConfigurations configurations) {
+        storeConfigurations(configurations, LOCATION);
+    }
+
+    protected static List<SnippetRepositoryConfiguration> fetchDefaultConfigurations() {
         List<SnippetRepositoryConfiguration> defaultConfigurations = Lists.newArrayList();
 
         Registry instance = EPackage.Registry.INSTANCE;
