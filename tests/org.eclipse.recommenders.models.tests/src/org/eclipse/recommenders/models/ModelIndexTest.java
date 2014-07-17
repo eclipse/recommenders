43f4d686d1c4b80d6ad1ac3f1d33a25454ab61c4
Olav Lenz <olav.lenz@gmail.com>
2014-07-17 09:38:53 +0200

    [models] Bug 439745: Indexes not updated at every Eclipse restart
    Change-Id: Iab7876552f128cdb54843619d4cfa24c63cd5844
    Signed-off-by: Olav Lenz <olav.lenz@gmail.com>

--- tests/org.eclipse.recommenders.models.tests/src/org/eclipse/recommenders/models/ModelIndexTest.java
+++ tests/org.eclipse.recommenders.models.tests/src/org/eclipse/recommenders/models/ModelIndexTest.java
@@ -1,24 +1,16 @@
 package org.eclipse.recommenders.models;
 
+import static org.eclipse.recommenders.models.ModelIndexTestUtils.*;
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
-import org.apache.lucene.analysis.KeywordAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.Field.Index;
-import org.apache.lucene.document.Field.Store;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
-import org.apache.lucene.util.Version;
-import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
-import org.eclipse.recommenders.utils.Constants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -29,69 +21,78 @@ import com.google.common.collect.Lists;
 @RunWith(Parameterized.class)
 public class ModelIndexTest {
 
-    private static final String SYMBOLIC_NAME = "org.example.project";
+    private static ProjectCoordinate PC_1 = new ProjectCoordinate("org.example", "project", "1.0.0");
+    private static ProjectCoordinate PC_2 = new ProjectCoordinate("org.example", "project", "2.0.0");
+    private static ProjectCoordinate PC_3 = new ProjectCoordinate("org.example", "example", "1.0.0");
+    private static ProjectCoordinate PC_4 = new ProjectCoordinate("com.example", "project", "1.0.0");
 
-    private static final ProjectCoordinate EXPECTED = new ProjectCoordinate("org.example", "project", "1.0.0");
+    private List<ProjectCoordinate> oldIndex;
+    private List<ProjectCoordinate> newIndex;
 
-    private final ProjectCoordinate expected;
-    private final String symbolicName;
-    private final Document[] indexContents;
-
-    public ModelIndexTest(ProjectCoordinate expected, String symbolicName, Document... indexContents) {
-        this.expected = expected;
-        this.symbolicName = symbolicName;
-        this.indexContents = indexContents;
+    public ModelIndexTest(List<ProjectCoordinate> oldIndex, List<ProjectCoordinate> newIndex) throws Exception {
+        this.oldIndex = oldIndex;
+        this.newIndex = newIndex;
     }
 
     @Parameters
     public static Collection<Object[]> scenarios() {
         LinkedList<Object[]> scenarios = Lists.newLinkedList();
 
-        scenarios.add(scenario(EXPECTED, SYMBOLIC_NAME,
-                coordinateWithSymbolicName(new DefaultArtifact("org.example:project:1.0.0"), SYMBOLIC_NAME)));
-        scenarios.add(scenario(EXPECTED, SYMBOLIC_NAME,
-                coordinateWithSymbolicName(new DefaultArtifact("org.example:project:1.0"), SYMBOLIC_NAME)));
-        scenarios.add(scenario(EXPECTED, SYMBOLIC_NAME,
-                coordinateWithSymbolicName(new DefaultArtifact("org.example:project:1.0.0.rc1"), SYMBOLIC_NAME)));
+        scenarios.add(scenario(Lists.newArrayList(PC_1, PC_3, PC_4), Lists.newArrayList(PC_2, PC_4)));
+        scenarios.add(scenario(Lists.newArrayList(PC_2, PC_4), Lists.<ProjectCoordinate>newArrayList()));
+        scenarios.add(scenario(Lists.<ProjectCoordinate>newArrayList(), Lists.newArrayList(PC_2, PC_4)));
 
         return scenarios;
     }
 
-    private static Object[] scenario(ProjectCoordinate expected, String symbolicName, Document... indexContents) {
-        return new Object[] { expected, symbolicName, indexContents };
+    private static Object[] scenario(List<ProjectCoordinate> oldIndex, List<ProjectCoordinate> newIndex) {
+        return new Object[] { oldIndex, newIndex };
     }
 
     @Test
     public void test() throws Exception {
-        Directory index = inMemoryIndex(indexContents);
+        Directory oldIndexDirectory = createIndexDirectory(oldIndex);
+        Directory newIndexDirectory = createIndexDirectory(newIndex);
 
-        IModelIndex sut = new ModelIndex(index);
+        IModelIndex sut = new ModelIndex(oldIndexDirectory);
         sut.open();
-        ProjectCoordinate pc = sut.suggestProjectCoordinateByArtifactId(symbolicName).get();
-        sut.close();
 
-        assertThat(pc, is(equalTo(expected)));
-    }
-
-    private Directory inMemoryIndex(Document... documents) throws Exception {
-        RAMDirectory directory = new RAMDirectory();
-        IndexWriterConfig conf = new IndexWriterConfig(Version.LUCENE_35, new KeywordAnalyzer());
-        IndexWriter writer = new IndexWriter(directory, conf);
-        for (Document document : documents) {
-            writer.addDocument(document);
+        for (ProjectCoordinate expected : oldIndex) {
+            ProjectCoordinate actual = sut.suggestProjectCoordinateByArtifactId(expected.toString()).get();
+            assertThat(actual, is(equalTo(expected)));
         }
-        writer.close();
-        return directory;
+
+        for (ProjectCoordinate expected : newIndex) {
+            if (!oldIndex.contains(expected)) {
+                assertThat(sut.suggestProjectCoordinateByArtifactId(expected.toString()).isPresent(), is(false));
+            }
+        }
+
+        sut.updateIndex(newIndexDirectory);
+
+        for (ProjectCoordinate expected : newIndex) {
+            ProjectCoordinate actual = sut.suggestProjectCoordinateByArtifactId(expected.toString()).get();
+            assertThat(actual, is(equalTo(expected)));
+        }
+
+        for (ProjectCoordinate expected : oldIndex) {
+            if (!newIndex.contains(expected)) {
+                assertThat(sut.suggestProjectCoordinateByArtifactId(expected.toString()).isPresent(), is(false));
+            }
+        }
+
+        sut.close();
     }
 
-    private static Document coordinateWithSymbolicName(Artifact coordinate, String symbolicName) {
-        Document doc = new Document();
-        doc.add(newStored(Constants.F_COORDINATE, coordinate.toString()));
-        doc.add(newStored(Constants.F_SYMBOLIC_NAMES, symbolicName));
-        return doc;
-    }
+    private Directory createIndexDirectory(Collection<ProjectCoordinate> projectCoordinates) throws Exception {
+        Document[] documents = new Document[projectCoordinates.size()];
 
-    private static Field newStored(String key, String value) {
-        return new Field(key, value, Store.YES, Index.NOT_ANALYZED);
+        int index = 0;
+        for (ProjectCoordinate pc : projectCoordinates) {
+            documents[index] = coordinateWithSymbolicName(new DefaultArtifact(pc.toString()), pc.toString());
+            index++;
+        }
+
+        return inMemoryIndex(documents);
     }
 }
