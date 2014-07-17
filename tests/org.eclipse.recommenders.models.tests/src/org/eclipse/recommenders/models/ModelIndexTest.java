75412099a0c4ac41b42f00b01bed1c4f06c215bb
Olav Lenz <olav.lenz@gmail.com>
2014-07-17 09:38:53 +0200

    Bug 439745: Indexes are not checked for updates at every Eclipse restart
    Change-Id: Iab7876552f128cdb54843619d4cfa24c63cd5844
    Signed-off-by: Olav Lenz <olav.lenz@gmail.com>

--- tests/org.eclipse.recommenders.models.tests/src/org/eclipse/recommenders/models/ModelIndexTest.java
+++ tests/org.eclipse.recommenders.models.tests/src/org/eclipse/recommenders/models/ModelIndexTest.java
@@ -2,23 +2,14 @@ package org.eclipse.recommenders.models;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
+import static org.eclipse.recommenders.tests.models.utils.ModelIndexTestUtils.*;
 
 import java.util.Collection;
 import java.util.LinkedList;
 
-import org.apache.lucene.analysis.KeywordAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.Field.Index;
-import org.apache.lucene.document.Field.Store;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
-import org.apache.lucene.util.Version;
-import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
-import org.eclipse.recommenders.utils.Constants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -73,25 +64,4 @@ public class ModelIndexTest {
         assertThat(pc, is(equalTo(expected)));
     }
 
-    private Directory inMemoryIndex(Document... documents) throws Exception {
-        RAMDirectory directory = new RAMDirectory();
-        IndexWriterConfig conf = new IndexWriterConfig(Version.LUCENE_35, new KeywordAnalyzer());
-        IndexWriter writer = new IndexWriter(directory, conf);
-        for (Document document : documents) {
-            writer.addDocument(document);
-        }
-        writer.close();
-        return directory;
-    }
-
-    private static Document coordinateWithSymbolicName(Artifact coordinate, String symbolicName) {
-        Document doc = new Document();
-        doc.add(newStored(Constants.F_COORDINATE, coordinate.toString()));
-        doc.add(newStored(Constants.F_SYMBOLIC_NAMES, symbolicName));
-        return doc;
-    }
-
-    private static Field newStored(String key, String value) {
-        return new Field(key, value, Store.YES, Index.NOT_ANALYZED);
-    }
 }
