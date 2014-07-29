fbe5e6c93046bcb46c9f7f43bcbb9ba3904b6cd1
Olav Lenz <olav.lenz@gmail.com>
2014-07-29 10:59:13 +0200

    [snipmatch] Bug 440622: Snippets View should display repo name
    Snippets View should display repo name instead of url.
    
    Change-Id: I08ee4d5cd8e0c978e05b2d4747c4ce493cd0fa5c
    Signed-off-by: Olav Lenz <olav.lenz@gmail.com>

--- plugins/org.eclipse.recommenders.snipmatch/src/org/eclipse/recommenders/snipmatch/ISnippetRepository.java
+++ plugins/org.eclipse.recommenders.snipmatch/src/org/eclipse/recommenders/snipmatch/ISnippetRepository.java
@@ -42,6 +42,8 @@ public interface ISnippetRepository extends Openable, Closeable {
      */
     List<Recommendation<ISnippet>> search(String query, int maxResults);
 
+    int getID();
+
     String getRepositoryLocation();
 
     boolean hasSnippet(UUID uuid);
