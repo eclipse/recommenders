c8a21fc73b70ff4d525d27db37bc51c414af9a5c
Marcel Bruch <marcel.bruch@codetrails.com>
2014-08-07 20:39:36 +0200

    [stacktraces] improved stacktrace data structures and toString output
    Change-Id: I03d4cccd3c2024e0b8c226856115d058b127966d

--- plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/LogListener.java
+++ plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/LogListener.java
@@ -20,15 +20,19 @@ import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.recommenders.stacktraces.StackTraceEvent;
+import org.eclipse.recommenders.internal.stacktraces.rcp.dto.StackTraceEvent;
 import org.eclipse.recommenders.utils.gson.GsonUtil;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.PlatformUI;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
 public class LogListener implements ILogListener, IStartup {
 
-    IEclipseContext ctx = (IEclipseContext) PlatformUI.getWorkbench().getService(IEclipseContext.class);
+    Cache<String, String> cache = CacheBuilder.newBuilder().maximumSize(10).build();
 
+    IEclipseContext ctx = (IEclipseContext) PlatformUI.getWorkbench().getService(IEclipseContext.class);
     StacktracesRcpPreferences pref = ContextInjectionFactory.make(StacktracesRcpPreferences.class, ctx);
 
     @Override
@@ -37,6 +41,12 @@ public class LogListener implements ILogListener, IStartup {
             return;
         }
         if (!status.matches(IStatus.ERROR)) {
+            return;
+        }
+
+        if (cache.getIfPresent(status.toString()) != null) {
+            // if this / similar error is was sent before, send it right away!
+            send(status);
             return;
         }
         String plugin = status.getPlugin();
@@ -71,11 +81,17 @@ public class LogListener implements ILogListener, IStartup {
             tmp.email = "[filled on submit]";
             int open = new WizardDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
                     new StacktraceWizard(pref, GsonUtil.serialize(tmp))).open();
-            if (open == Dialog.OK) {
-                StackTraceEvent event = createDto(status, pref);
-                new StacktraceUploadJob(event, pref.getServerUri()).schedule();
+            if (open != Dialog.OK) {
+                return;
+            } else if (pref.modeIgnore()) {
+                // the user may have chosen to ignore events in the wizard just now. Respect this preference:
+                return;
             }
         }
+        cache.put(status.toString(), status.toString());
+        StackTraceEvent event = createDto(status, pref);
+        // System.out.println(event);
+        new StacktraceUploadJob(event, pref.getServerUri()).schedule();
     }
 
     @Override
