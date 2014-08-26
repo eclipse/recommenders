c30f01b08873bffb6743671437ccf417918b982e
Marcel Bruch <marcel.bruch@codetrails.com>
2014-08-26 17:40:18 +0200

    [completion] added stopwatch decorating sessionprocessor
    Change-Id: I567978fa1b5c6ade99d30e49f21991d4ae861291

---
+++ plugins/org.eclipse.recommenders.completion.rcp/src/org/eclipse/recommenders/completion/rcp/processable/StopwatchSessionProcessor.java
@@ -0,0 +1,94 @@
+package org.eclipse.recommenders.completion.rcp.processable;
+
+import java.util.List;
+
+import org.apache.commons.lang3.time.StopWatch;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.recommenders.completion.rcp.IRecommendersCompletionContext;
+
+public class StopwatchSessionProcessor extends SessionProcessor {
+
+    private StopWatch watch = new StopWatch();
+    private SessionProcessor delegate;
+
+    public StopwatchSessionProcessor(SessionProcessor delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public boolean startSession(IRecommendersCompletionContext context) {
+        watch.start();
+        try {
+            return delegate.startSession(context);
+        } finally {
+            watch.suspend();
+        }
+    }
+
+    @Override
+    public void process(IProcessableProposal proposal) throws Exception {
+        try {
+            watch.resume();
+            delegate.process(proposal);
+        } finally {
+            watch.suspend();
+        }
+    }
+
+    @Override
+    public void endSession(List<ICompletionProposal> proposals) {
+        try {
+            watch.resume();
+            delegate.endSession(proposals);
+        } finally {
+            watch.suspend();
+        }
+    }
+
+    @Override
+    public void aboutToShow(List<ICompletionProposal> proposals) {
+        try {
+            watch.resume();
+            delegate.aboutToShow(proposals);
+        } finally {
+            watch.suspend();
+        }
+    }
+
+    @Override
+    public void selected(ICompletionProposal proposal) {
+        try {
+            watch.resume();
+            delegate.selected(proposal);
+        } finally {
+            watch.suspend();
+        }
+    }
+
+    @Override
+    public void applied(ICompletionProposal proposal) {
+        try {
+            watch.resume();
+            delegate.applied(proposal);
+        } finally {
+            watch.suspend();
+        }
+    }
+
+    @Override
+    public void aboutToClose() {
+        try {
+            watch.resume();
+            delegate.aboutToClose();
+        } finally {
+            watch.suspend();
+        }
+    }
+
+    /**
+     * @return the time in milliseconds
+     */
+    public long getTime() {
+        return watch.getTime();
+    }
+}
