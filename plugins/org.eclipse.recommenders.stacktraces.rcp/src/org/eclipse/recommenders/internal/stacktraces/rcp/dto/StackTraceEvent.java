45bcc1db5be67fdbafd39ed52e263f5fe4a77049
Daniel Haftstein <haftsteind@gmail.com>
2014-08-27 14:20:51 +0200

    [stacktraces] Extending data structure
    Preparation for further changes in Bug 442128 and Bug 441834
    
    Change-Id: I04137d2e42f420add11b8d800cd4f54cba0a7776
    Signed-off-by: Daniel Haftstein <haftsteind@gmail.com>

--- plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/dto/StackTraceEvent.java
+++ plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/dto/StackTraceEvent.java
@@ -24,8 +24,12 @@ public class StackTraceEvent implements Serializable {
     private static final long serialVersionUID = 1L;
 
     public UUID anonymousId;
+    public UUID evendId;
+    public UUID parendId;
+
     public String name;
     public String email;
+    public String comment;
 
     public String pluginId;
     public String pluginVersion;
