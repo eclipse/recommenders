177c51fed69e4e82fab74e142955e699a259bfe7
Johannes Dorn <johannes.dorn@codetrails.com>
2014-08-28 12:56:41 +0200

    [subwords] Bug 442778: Subwords w/ adjustable prefix length for types
    Change-Id: I731365b75015e4135fbbc1358ece88cf1c9829e3

--- plugins/org.eclipse.recommenders.subwords.rcp/src/org/eclipse/recommenders/internal/subwords/rcp/SubwordsPreferencePage.java
+++ plugins/org.eclipse.recommenders.subwords.rcp/src/org/eclipse/recommenders/internal/subwords/rcp/SubwordsPreferencePage.java
@@ -10,9 +10,18 @@
  */
 package org.eclipse.recommenders.internal.subwords.rcp;
 
+import static org.eclipse.jface.fieldassist.FieldDecorationRegistry.DEC_INFORMATION;
+
 import org.eclipse.core.runtime.preferences.InstanceScope;
-import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.fieldassist.ControlDecoration;
+import org.eclipse.jface.fieldassist.FieldDecoration;
+import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
@@ -27,14 +36,27 @@ public class SubwordsPreferencePage extends FieldEditorPreferencePage implements
     public void init(IWorkbench workbench) {
         setPreferenceStore(new ScopedPreferenceStore(InstanceScope.INSTANCE, Constants.BUNDLE_ID));
         setMessage(Messages.PREFPAGE_TITLE_SUBWORDS);
-        setDescription(Messages.PREFPAGE_DESCRIPTION_SUBWORDS);
     }
 
     @Override
     protected void createFieldEditors() {
-        addField(new BooleanFieldEditor(Constants.PREF_COMPREHENSIVE_SUBWORDS_MATCHING_CONSTRUCTORS,
-                Messages.FIELD_LABEL_COMPREHENSIVE_SUBWORDS_MATCHING_CONSTRUCTORS, getFieldEditorParent()));
-        addField(new BooleanFieldEditor(Constants.PREF_COMPREHENSIVE_SUBWORDS_MATCHING_TYPES,
-                Messages.FIELD_LABEL_COMPREHENSIVE_SUBWORDS_MATCHING_TYPES, getFieldEditorParent()));
+        addText(Messages.PREFPAGE_DESCRIPTION_SUBWORDS_PREFIX_LENGTH);
+        IntegerFieldEditor prefixLengthEditor = new IntegerFieldEditor(Constants.PREF_MIN_PREFIX_LENGTH_FOR_TYPES,
+                Messages.PREFPAGE_LABEL_PREFIX_LENGTH, getFieldEditorParent());
+        prefixLengthEditor.setValidRange(1, 99);
+        Text control = prefixLengthEditor.getTextControl(getFieldEditorParent());
+        FieldDecoration infoDecoration = FieldDecorationRegistry.getDefault().getFieldDecoration(DEC_INFORMATION);
+        ControlDecoration dec = new ControlDecoration(control, SWT.TOP | SWT.LEFT);
+        dec.setImage(infoDecoration.getImage());
+        dec.setDescriptionText(Messages.PREFPAGE_TOOLTIP_PREFIX_LENGTH);
+        addField(prefixLengthEditor);
     }
+
+    private void addText(final String text) {
+        final Label label = new Label(getFieldEditorParent(), SWT.WRAP);
+        label.setText(text);
+        GridDataFactory.fillDefaults().span(2, 1).hint(300, SWT.DEFAULT).grab(true, false)
+        .align(SWT.FILL, SWT.BEGINNING).applyTo(label);
+    }
+
 }
