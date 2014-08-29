6437e6582d47abe4193e00b8c76f602081087425
Daniel Haftstein <haftsteind@gmail.com>
2014-08-29 11:38:31 +0200

    [stacktraces] Bug 442753: Added preference page for stacktrace reporting
    Change-Id: Icebafc98f23a1cdeaff7c3f71871d5e94bd5679b
    Signed-off-by: Daniel Haftstein <haftsteind@gmail.com>
    Signed-off-by: Marcel Bruch <marcel.bruch@codetrails.com>

---
+++ plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/Messages.java
@@ -0,0 +1,30 @@
+package org.eclipse.recommenders.internal.stacktraces.rcp;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+    private static final String BUNDLE_NAME = "org.eclipse.recommenders.internal.stacktraces.rcp.messages"; //$NON-NLS-1$
+    public static String PREF_SERVER_LABEL;
+    public static String SETTINGS_ACTION;
+    public static String SETTINGS_ACTION_REPORT_ALWAYS;
+    public static String SETTINGS_ACTION_REPORT_NEVER;
+    public static String SETTINGS_ACTION_REPORT_NOW;
+    public static String SETTINGS_ANONYMIZE_STACKTRACES;
+    public static String SETTINGS_CLEAR_MESSAGES;
+    public static String SETTINGS_DESC;
+    public static String SETTINGS_EMAIL;
+    public static String SETTINGS_EMAIL_DESC;
+    public static String SETTINGS_GROUP_PERSONAL;
+    public static String SETTINGS_LEARN_MORE;
+    public static String SETTINGS_NAME;
+    public static String SETTINGS_NAME_DESC;
+    public static String SETTINGS_PROVIDE_FEEDBACK;
+    public static String SETTINGS_TITEL;
+    static {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+    private Messages() {
+    }
+}
