241b4eae3d2c67071b919c6055f7d91eb0b77f48
Michael Kutschke <kutschke.michael@googlemail.com>
2014-08-30 08:09:06 +0200

    [Jayes] Bug 441717: Import in GeNIe not working
    - added genie extensions so that the import in GeNIe should now work
    - GeNIe only allows alphanumeric characters and underscores in
    identifiers, everything else is now escaped with underscore + hex code
    (including underscores to avoid ambiguities)
    
    Change-Id: I335d8958e23eebaba7c29f8648b7b9eef58f50e2

--- plugins/org.eclipse.recommenders.jayes.io/src/org/eclipse/recommenders/internal/jayes/io/util/XMLUtil.java
+++ plugins/org.eclipse.recommenders.jayes.io/src/org/eclipse/recommenders/internal/jayes/io/util/XMLUtil.java
@@ -10,6 +10,9 @@
  */
 package org.eclipse.recommenders.internal.jayes.io.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class XMLUtil {
 
     private XMLUtil() {
@@ -17,14 +20,16 @@ public final class XMLUtil {
     }
 
     /**
-     * this method expects the attributes in pairwise name, value form e.g. </br> attributes = [ "id", "12345", "size",
-     * "15" ]
+     * this method expects the attributes in pairwise name, value form e.g.
+     * </br> attributes = [ "id", "12345", "size", "15" ]
      */
-    public static void surround(int offset, StringBuilder bldr, String surroundingTag, String... attributes) {
+    public static void surround(int offset, StringBuilder bldr,
+            String surroundingTag, String... attributes) {
         // TODO addTab
         bldr.insert(offset, '>');
 
-        for (int i = 0; i < attributes.length; i += 2) { // insert in reverted order
+        for (int i = 0; i < attributes.length; i += 2) { // insert in reverted
+                                                         // order
             bldr.insert(offset, "\" ");
             bldr.insert(offset, attributes[i + 1]);
             bldr.insert(offset, "=\"");
@@ -42,7 +47,7 @@ public final class XMLUtil {
 
     /**
      * adds a tab to every line
-     *
+     * 
      * @param text
      * @return
      */
@@ -50,7 +55,8 @@ public final class XMLUtil {
         return text.replaceAll("\n", "\n\t");
     }
 
-    public static void emptyTag(StringBuilder stringBuilder, String tagname, String... attributes) {
+    public static void emptyTag(StringBuilder stringBuilder, String tagname,
+            String... attributes) {
         stringBuilder.append('<');
         stringBuilder.append(tagname);
         stringBuilder.append(' ');
@@ -63,5 +69,33 @@ public final class XMLUtil {
         }
 
         stringBuilder.append("/>");
+    }
+
+    public static String escape(String text) {
+        String strPattern = "[^a-zA-z0-9]";
+        Pattern pattern = Pattern.compile(strPattern);
+        Matcher m = pattern.matcher(text);
+        StringBuffer buf = new StringBuffer(text.length());
+        while (m.find()) {
+            String replacement = String.format("_%04x",
+                    (int) m.group().charAt(0));
+            m.appendReplacement(buf, replacement);
+        }
+        m.appendTail(buf);
+        return buf.toString();
+    }
+
+    public static String unescape(String text) {
+        String strPattern = "_....";
+        Pattern pattern = Pattern.compile(strPattern);
+        Matcher m = pattern.matcher(text);
+        StringBuffer buf = new StringBuffer(text.length());
+        while (m.find()) {
+            String replacement = String.valueOf((char) Integer.parseInt(
+                    m.group(1), 16));
+            m.appendReplacement(buf, Matcher.quoteReplacement(replacement));
+        }
+        m.appendTail(buf);
+        return buf.toString();
     }
 }
