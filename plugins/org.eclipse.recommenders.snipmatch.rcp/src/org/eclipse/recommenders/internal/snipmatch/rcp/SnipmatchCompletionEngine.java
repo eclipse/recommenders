4139efcebe23245b76e1f4fdda1c45b1cc9b0e80
mlakjeewa <lakjeewadmm@gmail.com>
2014-11-01 22:43:43 +0530

    Bug 434332: Added a placeholder text to search text field
    Change-Id: I8eff739a9fd661eedac74fe7e07de20cd6b48d6a
    Signed-off-by: mlakjeewa <lakjeewadmm@gmail.com>

--- plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/internal/snipmatch/rcp/SnipmatchCompletionEngine.java
+++ plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/internal/snipmatch/rcp/SnipmatchCompletionEngine.java
@@ -62,245 +62,245 @@ import com.google.common.eventbus.EventBus;
 @SuppressWarnings("restriction")
 public class SnipmatchCompletionEngine {
 
-	private static enum AssistantControlState {
-		KEEP_OPEN, ENABLE_HIDE
-	}
+    private static enum AssistantControlState {
+        KEEP_OPEN, ENABLE_HIDE
+    }
 
-	private static final int SEARCH_BOX_WIDTH = 273;
+    private static final int SEARCH_BOX_WIDTH = 273;
 
-	private final SnipmatchContentAssistProcessor processor;
-	private final EventBus bus;
-	private final ColorRegistry colorRegistry;
-	private final FontRegistry fontRegistry;
-	private final ContentAssistant assistant;
+    private final SnipmatchContentAssistProcessor processor;
+    private final EventBus bus;
+    private final ColorRegistry colorRegistry;
+    private final FontRegistry fontRegistry;
+    private final ContentAssistant assistant;
 
-	private Shell searchShell;
-	private JavaContentAssistInvocationContext context;
-	private TemplateProposal selectedProposal;
-	private StyledText searchText;
-	private AssistantControlState state;
+    private Shell searchShell;
+    private JavaContentAssistInvocationContext context;
+    private TemplateProposal selectedProposal;
+    private StyledText searchText;
+    private AssistantControlState state;
 
-	@Inject
-	public SnipmatchCompletionEngine(SnipmatchContentAssistProcessor processor, EventBus bus,
-			ColorRegistry colorRegistry, FontRegistry fontRegistry) {
-		this.processor = processor;
-		this.bus = bus;
-		this.colorRegistry = colorRegistry;
-		this.fontRegistry = fontRegistry;
-		assistant = newContentAssistant();
-	}
+    @Inject
+    public SnipmatchCompletionEngine(SnipmatchContentAssistProcessor processor, EventBus bus,
+            ColorRegistry colorRegistry, FontRegistry fontRegistry) {
+        this.processor = processor;
+        this.bus = bus;
+        this.colorRegistry = colorRegistry;
+        this.fontRegistry = fontRegistry;
+        assistant = newContentAssistant();
+    }
 
-	private ContentAssistant newContentAssistant() {
-		ContentAssistant assistant = new ContentAssistant() {
+    private ContentAssistant newContentAssistant() {
+        ContentAssistant assistant = new ContentAssistant() {
 
-			@Override
-			public void hide() {
-				if (isFocused(searchText) && state != AssistantControlState.ENABLE_HIDE) {
-					// Ignore
-				} else {
-					super.hide();
-					selectedProposal = null;
-				}
-			}
+            @Override
+            public void hide() {
+                if (isFocused(searchText) && state != AssistantControlState.ENABLE_HIDE) {
+                    // Ignore
+                } else {
+                    super.hide();
+                    selectedProposal = null;
+                }
+            }
 
-			private boolean isFocused(Control control) {
-				Control focusControl = Display.getCurrent().getFocusControl();
-				return control.equals(focusControl);
-			}
-		};
-		assistant.addCompletionListener(new ICompletionListener() {
+            private boolean isFocused(Control control) {
+                Control focusControl = Display.getCurrent().getFocusControl();
+                return control.equals(focusControl);
+            }
+        };
+        assistant.addCompletionListener(new ICompletionListener() {
 
-			@Override
-			public void assistSessionEnded(ContentAssistEvent event) {
-				selectedProposal = null;
-				if (searchShell != null) {
-					searchShell.dispose();
-				}
-			}
+            @Override
+            public void assistSessionEnded(ContentAssistEvent event) {
+                selectedProposal = null;
+                if (searchShell != null) {
+                    searchShell.dispose();
+                }
+            }
 
-			@Override
-			public void selectionChanged(ICompletionProposal proposal, boolean smartToggle) {
+            @Override
+            public void selectionChanged(ICompletionProposal proposal, boolean smartToggle) {
 
-				if (proposal instanceof TemplateProposal) {
-					selectedProposal = (TemplateProposal) proposal;
-				} else {
-					selectedProposal = null;
-				}
-			}
+                if (proposal instanceof TemplateProposal) {
+                    selectedProposal = (TemplateProposal) proposal;
+                } else {
+                    selectedProposal = null;
+                }
+            }
 
-			@Override
-			public void assistSessionStarted(ContentAssistEvent event) {
-			}
-		});
+            @Override
+            public void assistSessionStarted(ContentAssistEvent event) {
+            }
+        });
 
-		assistant.setShowEmptyList(true);
-		assistant.enablePrefixCompletion(true);
-		assistant.enableColoredLabels(true);
-		assistant.setContentAssistProcessor(processor, DEFAULT_CONTENT_TYPE);
-		assistant.setInformationControlCreator(new TemplateInformationControlCreator(SWT.LEFT_TO_RIGHT));
-		assistant.setEmptyMessage(Messages.COMPLETION_ENGINE_NO_SNIPPETS_FOUND);
-		assistant.setRepeatedInvocationMode(true);
-		assistant.setStatusLineVisible(true);
-		assistant.setSorter(new RelevanceSorter());
+        assistant.setShowEmptyList(true);
+        assistant.enablePrefixCompletion(true);
+        assistant.enableColoredLabels(true);
+        assistant.setContentAssistProcessor(processor, DEFAULT_CONTENT_TYPE);
+        assistant.setInformationControlCreator(new TemplateInformationControlCreator(SWT.LEFT_TO_RIGHT));
+        assistant.setEmptyMessage(Messages.COMPLETION_ENGINE_NO_SNIPPETS_FOUND);
+        assistant.setRepeatedInvocationMode(true);
+        assistant.setStatusLineVisible(true);
+        assistant.setSorter(new RelevanceSorter());
 
-		return assistant;
-	}
+        return assistant;
+    }
 
-	public void show(final JavaContentAssistInvocationContext context) {
-		this.context = context;
-		processor.setContext(context);
-		assistant.install(context.getViewer());
-		state = AssistantControlState.KEEP_OPEN;
-		createSearchPopup();
-	}
+    public void show(final JavaContentAssistInvocationContext context) {
+        this.context = context;
+        processor.setContext(context);
+        assistant.install(context.getViewer());
+        state = AssistantControlState.KEEP_OPEN;
+        createSearchPopup();
+    }
 
-	private void createSearchPopup() {
-		Shell parentShell = context.getViewer().getTextWidget().getShell();
-		searchShell = new Shell(parentShell, SWT.ON_TOP);
-		searchShell.setLayout(new FillLayout());
-		searchShell.addListener(SWT.Traverse, new Listener() {
+    private void createSearchPopup() {
+        Shell parentShell = context.getViewer().getTextWidget().getShell();
+        searchShell = new Shell(parentShell, SWT.ON_TOP);
+        searchShell.setLayout(new FillLayout());
+        searchShell.addListener(SWT.Traverse, new Listener() {
 
-			@Override
-			public void handleEvent(Event e) {
-				if (e.detail == SWT.TRAVERSE_ESCAPE) {
-					state = AssistantControlState.ENABLE_HIDE;
-					assistant.uninstall();
-				}
-			}
-		});
+            @Override
+            public void handleEvent(Event e) {
+                if (e.detail == SWT.TRAVERSE_ESCAPE) {
+                    state = AssistantControlState.ENABLE_HIDE;
+                    assistant.uninstall();
+                }
+            }
+        });
 
-		searchText = new StyledText(searchShell, SWT.SINGLE);
-		searchText.setFont(fontRegistry.get(PREF_SEARCH_PLACEHOLDER_FONT)); //$NON-NLS-1$
-		searchText.setBackground(colorRegistry.get(PREF_SEARCH_BOX_BACKGROUND));
-		searchText.setForeground(colorRegistry.get(PREF_SEARCH_PLACEHOLDER_TEXT_COLOR));
-		searchText.setText(SEARCH_PLACEHOLDER_TEXT);
+        searchText = new StyledText(searchShell, SWT.SINGLE);
+        searchText.setFont(fontRegistry.get(PREF_SEARCH_PLACEHOLDER_FONT)); //$NON-NLS-1$
+        searchText.setBackground(colorRegistry.get(PREF_SEARCH_BOX_BACKGROUND));
+        searchText.setForeground(colorRegistry.get(PREF_SEARCH_PLACEHOLDER_TEXT_COLOR));
+        searchText.setText(SEARCH_PLACEHOLDER_TEXT);
 
-		searchText.addFocusListener(new FocusAdapter() {
+        searchText.addFocusListener(new FocusAdapter() {
 
-			@Override
-			public void focusLost(FocusEvent e) {
-				if (!assistant.hasProposalPopupFocus()) {
-					state = AssistantControlState.ENABLE_HIDE;
-					searchShell.dispose();
-					assistant.uninstall();
-				}
-			}
-		});
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (!assistant.hasProposalPopupFocus()) {
+                    state = AssistantControlState.ENABLE_HIDE;
+                    searchShell.dispose();
+                    assistant.uninstall();
+                }
+            }
+        });
 
-		searchText.addVerifyKeyListener(new VerifyKeyListener() {
+        searchText.addVerifyKeyListener(new VerifyKeyListener() {
 
-			boolean isFirstModification = true;
+            boolean isFirstModification = true;
 
-			@Override
-			public void verifyKey(VerifyEvent e) {
+            @Override
+            public void verifyKey(VerifyEvent e) {
 
-				if (isFirstModification) {
-					isFirstModification = false;
-					searchText.setFont(fontRegistry.get(PREF_SEARCH_TEXT_FONT));
-					searchText.setForeground(colorRegistry.get(PREF_SEARCH_TEXT_COLOR));
-					searchText.setText("");
-				}
+                if (isFirstModification) {
+                    isFirstModification = false;
+                    searchText.setFont(fontRegistry.get(PREF_SEARCH_TEXT_FONT));
+                    searchText.setForeground(colorRegistry.get(PREF_SEARCH_TEXT_COLOR));
+                    searchText.setText("");
+                }
 
-				TemplateProposal appliedProposal = selectedProposal;
-				switch (e.character) {
-				case SWT.CR:
-					e.doit = false;
-					if (appliedProposal != null) {
-						state = AssistantControlState.ENABLE_HIDE;
-						assistant.uninstall();
-						if (appliedProposal.isValidFor(context.getDocument(), context.getInvocationOffset())) {
-							if (appliedProposal instanceof SnippetProposal) {
-								snippetApplied((SnippetProposal) appliedProposal);
-							}
-							appliedProposal.apply(context.getViewer(), (char) 0, SWT.NONE,
-									context.getInvocationOffset());
+                TemplateProposal appliedProposal = selectedProposal;
+                switch (e.character) {
+                case SWT.CR:
+                    e.doit = false;
+                    if (appliedProposal != null) {
+                        state = AssistantControlState.ENABLE_HIDE;
+                        assistant.uninstall();
+                        if (appliedProposal.isValidFor(context.getDocument(), context.getInvocationOffset())) {
+                            if (appliedProposal instanceof SnippetProposal) {
+                                snippetApplied((SnippetProposal) appliedProposal);
+                            }
+                            appliedProposal.apply(context.getViewer(), (char) 0, SWT.NONE,
+                                    context.getInvocationOffset());
 
-							Point selection = appliedProposal.getSelection(context.getDocument());
-							if (selection != null) {
-								context.getViewer().setSelectedRange(selection.x, selection.y);
-								context.getViewer().revealRange(selection.x, selection.y);
-							}
-						}
-					} else {
-						state = AssistantControlState.ENABLE_HIDE;
-						assistant.uninstall();
-					}
-					return;
-				case SWT.TAB:
-					e.doit = false;
-					return;
-				}
+                            Point selection = appliedProposal.getSelection(context.getDocument());
+                            if (selection != null) {
+                                context.getViewer().setSelectedRange(selection.x, selection.y);
+                                context.getViewer().revealRange(selection.x, selection.y);
+                            }
+                        }
+                    } else {
+                        state = AssistantControlState.ENABLE_HIDE;
+                        assistant.uninstall();
+                    }
+                    return;
+                case SWT.TAB:
+                    e.doit = false;
+                    return;
+                }
 
-				// there is no navigation to support if no proposal is selected:
-				if (appliedProposal == null) {
-					return;
-				}
-				// but if there is, let's navigate...
-				switch (e.keyCode) {
-				case SWT.ARROW_UP:
-					execute(ContentAssistant.SELECT_PREVIOUS_PROPOSAL_COMMAND_ID);
-					return;
-				case SWT.ARROW_DOWN:
-					execute(ContentAssistant.SELECT_NEXT_PROPOSAL_COMMAND_ID);
-					return;
-				}
-			}
+                // there is no navigation to support if no proposal is selected:
+                if (appliedProposal == null) {
+                    return;
+                }
+                // but if there is, let's navigate...
+                switch (e.keyCode) {
+                case SWT.ARROW_UP:
+                    execute(ContentAssistant.SELECT_PREVIOUS_PROPOSAL_COMMAND_ID);
+                    return;
+                case SWT.ARROW_DOWN:
+                    execute(ContentAssistant.SELECT_NEXT_PROPOSAL_COMMAND_ID);
+                    return;
+                }
+            }
 
-			private void execute(String commandId) {
-				try {
-					assistant.getHandler(commandId).execute(null);
-				} catch (ExecutionException e) {
-					Throwables.propagate(e);
-				}
-			}
-		});
-		searchText.addModifyListener(new ModifyListener() {
+            private void execute(String commandId) {
+                try {
+                    assistant.getHandler(commandId).execute(null);
+                } catch (ExecutionException e) {
+                    Throwables.propagate(e);
+                }
+            }
+        });
+        searchText.addModifyListener(new ModifyListener() {
 
-			boolean isFirstModification = true;
+            boolean isFirstModification = true;
 
-			@Override
-			public void modifyText(ModifyEvent e) {
-				/*
-				 * The search place holder text is replaced with empty string at
-				 * the VerifyKeyListener. Therefore that empty replacement is
-				 * captured here as the first modification.
-				 */
-				if (isFirstModification) {
-					isFirstModification = false;
-					return;
-				}
-				String query = searchText.getText().trim();
-				processor.setTerms(query);
-				assistant.showPossibleCompletions();
-				assistant.showContextInformation();
-			}
-		});
+            @Override
+            public void modifyText(ModifyEvent e) {
+                /*
+                 * The search place holder text is replaced with empty string at
+                 * the VerifyKeyListener. Therefore that empty replacement is
+                 * captured here as the first modification.
+                 */
+                if (isFirstModification) {
+                    isFirstModification = false;
+                    return;
+                }
+                String query = searchText.getText().trim();
+                processor.setTerms(query);
+                assistant.showPossibleCompletions();
+                assistant.showContextInformation();
+            }
+        });
 
-		placeShell();
+        placeShell();
 
-		searchShell.open();
-		searchShell.setFocus();
-	}
+        searchShell.open();
+        searchShell.setFocus();
+    }
 
-	private void placeShell() {
-		// Pack the shell so that it is high enough for the text field.
-		searchShell.pack();
+    private void placeShell() {
+        // Pack the shell so that it is high enough for the text field.
+        searchShell.pack();
 
-		int searchBoxHeight = searchShell.getSize().y;
-		StyledText editorText = context.getViewer().getTextWidget();
-		Caret caret = editorText.getCaret();
-		int lineHeight = caret.getSize().y;
-		Point location = caret.getLocation();
-		Point anchor = editorText.toDisplay(location.x, location.y + lineHeight - searchBoxHeight);
+        int searchBoxHeight = searchShell.getSize().y;
+        StyledText editorText = context.getViewer().getTextWidget();
+        Caret caret = editorText.getCaret();
+        int lineHeight = caret.getSize().y;
+        Point location = caret.getLocation();
+        Point anchor = editorText.toDisplay(location.x, location.y + lineHeight - searchBoxHeight);
 
-		searchShell.setLocation(anchor.x, anchor.y);
-		searchShell.setSize(SEARCH_BOX_WIDTH, searchBoxHeight);
-	}
+        searchShell.setLocation(anchor.x, anchor.y);
+        searchShell.setSize(SEARCH_BOX_WIDTH, searchBoxHeight);
+    }
 
-	private void snippetApplied(SnippetProposal proposal) {
-		ISnippet snippet = proposal.getSnippet();
-		String repoUri = null;
-		// TODO How to get the repo uri?
-		bus.post(new SnippetAppliedEvent(snippet.getUuid(), repoUri));
-	}
+    private void snippetApplied(SnippetProposal proposal) {
+        ISnippet snippet = proposal.getSnippet();
+        String repoUri = null;
+        // TODO How to get the repo uri?
+        bus.post(new SnippetAppliedEvent(snippet.getUuid(), repoUri));
+    }
 }
