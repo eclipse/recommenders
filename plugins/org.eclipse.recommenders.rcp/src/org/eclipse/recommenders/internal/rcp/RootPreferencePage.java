85698f5538ffa8839de1c06793c9e7bcbabcf3ef
Moritz Beller <moritzbeller@gmx.de>
2014-11-10 16:53:24 +0100

    Bug 450853: Add link to dialog to code recommenders extensions discovery dialog on main preference page
    Change-Id: I1d743fe23a0bb555a27a5d0d04308d26236885a8
    Signed-off-by: Moritz Beller <moritzbeller@gmx.de>

--- plugins/org.eclipse.recommenders.rcp/src/org/eclipse/recommenders/internal/rcp/RootPreferencePage.java
+++ plugins/org.eclipse.recommenders.rcp/src/org/eclipse/recommenders/internal/rcp/RootPreferencePage.java
@@ -19,6 +19,7 @@ import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.recommenders.rcp.SharedImages;
 import org.eclipse.recommenders.rcp.SharedImages.Images;
 import org.eclipse.recommenders.rcp.utils.BrowserUtils;
+import org.eclipse.recommenders.rcp.utils.RemoteDiscoveryUtils;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -49,33 +50,47 @@ public class RootPreferencePage extends org.eclipse.jface.preference.PreferenceP
         Composite content = new Composite(parent, SWT.NONE);
         GridLayoutFactory.fillDefaults().numColumns(2).applyTo(content);
 
-        createLink(content, Messages.PREFPAGE_LABEL_HOMEPAGE, Images.OBJ_HOMEPAGE, Messages.PREFPAGE_LINK_HOMEPAGE,
-                "http://www.eclipse.org/recommenders/"); //$NON-NLS-1$
+        createBrowserLink(content, Messages.PREFPAGE_LABEL_HOMEPAGE, Images.OBJ_HOMEPAGE,
+                Messages.PREFPAGE_LINK_HOMEPAGE, "http://www.eclipse.org/recommenders/"); //$NON-NLS-1$
 
-        createLink(content, Messages.PREFPAGE_LABEL_MANUAL, Images.OBJ_CONTAINER, Messages.PREFPAGE_LINK_MANUAL,
+        createBrowserLink(content, Messages.PREFPAGE_LABEL_MANUAL, Images.OBJ_CONTAINER, Messages.PREFPAGE_LINK_MANUAL,
                 "http://www.eclipse.org/recommenders/manual/"); //$NON-NLS-1$
 
-        createLink(content, Messages.PREFPAGE_LABEL_FAVORITE, Images.OBJ_FAVORITE_STAR,
+        createBrowserLink(content, Messages.PREFPAGE_LABEL_FAVORITE, Images.OBJ_FAVORITE_STAR,
                 Messages.PREFPAGE_LINK_FAVORITE, "http://marketplace.eclipse.org/content/eclipse-code-recommenders"); //$NON-NLS-1$
 
-        createLink(content, Messages.PREFPAGE_LABEL_TWITTER, Images.OBJ_BIRD_BLUE, Messages.PREFPAGE_LINK_TWITTER,
-                "http://twitter.com/recommenders"); //$NON-NLS-1$
+        createBrowserLink(content, Messages.PREFPAGE_LABEL_TWITTER, Images.OBJ_BIRD_BLUE,
+                Messages.PREFPAGE_LINK_TWITTER, "http://twitter.com/recommenders"); //$NON-NLS-1$
+
+        Link extensionLink = createLink(content, Messages.PREFPAGE_LABEL_EXTENSIONS, Images.OBJ_LIGHTBULB,
+                Messages.PREFPAGE_LINK_EXTENSIONS, ""); //$NON-NLS-1$
+        extensionLink.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                RemoteDiscoveryUtils.openDiscoveryDialog();
+            }
+        });
 
         return new Composite(parent, SWT.NONE);
     }
 
-    private void createLink(Composite content, String description, Images icon, String urlLabel, String url) {
-        CLabel label = new CLabel(content, SWT.BEGINNING);
-        label.setText(description);
-        label.setImage(images.getImage(icon));
-
-        Link link = new Link(content, SWT.BEGINNING);
-        link.setText(MessageFormat.format(urlLabel, url));
+    private void createBrowserLink(Composite content, String description, Images icon, String urlLabel, String url) {
+        Link link = createLink(content, description, icon, urlLabel, url);
         link.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent event) {
                 BrowserUtils.openInExternalBrowser(event.text);
             }
         });
+    }
+
+    private Link createLink(Composite content, String description, Images icon, String urlLabel, String url) {
+        CLabel label = new CLabel(content, SWT.BEGINNING);
+        label.setText(description);
+        label.setImage(images.getImage(icon));
+
+        Link link = new Link(content, SWT.BEGINNING);
+        link.setText(MessageFormat.format(urlLabel, url));
+        return link;
     }
 }
