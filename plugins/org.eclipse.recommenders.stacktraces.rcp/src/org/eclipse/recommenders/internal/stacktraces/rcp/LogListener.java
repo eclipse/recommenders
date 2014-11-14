036c45fc1e0e369e23efee6ed0b160a7d57dcc3d
Daniel Haftstein <haftsteind@gmail.com>
2014-11-14 14:56:43 +0100

    [stacktraces] Bug 448771: Initial configuration dialog
    Change-Id: I99de9be29543ea656f59bb3bdc791699ed7793bc
    Signed-off-by: Daniel Haftstein <haftsteind@gmail.com>

--- plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/LogListener.java
+++ plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/LogListener.java
@@ -25,6 +25,7 @@ import org.eclipse.core.runtime.ILogListener;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.recommenders.internal.stacktraces.rcp.model.ErrorReport;
 import org.eclipse.recommenders.internal.stacktraces.rcp.model.SendAction;
@@ -66,10 +67,14 @@ public class LogListener implements ILogListener, IStartup {
     @Override
     public void logging(final IStatus status, String nouse) {
         try {
-            if (skipSendingReports() || !isErrorSeverity(status) || isRuntimeEclipse()) {
+            if (!isReportingAllowedInEnvironment() || !isErrorSeverity(status)) {
                 return;
             }
             settings = readSettings();
+
+            if (!settings.isConfigured()) {
+                firstConfiguration();
+            }
             if (!hasPluginIdWhitelistedPrefix(status, settings.getWhitelistedPluginIds())) {
                 return;
             }
@@ -93,16 +98,20 @@ public class LogListener implements ILogListener, IStartup {
         }
     }
 
+    private boolean isReportingAllowedInEnvironment() {
+        return !skipSendingReports() && !isRuntimeEclipse();
+    }
+
     private boolean skipSendingReports() {
         return Boolean.getBoolean(SYSPROP_SKIP_REPORTS);
     }
 
-    private boolean isErrorSeverity(final IStatus status) {
-        return status.matches(IStatus.ERROR);
-    }
-
     private boolean isRuntimeEclipse() {
         return null == System.getProperty(SYSPROP_ECLIPSE_BUILD_ID);
+    }
+
+    private boolean isErrorSeverity(final IStatus status) {
+        return status.matches(IStatus.ERROR);
     }
 
     @VisibleForTesting
@@ -126,6 +135,38 @@ public class LogListener implements ILogListener, IStartup {
 
     private boolean sentSimilarErrorBefore(final ErrorReport report) {
         return cache.getIfPresent(computeCacheKey(report)) != null;
+    }
+
+    private void firstConfiguration() {
+        Display.getDefault().syncExec(new Runnable() {
+            @Override
+            public void run() {
+                ConfigurationDialog configurationDialog = new ConfigurationDialog(PlatformUI.getWorkbench()
+                        .getActiveWorkbenchWindow().getShell(), settings);
+                configurationDialog.setBlockOnOpen(true);
+                int status = configurationDialog.open();
+
+                switch (status) {
+                case Window.OK: {
+                    settings.setConfigured(true);
+                    break;
+                }
+                case Window.CANCEL: {
+                    settings.setAction(SendAction.IGNORE);
+                    settings.setConfigured(true);
+                    break;
+                }
+                case ConfigurationDialog.ESC_CANCEL: {
+                    settings.setAction(SendAction.PAUSE_RESTART);
+                    settings.setConfigured(false);
+                    break;
+                }
+                default:
+                    ;
+                }
+                PreferenceInitializer.saveSettings(settings);
+            }
+        });
     }
 
     private String computeCacheKey(final ErrorReport report) {
@@ -164,6 +205,7 @@ public class LogListener implements ILogListener, IStartup {
                 } else if (settings.getAction() == SendAction.IGNORE || settings.getAction() == SendAction.PAUSE_DAY
                         || settings.getAction() == SendAction.PAUSE_RESTART) {
                     // the user may have chosen to not to send events in the wizard. Respect this preference:
+                    clear();
                     return;
                 }
                 sendAndClear();
