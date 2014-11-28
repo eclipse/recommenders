7081fac813c918b5bb142407ef47c6a1cbf0588e
Daniel Haftstein <haftsteind@gmail.com>
2014-11-28 15:39:11 +0100

    [stacktraces] Bug 451714: Stand-in-stacktraces added to internal status
    Change-Id: Iae0ef16743550ec29491486e6b402233997b1437
    Signed-off-by: Daniel Haftstein <haftsteind@gmail.com>

--- plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/StandInStacktraceProvider.java
+++ plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/StandInStacktraceProvider.java
@@ -16,8 +16,8 @@ import java.lang.reflect.Method;
 import java.util.Set;
 
 import org.apache.commons.lang3.ArrayUtils;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
+import org.eclipse.recommenders.internal.stacktraces.rcp.model.ErrorReports;
+import org.eclipse.recommenders.internal.stacktraces.rcp.model.Status;
 import org.eclipse.recommenders.utils.Reflections;
 
 public class StandInStacktraceProvider {
@@ -36,7 +36,7 @@ public class StandInStacktraceProvider {
     private static Method SET_EXCEPTION = Reflections.getDeclaredMethod(Status.class, "setException", Throwable.class)
             .orNull();
 
-    protected void insertStandInStacktraceIfEmpty(final IStatus status) {
+    protected void insertStandInStacktraceIfEmpty(final Status status) {
         if (requiresStandInStacktrace(status)) {
             Throwable syntetic = new StandInException(STAND_IN_MESSAGE);
             syntetic.fillInStackTrace();
@@ -44,6 +44,7 @@ public class StandInStacktraceProvider {
             StackTraceElement[] clearedStacktrace = clearBlacklistedTopStackframes(stacktrace,
                     Constants.STAND_IN_STACKTRACE_BLACKLIST);
             syntetic.setStackTrace(clearedStacktrace);
+            status.setException(ErrorReports.newThrowable(syntetic));
             try {
                 SET_EXCEPTION.invoke(status, syntetic);
             } catch (Exception e) {
@@ -52,13 +53,9 @@ public class StandInStacktraceProvider {
         }
     }
 
-    private boolean requiresStandInStacktrace(final IStatus status) {
+    private boolean requiresStandInStacktrace(final Status status) {
         if (status.getException() != null) {
             return false;
-        }
-        if (!(status instanceof Status)) {
-            return false;
-            // TODO log unknown status and send classname
         }
         return true;
     }
