815e01f546e58bf1a0c396e777836d89f6aa79d0
Daniel Haftstein <haftsteind@gmail.com>
2014-12-05 18:11:05 +0100

    [stacktraces] Message tests for ThankYouDialog rework
    Change-Id: I156a29f8cb6d835925dc2d348065a03b1e379068
    Signed-off-by: Daniel Haftstein <haftsteind@gmail.com>

--- tests/org.eclipse.recommenders.stacktraces.rcp.tests/src/org/eclipse/recommenders/internal/stacktraces/rcp/ThankYouDialogTest.java
+++ tests/org.eclipse.recommenders.stacktraces.rcp.tests/src/org/eclipse/recommenders/internal/stacktraces/rcp/ThankYouDialogTest.java
@@ -10,8 +10,10 @@
  */
 package org.eclipse.recommenders.internal.stacktraces.rcp;
 
+import static org.apache.commons.lang3.ArrayUtils.toArray;
+
 import java.text.MessageFormat;
-import java.util.Arrays;
+import java.util.LinkedList;
 
 import org.hamcrest.Matchers;
 import org.junit.Assert;
@@ -22,120 +24,178 @@ import org.junit.runners.Parameterized.Parameters;
 import org.mockito.Mockito;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 
 @RunWith(Parameterized.class)
 public class ThankYouDialogTest {
 
+    private static class ReportStateBuilder {
+
+        private String bugUrl;
+        private String bugId;
+        private String information;
+        private String[] keywords;
+        private String resolved;
+        private String status;
+        private Boolean created;
+
+        public ReportStateBuilder fillDefaults() {
+            bugUrl = BUG_URL;
+            bugId = BUG_ID;
+            information = null;
+            keywords = null;
+            resolved = null;
+            status = null;
+            created = false;
+            return this;
+        }
+
+        public ReportStateBuilder withInformation() {
+            information = BUG_INFORMATION;
+            return this;
+        }
+
+        public ReportStateBuilder unconfirmed() {
+            status = ReportState.UNCONFIRMED;
+            resolved = ReportState.UNKNOWN;
+            created = true;
+            return this;
+        }
+
+        public ReportStateBuilder newBug() {
+            status = ReportState.NEW;
+            resolved = ReportState.UNKNOWN;
+            return this;
+        }
+
+        public ReportStateBuilder assigned() {
+            status = ReportState.ASSIGNED;
+            resolved = ReportState.UNKNOWN;
+            return this;
+        }
+
+        public ReportStateBuilder resolvedFixed() {
+            status = ReportState.RESOLVED;
+            resolved = ReportState.FIXED;
+            return this;
+        }
+
+        public ReportStateBuilder resolvedDuplicate() {
+            status = ReportState.RESOLVED;
+            resolved = ReportState.DUPLICATE;
+            return this;
+        }
+
+        public ReportStateBuilder resolvedWontfix() {
+            status = ReportState.RESOLVED;
+            resolved = ReportState.WONTFIX;
+            return this;
+        }
+
+        public ReportStateBuilder resolvedWorksforme() {
+            status = ReportState.RESOLVED;
+            resolved = ReportState.WORKSFORME;
+            return this;
+        }
+
+        public ReportStateBuilder resolvedInvalid() {
+            status = ReportState.RESOLVED;
+            resolved = ReportState.INVALID;
+            return this;
+        }
+
+        public ReportStateBuilder closedFixed() {
+            status = ReportState.CLOSED;
+            resolved = ReportState.FIXED;
+            return this;
+        }
+
+        public ReportStateBuilder closedDuplicate() {
+            status = ReportState.CLOSED;
+            resolved = ReportState.DUPLICATE;
+            return this;
+        }
+
+        public ReportStateBuilder closedWontfix() {
+            status = ReportState.CLOSED;
+            resolved = ReportState.WONTFIX;
+            return this;
+        }
+
+        public ReportStateBuilder closedWorksforme() {
+            status = ReportState.CLOSED;
+            resolved = ReportState.WORKSFORME;
+            return this;
+        }
+
+        public ReportStateBuilder closedInvalid() {
+            status = ReportState.CLOSED;
+            resolved = ReportState.INVALID;
+            return this;
+        }
+
+        public ReportStateBuilder needInfo() {
+            keywords = new String[] { ReportState.KEYWORD_NEEDINFO };
+            return this;
+        }
+
+        public ReportState build() {
+            return newReportState(bugUrl, bugId, information, keywords, resolved, status, created);
+        }
+    }
+
     private static final String BUG_URL = "http://bug/bug42";
     private static final String BUG_ID = "42";
     private static final String BUG_INFORMATION = "Bug information";
+    private static final ReportStateBuilder builder = new ReportStateBuilder();
 
     private static String MESSAGE_INFO = MessageFormat.format(Messages.THANKYOUDIALOG_COMMITTER_MESSAGE,
             BUG_INFORMATION);
 
     private static String MESSAGE_END = Messages.THANKYOUDIALOG_THANK_YOU_FOR_HELP;
 
-    private static ReportState S_NEW_UNKNOWN_CREATED = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.UNKNOWN, ReportState.NEW, true);
-    private static String M_TRACKED = MessageFormat.format(Messages.THANKYOUDIALOG_NEW, BUG_URL, BUG_ID) + MESSAGE_INFO
-            + MESSAGE_END;
+    private static ReportState S_UNCONFIRMED_UNKNOWN = builder.fillDefaults().unconfirmed().build();
+    private static ReportState S_NEW_UNKNOWN = builder.fillDefaults().newBug().build();
+    private static ReportState S_ASSIGNED_UNKNOWN = builder.fillDefaults().assigned().build();
 
-    private static ReportState S_UNCONFIRMED_UNKNOWN = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.UNKNOWN, ReportState.UNCONFIRMED, false);
-    private static ReportState S_NEW_UNKNOWN = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.UNKNOWN, ReportState.NEW, false);
-    private static ReportState S_ASSIGNED_UNKNOWN = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.UNKNOWN, ReportState.ASSIGNED, false);
-    private static String M_MATCHED = MessageFormat.format(Messages.THANKYOUDIALOG_MATCHED_EXISTING_BUG, BUG_URL,
-            BUG_ID) + MESSAGE_INFO + MESSAGE_END;
+    private static ReportState S_RESOLVED_FIXED = builder.fillDefaults().resolvedFixed().build();
+    private static ReportState S_RESOLVED_DUPLICATE = builder.fillDefaults().resolvedDuplicate().build();
+    private static ReportState S_RESOLVED_WONTFIX = builder.fillDefaults().resolvedWontfix().build();
+    private static ReportState S_RESOLVED_WORKSFORME = builder.fillDefaults().resolvedWorksforme().build();
+    private static ReportState S_RESOLVED_INVALID = builder.fillDefaults().resolvedInvalid().build();
 
-    private static ReportState S_RESOLVED_FIXED = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.FIXED, ReportState.RESOLVED, false);
-    private static ReportState S_CLOSED_FIXED = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.FIXED, ReportState.CLOSED, false);
-    private static String M_FIXED = MessageFormat.format(Messages.THANKYOUDIALOG_MARKED_FIXED, BUG_URL, BUG_ID)
-            + MESSAGE_INFO + MESSAGE_END;
-
-    private static ReportState S_RESOLVED_DUPLICATE = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.DUPLICATE, ReportState.RESOLVED, false);
-    private static ReportState S_CLOSED_DUPLICATE = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.DUPLICATE, ReportState.CLOSED, false);
-    private static String M_DUPLICATE = MessageFormat.format(Messages.THANKYOUDIALOG_MARKED_DUPLICATE, BUG_URL, BUG_ID)
-            + MESSAGE_INFO + MESSAGE_END;
-
-    private static ReportState S_RESOLVED_MOVED = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.MOVED, ReportState.RESOLVED, false);
-    private static ReportState S_CLOSED_MOVED = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.MOVED, ReportState.CLOSED, false);
-    private static String M_MOVED = MessageFormat.format(Messages.THANKYOUDIALOG_MARKED_MOVED, BUG_URL, BUG_ID)
-            + MESSAGE_INFO + MESSAGE_END;
-
-    private static ReportState S_RESOLVED_WORKSFORME = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.WORKSFORME, ReportState.RESOLVED, false);
-    private static ReportState S_CLOSED_WORKSFORME = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.WORKSFORME, ReportState.CLOSED, false);
-    private static String M_VISIT = MessageFormat.format(Messages.THANKYOUDIALOG_MARKED_WORKSFORME, BUG_URL, BUG_ID)
-            + MESSAGE_INFO + MESSAGE_END;
-
-    private static ReportState S_RESOLVED_WONTFIX = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.WONTFIX, ReportState.RESOLVED, false);
-    private static ReportState S_CLOSED_WONTFIX = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.WONTFIX, ReportState.CLOSED, false);
-    private static ReportState S_RESOLVED_INVALID = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.INVALID, ReportState.RESOLVED, false);
-    private static ReportState S_CLOSED_INVALID = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.INVALID, ReportState.CLOSED, false);
-    private static ReportState S_RESOLVED_NOT_ECLIPSE = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.NOT_ECLIPSE, ReportState.RESOLVED, false);
-    private static ReportState S_CLOSED_NOT_ECLIPSE = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.NOT_ECLIPSE, ReportState.CLOSED, false);
-    private static String M_MARKED_NORMAL = MessageFormat
-            .format(Messages.THANKYOUDIALOG_MARKED_NORMAL, BUG_URL, BUG_ID) + MESSAGE_INFO + MESSAGE_END;
-
-    private static ReportState S_RESOLVED_UNKNOWN = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.UNKNOWN, ReportState.RESOLVED, false);
-    private static ReportState S_CLOSED_UNKNOWN = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.UNKNOWN, ReportState.CLOSED, false);
-    private static String M_UNKNWON = MessageFormat.format(Messages.THANKYOUDIALOG_MARKED_UNKNOWN, ReportState.UNKNOWN,
-            BUG_URL, BUG_ID) + MESSAGE_INFO + MESSAGE_END;
-
-    private static ReportState S_UNKNOWN_STATUS = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION, null,
-            ReportState.UNKNOWN, "any unknown server status", false);
-    private static String M_UNKNOWN_RESPONSE = MessageFormat.format(
-            Messages.THANKYOUDIALOG_RECEIVED_UNKNOWN_SERVER_RESPONSE, ReportState.UNKNOWN, BUG_URL, BUG_ID)
-            + MESSAGE_INFO + MESSAGE_END;
-
-    private static ReportState S_NEED_INFO = newReportState(BUG_URL, BUG_ID, BUG_INFORMATION,
-            new String[] { ReportState.KEYWORD_NEEDINFO }, ReportState.UNKNOWN, ReportState.ASSIGNED, true);
-    private static String M_NEED_INFO = MessageFormat.format(Messages.THANKYOUDIALOG_NEW, BUG_URL, BUG_ID)
-            + MESSAGE_INFO + MESSAGE_END;
-
-    private static ReportState S_NEED_INFO_NULL = newReportState(BUG_URL, BUG_ID, null,
-            new String[] { ReportState.KEYWORD_NEEDINFO }, ReportState.UNKNOWN, ReportState.ASSIGNED, true);
-    private static String M_NEED_INFO_NULL = MessageFormat.format(Messages.THANKYOUDIALOG_NEW, BUG_URL, BUG_ID)
-            + MESSAGE_END;
+    private static ReportState S_CLOSED_FIXED = builder.fillDefaults().closedFixed().build();
+    private static ReportState S_CLOSED_DUPLICATE = builder.fillDefaults().closedDuplicate().build();
+    private static ReportState S_CLOSED_WONTFIX = builder.fillDefaults().closedWontfix().build();
+    private static ReportState S_CLOSED_WORKSFORME = builder.fillDefaults().closedWorksforme().build();
+    private static ReportState S_CLOSED_INVALID = builder.fillDefaults().closedInvalid().build();
 
     private ReportState state;
-    private String expectedMessage;
+    private String messageShouldContain;
 
-    public ThankYouDialogTest(ReportState state, String expectedMessage) {
+    public ThankYouDialogTest(ReportState state, String messageShouldContain, String description) {
         this.state = state;
-        this.expectedMessage = expectedMessage;
+        this.messageShouldContain = messageShouldContain;
     }
 
-    @Parameters
+    @Parameters(name = "{2}")
     public static Iterable<Object[]> parameters() {
-        return Arrays.asList(new Object[][] { { S_NEW_UNKNOWN_CREATED, M_TRACKED },
-                { S_UNCONFIRMED_UNKNOWN, M_MATCHED }, { S_NEW_UNKNOWN, M_MATCHED }, { S_ASSIGNED_UNKNOWN, M_MATCHED },
-                { S_RESOLVED_FIXED, M_FIXED }, { S_CLOSED_FIXED, M_FIXED }, { S_RESOLVED_DUPLICATE, M_DUPLICATE },
-                { S_CLOSED_DUPLICATE, M_DUPLICATE }, { S_RESOLVED_MOVED, M_MOVED }, { S_CLOSED_MOVED, M_MOVED },
-                { S_RESOLVED_WORKSFORME, M_VISIT }, { S_CLOSED_WORKSFORME, M_VISIT },
-                { S_RESOLVED_WONTFIX, M_MARKED_NORMAL }, { S_CLOSED_WONTFIX, M_MARKED_NORMAL },
-                { S_RESOLVED_INVALID, M_MARKED_NORMAL }, { S_CLOSED_INVALID, M_MARKED_NORMAL },
-                { S_RESOLVED_NOT_ECLIPSE, M_MARKED_NORMAL }, { S_CLOSED_NOT_ECLIPSE, M_MARKED_NORMAL },
-                { S_RESOLVED_UNKNOWN, M_UNKNWON }, { S_CLOSED_UNKNOWN, M_UNKNWON },
-                { S_UNKNOWN_STATUS, M_UNKNOWN_RESPONSE }, { S_NEED_INFO, M_NEED_INFO },
-                { S_NEED_INFO_NULL, M_NEED_INFO_NULL } });
+        LinkedList<Object[]> tests = Lists.newLinkedList();
+        tests.add(toArray(S_UNCONFIRMED_UNKNOWN, Messages.THANKYOUDIALOG_NEW, "UNCONFIRMED UNKNOWN"));
+        tests.add(toArray(S_NEW_UNKNOWN, "###", "NEW UNKNOWN"));
+        tests.add(toArray(S_ASSIGNED_UNKNOWN, "###", "ASSIGNED UNKNOWN"));
+        tests.add(toArray(S_RESOLVED_FIXED, "###", "RESOLVED FIXED"));
+        tests.add(toArray(S_RESOLVED_DUPLICATE, "###", "RESOLVED DUPLICATE"));
+        tests.add(toArray(S_RESOLVED_WONTFIX, "###", "RESOLVED WONTFIX"));
+        tests.add(toArray(S_RESOLVED_WORKSFORME, "###", "RESOLVED WORKSFORME"));
+        tests.add(toArray(S_RESOLVED_INVALID, "###", "RESOLVED INVALID"));
+        tests.add(toArray(S_CLOSED_FIXED, "###", "CLOSED FIXED"));
+        tests.add(toArray(S_CLOSED_DUPLICATE, "###", "CLOSED DUPLICATE"));
+        tests.add(toArray(S_CLOSED_WONTFIX, "###", "CLOSED WONTFIX"));
+        tests.add(toArray(S_CLOSED_WORKSFORME, "###", "CLOSED WORKSFORME"));
+        tests.add(toArray(S_CLOSED_INVALID, "###", "CLOSED INVALID"));
+
+        return tests;
     }
 
     private static ReportState newReportState(String bugUrl, String bugId, String information, String[] keywords,
@@ -155,7 +215,12 @@ public class ThankYouDialogTest {
     public void testMessageForState() {
         ThankYouDialog sut = new ThankYouDialog(null, state);
 
+        // split at each placeholder like {0}
+        String[] split = messageShouldContain.split("\\{(.+?)\\}");
+
         String buildText = sut.buildText();
-        Assert.assertThat(buildText, Matchers.is(expectedMessage));
+        for (String s : split) {
+            Assert.assertThat(buildText, Matchers.containsString(s));
+        }
     }
 }
