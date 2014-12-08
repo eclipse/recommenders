6c97c1db7747e856f5ef63a98e2c600742403e9f
Varun <varunraiko1803@gmail.com>
2014-12-08 18:05:33 +0530

    Bug 434737
    Change-Id: I267299391bee53f71d654b15c14f8bc0f4dd7f18
    Signed-off-by: Varun <varunraiko1803@gmail.com>

---
+++ plugins/org.eclipse.recommenders.models.rcp/src/org/eclipse/recommenders/internal/models/rcp/ProjectCoordinateComparator.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Varun Gupta.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Varun Gupta - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.recommenders.models;
+
+import java.util.Comparator;
+
+public class ProjectCoordinateComparator implements Comparator<ProjectCoordinate> {
+    @Override
+    public int compare(ProjectCoordinate pc1, ProjectCoordinate pc2) {
+        int cmpGroupId = pc1.getGroupId().compareToIgnoreCase(pc2.getGroupId());
+        int cmpArtId = pc1.getArtifactId().compareToIgnoreCase(pc2.getArtifactId());
+        int cmpVersion = versionComparison(pc1.getVersion(), pc2.getVersion());
+        int cmpFinal = cmpGroupId != 0 ? cmpGroupId : cmpArtId != 0 ? cmpArtId : cmpVersion;
+        return cmpFinal;
+    }
+
+    public static int versionComparison(String str1, String str2) {
+        int val = 0;
+        String[] version1 = str1.split("\\.");
+        String[] version2 = str2.split("\\.");
+        // max and min considered if there is any one of them missing <major version>.<minor version>.<incremental
+        // version>
+
+        int length = Math.min(version1.length, version2.length);
+        int maximum = Math.max(version1.length, version2.length);
+
+        for (int i = 0; i < length; i++) {
+            if (version1[i].compareTo(version2[i]) < 0) {
+                val = -1;
+                break;
+            } else if (version1[i].compareTo(version2[i]) > 0) {
+                val = 1;
+                break;
+            }
+        }
+        if (val == 0) {
+            if (maximum == version1.length) {
+                val = 1;
+            } else {
+                val = -1;
+            }
+        }
+        return val;
+    }
+}
---
+++ tests/org.eclipse.recommenders.models.rcp.tests/src/org/eclipse/recommenders/internal/models/rcp/ProjectCoordinateComparatorTest.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Varun Gupta.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Varun Gupta - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.recommenders.internal.models.rcp;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.recommenders.models.ProjectCoordinate;
+import org.hamcrest.collection.IsIterableContainingInOrder;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+public class ProjectCoordinateComparatorTest {
+
+    @Test
+    public void test() {
+        ProjectCoordinate pc1 = new ProjectCoordinate("org.eclipse.zest", "org.eclipse.zest.core", "1.0.0");
+        ProjectCoordinate pc2 = new ProjectCoordinate("org.eclipse", "osgi", "4.0.0");
+        ProjectCoordinate pc3 = new ProjectCoordinate("org.ECLIPSE", "OSGI", "7.0.0");
+        List<ProjectCoordinate> proCoordinates = Lists.newArrayList();
+        List<ProjectCoordinate> proCoordinatesSorted = Lists.newArrayList();
+        ProjectCoordinateComparator pcc = new ProjectCoordinateComparator();
+        proCoordinates.add(pc1);
+        proCoordinates.add(pc2);
+        proCoordinates.add(pc3);
+        Collections.sort(proCoordinates, pcc);
+        proCoordinatesSorted.add(pc2);
+        proCoordinatesSorted.add(pc3);
+        proCoordinatesSorted.add(pc1);
+        Assert.assertThat(proCoordinatesSorted, IsIterableContainingInOrder.contains(proCoordinates.toArray()));
+    }
+}
