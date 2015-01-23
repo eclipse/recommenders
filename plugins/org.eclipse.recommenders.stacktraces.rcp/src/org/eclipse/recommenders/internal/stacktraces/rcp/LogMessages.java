261f95bfb75e814129fd280053a9b28765b5a3ac
Daniel Haftstein <haftsteind@gmail.com>
2015-01-23 09:44:22 +0100

    [stacktraces] Bug 458202: stacktraces version added to log
    Change-Id: I1ab46bb59bc4add1e4b657b7930fbfcb28696b93
    Signed-off-by: Daniel Haftstein <haftsteind@gmail.com>

--- plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/LogMessages.java
+++ plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/LogMessages.java
@@ -13,7 +13,6 @@ package org.eclipse.recommenders.internal.stacktraces.rcp;
 import static org.eclipse.core.runtime.IStatus.*;
 
 import org.eclipse.recommenders.utils.Logs.DefaultLogMessage;
-import org.eclipse.recommenders.utils.Logs.ILogMessage;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
@@ -22,34 +21,39 @@ public final class LogMessages extends DefaultLogMessage {
     private static int code = 1;
 
     private static final Bundle BUNDLE = FrameworkUtil.getBundle(LogMessages.class);
+    private static final String VERSION = "Version: " + BUNDLE.getVersion().toString(); //$NON-NLS-1$
 
     public static final LogMessages NO_INTERNET = new LogMessages(WARNING,
-            "Could not connect to server. Your IP is ''{0}''"); //$NON-NLS-1$
+            "Could not connect to server. Your IP is ''{0}'' " + VERSION); //$NON-NLS-1$
     public static final LogMessages FAILED_TO_PARSE_SEND_MODE = new LogMessages(WARNING,
-            "Failed to parse send mode ''{0}''. Returning ''{1}'' instead."); //$NON-NLS-1$
+            "Failed to parse send mode ''{0}''. Returning ''{1}'' instead. " + VERSION); //$NON-NLS-1$
 
     public static final LogMessages FAILED_TO_PARSE_REMEMBER_SEND_MODE = new LogMessages(WARNING,
-            "Failed to parse remember send mode ''{0}''. Returning ''{1}'' instead."); //$NON-NLS-1$
+            "Failed to parse remember send mode ''{0}''. Returning ''{1}'' instead. " + VERSION); //$NON-NLS-1$
 
     public static final LogMessages LOG_WARNING_REFLECTION_FAILED = new LogMessages(WARNING,
-            "Could not access \u2018{0}\u2019 using reflection.  Functionality may be limited."); //$NON-NLS-1$
-    public static final ILogMessage PAUSE_PERIOD_ELAPSED = new LogMessages(INFO,
-            "The paused interval for error reporting is elapsed, returning to 'ASK'-Mode"); //$NON-NLS-1$
-    public static final LogMessages SAVE_PREFERENCES_FAILED = new LogMessages(ERROR, "Saving preferences failed"); //$NON-NLS-1$
+            "Could not access \u2018{0}\u2019 using reflection.  Functionality may be limited. " + VERSION); //$NON-NLS-1$
+    public static final LogMessages PAUSE_PERIOD_ELAPSED = new LogMessages(INFO,
+            "The paused interval for error reporting is elapsed, returning to 'ASK'-Mode " + VERSION); //$NON-NLS-1$
+    public static final LogMessages SAVE_PREFERENCES_FAILED = new LogMessages(ERROR,
+            "Saving preferences failed. " + VERSION); //$NON-NLS-1$
 
-    public static final ILogMessage REPORTING_ERROR = new LogMessages(WARNING, "Error during error-reporting");
+    public static final LogMessages REPORTING_ERROR = new LogMessages(WARNING, "Error during error-reporting. "
+            + VERSION);
 
-    public static final ILogMessage FIRST_CONFIGURATION_FAILED = new LogMessages(WARNING,
-            "First configuration failed, please check the log"); //$NON-NLS-1$
-    public static final ILogMessage THANK_YOU_DIALOG_ERROR = new LogMessages(WARNING, "Error in thank you dialog"); //$NON-NLS-1$
+    public static final LogMessages FIRST_CONFIGURATION_FAILED = new LogMessages(WARNING,
+            "First configuration failed, please check the log. " + VERSION); //$NON-NLS-1$
+    public static final LogMessages THANK_YOU_DIALOG_ERROR = new LogMessages(WARNING,
+            "Error in thank you dialog. " + VERSION); //$NON-NLS-1$
 
-    public static final LogMessages HISTORY_NOT_AVAILABLE = new LogMessages(WARNING, "History service is not available"); //$NON-NLS-1$
+    public static final LogMessages HISTORY_NOT_AVAILABLE = new LogMessages(WARNING,
+            "History service is not available. " + VERSION); //$NON-NLS-1$
     public static final LogMessages HISTORY_NO_FINGERPRINT = new LogMessages(WARNING,
-            "Cannot index error report without fingerprint"); //$NON-NLS-1$
+            "Cannot index error report without fingerprint. " + VERSION); //$NON-NLS-1$
     public static final LogMessages HISTORY_STOP_FAILED = new LogMessages(WARNING,
-            "Stoping the history service failed."); //$NON-NLS-1$
+            "Stoping the history service failed. " + VERSION); //$NON-NLS-1$
     public static final LogMessages HISTORY_START_FAILED = new LogMessages(WARNING,
-            "Starting the history service failed."); //$NON-NLS-1$
+            "Starting the history service failed. " + VERSION); //$NON-NLS-1$
 
     public LogMessages(int severity, String message) {
         super(severity, code++, message);
