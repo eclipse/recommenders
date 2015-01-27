e1258218131efa2c0ff6f97558fc7c9e405f5cce
Marcel Bruch <marcel.bruch@codetrails.com>
2015-01-27 10:27:59 +0100

    [stacktraces] multiple configure dialogs on init
    * Trying to recover from CME inside platform. Added additional try catch
    statements in runnable.
    * Outlined a e4 preferences based implementation of settings.
    
    Bug: 458202
    Bug: 458054
    Change-Id: I56b59a1b5a50ecdd936879c7acfe6380fa0f72bd
    Signed-off-by: Marcel Bruch <marcel.bruch@codetrails.com>

--- tests/org.eclipse.recommenders.stacktraces.rcp.tests/src/org/eclipse/recommenders/internal/stacktraces/rcp/WhitelistTest.java
+++ tests/org.eclipse.recommenders.stacktraces.rcp.tests/src/org/eclipse/recommenders/internal/stacktraces/rcp/WhitelistTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 
 public class WhitelistTest {
 
-    Settings s = PreferenceInitializer.readSettings();
+    Settings s = PreferenceInitializer.create();
 
     @Test
     public void testWhitelistPackages() throws Exception {
