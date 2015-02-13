01dbd5426699711ab4ecbd3535371fecaebace64
Andreas Sewe <andreas.sewe@codetrails.com>
2015-02-13 14:28:06 +0100

    [completion] Allow for periodic completion tips
    Bug: 459886
    Change-Id: Ie45ab458d3282586fbadf6b92d025894b14d556f
    Signed-off-by: Andreas Sewe <andreas.sewe@codetrails.com>

--- plugins/org.eclipse.recommenders.completion.rcp/src/org/eclipse/recommenders/internal/completion/rcp/tips/TipsSessionProcessor.java
+++ plugins/org.eclipse.recommenders.completion.rcp/src/org/eclipse/recommenders/internal/completion/rcp/tips/TipsSessionProcessor.java
@@ -10,14 +10,15 @@
  */
 package org.eclipse.recommenders.internal.completion.rcp.tips;
 
-import java.util.HashSet;
+import static org.eclipse.recommenders.internal.completion.rcp.Constants.EXT_POINT_COMPLETION_TIPS;
+
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.RegistryFactory;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
@@ -25,68 +26,62 @@ import org.eclipse.recommenders.completion.rcp.IRecommendersCompletionContext;
 import org.eclipse.recommenders.completion.rcp.processable.SessionProcessor;
 import org.eclipse.recommenders.completion.rcp.tips.ICompletionTipProposal;
 import org.eclipse.recommenders.internal.completion.rcp.Constants;
-import org.osgi.service.prefs.BackingStoreException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.eclipse.recommenders.internal.completion.rcp.LogMessages;
+import org.eclipse.recommenders.utils.Logs;
 
-import com.google.common.base.Joiner;
-import com.google.common.base.Predicate;
-import com.google.common.base.Splitter;
-import com.google.common.collect.Collections2;
+import com.google.common.base.Optional;
 import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 
 public class TipsSessionProcessor extends SessionProcessor {
-
-    private static final Logger LOG = LoggerFactory.getLogger(TipsSessionProcessor.class);
-
-    private static final String PREF_NODE_ID_TIPS = "org.eclipse.recommenders.completion.rcp"; //$NON-NLS-1$
-    private static final String SEEN = "completion_tips_seen"; //$NON-NLS-1$
 
     private static final String COMPLETION_TIP_ID = "id"; //$NON-NLS-1$
     private static final String COMPLETION_TIP_CLASS = "class"; //$NON-NLS-1$
 
-    private final Map<ICompletionTipProposal, String> unseenTips = Maps.newHashMap();
-    private final HashSet<String> seenTips;
-
-    private boolean tipsSeen;
-
-    public TipsSessionProcessor() {
-        final IConfigurationElement[] elements = Platform.getExtensionRegistry().getConfigurationElementsFor(
-                Constants.EXT_POINT_COMPLETION_TIPS);
-
-        Iterable<String> split = Splitter.on(':').omitEmptyStrings().split(getTipsPreferences().get(SEEN, "")); //$NON-NLS-1$
-        seenTips = Sets.newHashSet(split);
-
-        for (final IConfigurationElement element : elements) {
-            String id = element.getAttribute(COMPLETION_TIP_ID);
-            if (!seenTips.contains(id)) {
-                try {
-                    ICompletionTipProposal proposal = (ICompletionTipProposal) element
-                            .createExecutableExtension(COMPLETION_TIP_CLASS);
-                    unseenTips.put(proposal, id);
-                } catch (CoreException e) {
-                    LOG.error("Cannot instantiate completion tip", e); //$NON-NLS-1$
-                }
-            }
-        }
-    }
+    private final Map<ICompletionTipProposal, String> proposalsToShow = Maps.newHashMap();
+    private final Map<String, Date> seenTips = Maps.newHashMap();
 
     @Override
     public boolean startSession(IRecommendersCompletionContext context) {
-        if (unseenTips.isEmpty()) {
-            return false;
-        }
-
         if (preventsAutoComplete(context)) {
             return false;
         }
 
-        for (ICompletionTipProposal tip : unseenTips.keySet()) {
-            tip.setCursorPosition(context.getInvocationOffset());
-        }
+        proposalsToShow.clear();
+        seenTips.clear();
 
-        return true;
+        initializeProposalsToShow(context.getInvocationOffset());
+
+        return !proposalsToShow.isEmpty();
+    }
+
+    private void initializeProposalsToShow(int cursorPosition) {
+        final IConfigurationElement[] elements = RegistryFactory.getRegistry().getConfigurationElementsFor(
+                EXT_POINT_COMPLETION_TIPS);
+        for (final IConfigurationElement element : elements) {
+            String id = element.getAttribute(COMPLETION_TIP_ID);
+            if (id == null) {
+                continue;
+            }
+
+            if (!getEnabledPreference(id)) {
+                continue;
+            }
+
+            ICompletionTipProposal proposal;
+            try {
+                proposal = (ICompletionTipProposal) element.createExecutableExtension(COMPLETION_TIP_CLASS);
+            } catch (CoreException e) {
+                Logs.log(LogMessages.LOG_ERROR_CANNOT_INSTANTIATE_COMPLETION_TIP_PROPOSAL, e, id);
+                continue;
+            }
+            proposal.setCursorPosition(cursorPosition);
+
+            boolean isApplicable = proposal.isApplicable(getLastSeenPreference(id).orNull());
+
+            if (isApplicable) {
+                proposalsToShow.put(proposal, id);
+            }
+        }
     }
 
     private boolean preventsAutoComplete(IRecommendersCompletionContext context) {
@@ -95,44 +90,32 @@ public class TipsSessionProcessor extends SessionProcessor {
 
     @Override
     public void endSession(List<ICompletionProposal> proposals) {
-        proposals.addAll(Collections2.filter(unseenTips.keySet(), new Predicate<ICompletionTipProposal>() {
-
-            @Override
-            public boolean apply(ICompletionTipProposal input) {
-                return input.isApplicable();
-            }
-        }));
-        tipsSeen = false;
+        proposals.addAll(proposalsToShow.keySet());
     }
 
     @Override
     public void selected(ICompletionProposal proposal) {
-        if (unseenTips.containsKey(proposal)) {
-            String id = unseenTips.remove(proposal);
-            seenTips.add(id);
-            tipsSeen = true;
+        if (proposalsToShow.containsKey(proposal)) {
+            String tipId = proposalsToShow.get(proposal);
+            Date now = new java.util.Date();
+            setLastSeenPreference(tipId, now);
         }
     }
 
-    @Override
-    public void aboutToClose() {
-        if (tipsSeen) {
-            persistSeenTips(seenTips);
-        }
+    private static boolean getEnabledPreference(String tipId) {
+        return getTipsPreferences().node("tips").node(tipId).getBoolean("enabled", true);
     }
 
-    private static void persistSeenTips(Set<String> seenTips) {
-        String joined = Joiner.on(':').join(seenTips);
-        IEclipsePreferences store = getTipsPreferences();
-        store.put(SEEN, joined);
-        try {
-            store.flush();
-        } catch (BackingStoreException e) {
-            LOG.error("Failed to flush preferences", e); //$NON-NLS-1$
-        }
+    private static Optional<Date> getLastSeenPreference(String tipId) {
+        long lastSeen = getTipsPreferences().node("tips").node(tipId).getLong("lastSeen", -1);
+        return Optional.fromNullable(lastSeen > 0 ? new Date(lastSeen) : null);
+    }
+
+    private static void setLastSeenPreference(String tipId, Date lastSeen) {
+        getTipsPreferences().node("tips").node(tipId).putLong("lastSeen", lastSeen.getTime());
     }
 
     private static IEclipsePreferences getTipsPreferences() {
-        return InstanceScope.INSTANCE.getNode(PREF_NODE_ID_TIPS);
+        return InstanceScope.INSTANCE.getNode(Constants.BUNDLE_NAME);
     }
 }
