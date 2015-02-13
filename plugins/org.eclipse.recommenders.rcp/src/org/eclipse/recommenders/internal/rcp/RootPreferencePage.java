efa28689682ffc3d6573e8e454b3d9635482a50a
Yasser Aziza <yasser.aziza@gmail.com>
2015-02-13 14:32:33 +0100

    [ui] Read root preference page contribution over extension points
    Change-Id: Ic8b968bcbbbba6a384e8525aff01699a41f4131b
    Signed-off-by: Yasser Aziza <yasser.aziza@gmail.com>

--- plugins/org.eclipse.recommenders.rcp/src/org/eclipse/recommenders/internal/rcp/RootPreferencePage.java
+++ plugins/org.eclipse.recommenders.rcp/src/org/eclipse/recommenders/internal/rcp/RootPreferencePage.java
@@ -8,42 +8,30 @@
  * Contributors:
  *    Marcel Bruch - initial API and implementation.
  *    Olav Lenz - externalize Strings.
+ *    Yasser Aziza - contribution link
  */
 package org.eclipse.recommenders.internal.rcp;
 
-import java.text.MessageFormat;
-
-import javax.inject.Inject;
+import static org.eclipse.recommenders.internal.rcp.Constants.PREF_PAGE_ID;
 
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.jface.preference.PreferencePage;
-import org.eclipse.recommenders.rcp.SharedImages;
-import org.eclipse.recommenders.rcp.SharedImages.Images;
-import org.eclipse.recommenders.rcp.utils.BrowserUtils;
-import org.eclipse.recommenders.rcp.utils.Dialogs;
+import org.eclipse.recommenders.internal.rcp.contribution.AbstractLinkContributionPage;
+import org.eclipse.recommenders.internal.rcp.contribution.ContributionLink;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Link;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPreferencePage;
 
-public class RootPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+public class RootPreferencePage extends AbstractLinkContributionPage {
 
-    private final SharedImages images;
-
-    @Inject
-    public RootPreferencePage(SharedImages images) {
-        this.images = images;
+    public RootPreferencePage() {
+        super(PREF_PAGE_ID);
     }
 
     @Override
-    public void init(final IWorkbench workbench) {
+    public void init(IWorkbench workbench) {
         setDescription(Messages.PREFPAGE_DESCRIPTION_EMPTY);
     }
 
@@ -55,43 +43,15 @@ public class RootPreferencePage extends PreferencePage implements IWorkbenchPref
         GridLayoutFactory.swtDefaults().margins(0, 5).applyTo(composite);
 
         Group group = new Group(composite, SWT.NONE);
-        group.setText("Useful links");
+        group.setText(Messages.PREFPAGE_LINKS_DESCRIPTION);
         GridDataFactory.fillDefaults().grab(true, false).applyTo(group);
         GridLayoutFactory.swtDefaults().numColumns(2).applyTo(group);
 
-        BrowserUtils.addOpenBrowserAction(createLink(group, Images.OBJ_HOMEPAGE, Messages.PREFPAGE_LINK_HOMEPAGE,
-                "http://www.eclipse.org/recommenders/")); //$NON-NLS-1$
+        for (ContributionLink link : getContributionLinks()) {
+            link.appendLink(group);
+        }
 
-        BrowserUtils.addOpenBrowserAction(createLink(group, Images.OBJ_CONTAINER, Messages.PREFPAGE_LINK_MANUAL,
-                "http://www.eclipse.org/recommenders/manual/")); //$NON-NLS-1$
-
-        BrowserUtils.addOpenBrowserAction(createLink(group, Images.OBJ_FAVORITE_STAR, Messages.PREFPAGE_LINK_FAVORITE,
-                "http://marketplace.eclipse.org/content/eclipse-code-recommenders")); //$NON-NLS-1$
-
-        BrowserUtils.addOpenBrowserAction(createLink(group, Images.OBJ_BIRD_BLUE, Messages.PREFPAGE_LINK_TWITTER,
-                "http://twitter.com/recommenders")); //$NON-NLS-1$
-
-        addOpenExtensionDiscoveryDialogAction(createLink(group, Images.OBJ_LIGHTBULB,
-                Messages.PREFPAGE_LINK_EXTENSIONS, "")); //$NON-NLS-1$
-
+        applyDialogFont(composite);
         return parent;
-    }
-
-    private Link createLink(Composite content, Images icon, String urlLabel, String url) {
-        Label label = new Label(content, SWT.BEGINNING);
-        label.setImage(images.getImage(icon));
-
-        Link link = new Link(content, SWT.BEGINNING);
-        link.setText(MessageFormat.format(urlLabel, url));
-        return link;
-    }
-
-    private void addOpenExtensionDiscoveryDialogAction(Link link) {
-        link.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-                Dialogs.newExtensionsDiscoveryDialog().open();
-            }
-        });
     }
 }
