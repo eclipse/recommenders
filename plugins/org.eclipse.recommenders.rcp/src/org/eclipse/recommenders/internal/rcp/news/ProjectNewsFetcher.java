65dde2d20c7b31c14dac1fb69b7b90647abc7246
Johannes Dorn <johannes.dorn@codetrails.com>
2015-02-16 14:10:22 +0100

    [news] Basic RSS reader fetching title and publishDates
    Change-Id: Iad2699a26fea449f6e00fe72f894704b6bdfccad
    Signed-off-by: Johannes Dorn <johannes.dorn@codetrails.com>

---
+++ plugins/org.eclipse.recommenders.rcp/src/org/eclipse/recommenders/internal/rcp/news/ProjectNewsFetcher.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2015 Codetrails GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Johannes Dorn - initial API and implementation.
+ */
+package org.eclipse.recommenders.internal.rcp.news;
+
+import static java.text.MessageFormat.format;
+import static org.eclipse.core.runtime.IStatus.WARNING;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.recommenders.internal.rcp.Constants;
+import org.eclipse.recommenders.net.Proxies;
+
+import com.google.common.base.Charsets;
+
+public class ProjectNewsFetcher extends Job {
+
+    private final URI feedUri;
+
+    private String feed;
+
+    public ProjectNewsFetcher(URI feedUri) {
+        super("Fetching project news feed");
+        this.feedUri = feedUri;
+    }
+
+    @Override
+    protected IStatus run(IProgressMonitor monitor) {
+        try {
+            Executor executor = Executor.newInstance();
+            Request request = Request.Get(feedUri);
+            Response response = Proxies.proxy(executor, feedUri).execute(request);
+            HttpResponse httpResponse = response.returnResponse();
+            int statusCode = httpResponse.getStatusLine().getStatusCode();
+            if (statusCode >= HttpStatus.SC_BAD_REQUEST) {
+                return new Status(WARNING, Constants.BUNDLE_ID, format("Could not access project news feed: {0}",
+                        httpResponse.getStatusLine().getReasonPhrase()));
+            }
+            HttpEntity entity = httpResponse.getEntity();
+
+            feed = IOUtils.toString(entity.getContent(), Charsets.UTF_8.name());
+            return Status.OK_STATUS;
+        } catch (IOException e) {
+            return new Status(WARNING, Constants.BUNDLE_ID, format("Could not access project news feed"), e);
+        }
+    }
+
+    public String getFeed() {
+        return feed;
+    }
+}
