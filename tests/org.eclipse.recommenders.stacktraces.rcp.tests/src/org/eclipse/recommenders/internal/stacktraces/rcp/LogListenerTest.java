7077adc130feee1aab1ef972ea8cf003838ef1ce
simonlaffoy <simon.laffoy@codetrails.com>
2015-02-17 23:42:09 +0100

    [stacktraces] Move/rename 'skip similar errors' in preference page
    Bug: 457935
    Change-Id: I965abc241abc7a5563f6fbf06b51d0a475ec22e3
    Signed-off-by: simonlaffoy <simon.laffoy@codetrails.com>

--- tests/org.eclipse.recommenders.stacktraces.rcp.tests/src/org/eclipse/recommenders/internal/stacktraces/rcp/LogListenerTest.java
+++ tests/org.eclipse.recommenders.stacktraces.rcp.tests/src/org/eclipse/recommenders/internal/stacktraces/rcp/LogListenerTest.java
@@ -244,7 +244,7 @@ public class LogListenerTest {
     @Test
     public void testUseHistory() {
         settings.setAction(SendAction.SILENT);
-        settings.setSkipSimilarErrors(true);
+        settings.setDeveloperMode(false);
         Status status = new Status(IStatus.ERROR, TEST_PLUGIN_ID, "test message");
 
         sut.logging(status, "");
@@ -265,7 +265,6 @@ public class LogListenerTest {
     @Test
     public void testCheckIfAskMode() {
         settings.setAction(ASK);
-        settings.setSkipSimilarErrors(true);
         Status status = new Status(IStatus.ERROR, TEST_PLUGIN_ID, "test message");
 
         sut.logging(status, "");
@@ -316,21 +315,8 @@ public class LogListenerTest {
     }
 
     @Test
-    public void testSkipSimilarErrors() {
-        settings.setSkipSimilarErrors(true);
-        settings.setAction(ASK);
-
-        Status s1 = createErrorStatus();
-        Status s2 = createErrorStatus();
-        sut.logging(s1, "");
-        sut.logging(s2, "");
-
-        verify(sut, times(1)).checkAndSendWithDialog(Mockito.any(ErrorReport.class));
-    }
-
-    @Test
-    public void testNoSkippingSimilarErrors() {
-        settings.setSkipSimilarErrors(false);
+    public void testDeveloperModeSilent() {
+        settings.setDeveloperMode(true);
         settings.setAction(SILENT);
 
         sut.logging(createErrorStatus(), "");
@@ -341,15 +327,11 @@ public class LogListenerTest {
 
     @Test
     public void testSilentSendsErrors() {
-
-        settings.setSkipSimilarErrors(false);
         settings.setAction(SILENT);
 
         sut.logging(createErrorStatus(), "");
-        sut.logging(createErrorStatus(), "");
 
-        verify(sut, times(2)).sendStatus(Mockito.any(ErrorReport.class));
-
+        verify(sut, times(1)).sendStatus(Mockito.any(ErrorReport.class));
     }
 
     @Test
