6a4641c03a84e6f12446de97dfddc861b0b8d6ee
Yasser Aziza <yasser.aziza@gmail.com>
2015-03-05 15:25:48 +0100

    [tests] Bug 461269 - Unit-test reading linkContribution extension point
    Change-Id: I7f37d6378ab722799257a0c1a6a62e3b0e21d92c
    Signed-off-by: Yasser Aziza <yasser.aziza@gmail.com>

---
+++ plugins/org.eclipse.recommenders.rcp/src/org/eclipse/recommenders/internal/rcp/links/ContributionExtensionReader.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2015 Codetrails GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Yasser Aziza - initial API and implementation.
+ */
+package org.eclipse.recommenders.internal.rcp.links;
+
+import static org.eclipse.recommenders.internal.rcp.LogMessages.LOG_ERROR_FAILED_TO_READ_EXTENSION_ELEMENT;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.recommenders.utils.Logs;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+
+public class ContributionExtensionReader {
+
+    private final String PREF_CONTRIBUTION_ID = "org.eclipse.recommenders.rcp.linkContribution"; //$NON-NLS-1$
+    private final String CONTRIBUTION_ELEMENT = "linkContribution"; //$NON-NLS-1$
+
+    private final String PREF_PAGE_ID_ATTRIBUTE = "preferencePageId"; //$NON-NLS-1$
+    private final String LABEL_ATTRIBUTE = "label"; //$NON-NLS-1$
+    private final String COMMAND_ID_ATTRIBUTE = "commandId"; //$NON-NLS-1$
+    private final String PRIORITY_ATTRIBUTE = "priority"; //$NON-NLS-1$
+    private final String ICON_ELEMENT = "icon"; //$NON-NLS-1$
+
+    private final String preferencePageId;
+
+    List<ContributionLink> links;
+
+    public ContributionExtensionReader(String preferencePageId) {
+        this.preferencePageId = preferencePageId;
+        readContributionLinks();
+    }
+
+    public List<ContributionLink> getContributionLinks() {
+        return links;
+    }
+
+    private void readContributionLinks() {
+        final IConfigurationElement[] configurationElements = Platform.getExtensionRegistry()
+                .getConfigurationElementsFor(PREF_CONTRIBUTION_ID);
+        readContributionLinks(configurationElements);
+    }
+
+    @VisibleForTesting
+    void readContributionLinks(IConfigurationElement... configurationElements) {
+        List<ContributionLink> links = this.links = Lists.newArrayList();
+
+        if (configurationElements == null) {
+            return;
+        }
+
+        for (final IConfigurationElement configurationElement : configurationElements) {
+            if (CONTRIBUTION_ELEMENT.equals(configurationElement.getName())) {
+                final String pluginId = configurationElement.getContributor().getName();
+                final String pageId = configurationElement.getAttribute(PREF_PAGE_ID_ATTRIBUTE);
+                if (!preferencePageId.equals(pageId)) {
+                    continue;
+                }
+                final String label = configurationElement.getAttribute(LABEL_ATTRIBUTE);
+                final String commandId = configurationElement.getAttribute(COMMAND_ID_ATTRIBUTE);
+                final String priority = configurationElement.getAttribute(PRIORITY_ATTRIBUTE);
+                final String icon = configurationElement.getAttribute(ICON_ELEMENT);
+
+                try {
+                    Image image = null;
+                    if (icon != null) {
+                        image = AbstractUIPlugin.imageDescriptorFromPlugin(pluginId, icon).createImage();
+                    }
+
+                    if (isValidAttribute(label) && isValidAttribute(commandId)) {
+                        links.add(new ContributionLink(label, commandId, priority, image));
+                    } else {
+                        Logs.log(LOG_ERROR_FAILED_TO_READ_EXTENSION_ELEMENT, CONTRIBUTION_ELEMENT);
+                    }
+                } catch (Exception e) {
+                    Logs.log(LOG_ERROR_FAILED_TO_READ_EXTENSION_ELEMENT, CONTRIBUTION_ELEMENT);
+                }
+            }
+        }
+        Collections.sort(links);
+        this.links = links;
+    }
+
+    private boolean isValidAttribute(String attribute) {
+        return !Strings.isNullOrEmpty(attribute);
+    }
+}
---
+++ tests/org.eclipse.recommenders.rcp.tests/src/org/eclipse/recommenders/internal/rcp/links/ContributionExtensionReaderTest.java
@@ -0,0 +1,183 @@
+/**
+ * Copyright (c) 2015 Codetrails GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Yasser Aziza - initial API and implementation.
+ */
+package org.eclipse.recommenders.internal.rcp.links;
+
+import static com.google.common.collect.Iterables.getOnlyElement;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IContributor;
+import org.eclipse.ui.internal.SharedImages;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+public class ContributionExtensionReaderTest {
+
+    private final String CONTRIBUTION_ELEMENT = "linkContribution";
+    private final String CONTRIBUTOR_ID = "org.eclipse.recommenders.rcp";
+
+    private final String PREF_PAGE_ID_ATTRIBUTE = "preferencePageId";
+    private final String LABEL_ATTRIBUTE = "label";
+    private final String COMMAND_ID_ATTRIBUTE = "commandId";
+    private final String PRIORITY_ATTRIBUTE = "priority";
+    private final String ICON_ELEMENT = "icon";
+
+    private IConfigurationElement mockConfigElement(String name, ImmutableMap<String, String> map) {
+        IConfigurationElement element = mock(IConfigurationElement.class);
+        IContributor contributor = mock(IContributor.class);
+
+        when(element.getName()).thenReturn(name);
+
+        for (String key : map.keySet()) {
+            when(element.getAttribute(key)).thenReturn(map.get(key));
+        }
+        when(contributor.getName()).thenReturn(CONTRIBUTOR_ID);
+        when(element.getContributor()).thenReturn(contributor);
+        return element;
+    }
+
+    @Test
+    public void testNoExtensionsFound() {
+        ContributionExtensionReader sut = new ContributionExtensionReader("some.pref.page.id");
+
+        IConfigurationElement[] configElements = null;
+        sut.readContributionLinks(configElements);
+        List<ContributionLink> links = sut.getContributionLinks();
+
+        assertThat(links.size(), is(0));
+
+        sut.readContributionLinks(new IConfigurationElement[] {});
+
+        assertThat(links.size(), is(0));
+    }
+
+    @Test
+    public void testLinkExtensionOptionalAttributes() {
+        IConfigurationElement configElement = mockConfigElement(CONTRIBUTION_ELEMENT, ImmutableMap.of(
+                PREF_PAGE_ID_ATTRIBUTE, "some.pref.page.id", LABEL_ATTRIBUTE, "<a>some Label</a>",
+                COMMAND_ID_ATTRIBUTE, "some.command.id"));
+
+        ContributionExtensionReader sut = new ContributionExtensionReader("some.pref.page.id");
+        sut.readContributionLinks(configElement);
+
+        List<ContributionLink> links = sut.getContributionLinks();
+
+        assertThat(links.size(), is(1));
+
+        ContributionLink link = getOnlyElement(links);
+
+        assertThat(link.getPriority(), is(Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void testLinkExtensionDefaultPriority() {
+        IConfigurationElement configElement = mockConfigElement(CONTRIBUTION_ELEMENT, ImmutableMap.of(
+                PREF_PAGE_ID_ATTRIBUTE, "some.pref.page.id", LABEL_ATTRIBUTE, "<a>some Label</a>",
+                COMMAND_ID_ATTRIBUTE, "some.command.id", ICON_ELEMENT, SharedImages.IMG_OBJ_ELEMENT));
+
+        ContributionExtensionReader sut = new ContributionExtensionReader("some.pref.page.id");
+        sut.readContributionLinks(configElement);
+
+        List<ContributionLink> links = sut.getContributionLinks();
+
+        assertThat(links.size(), is(1));
+
+        ContributionLink link = getOnlyElement(links);
+
+        assertThat(link.getPriority(), is(Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void testLinkExtensionInvalidPriority() {
+        IConfigurationElement firstConfigElement = mockConfigElement(CONTRIBUTION_ELEMENT, ImmutableMap.of(
+                PREF_PAGE_ID_ATTRIBUTE, "some.pref.page.id", LABEL_ATTRIBUTE, "<a>some Label</a>",
+                COMMAND_ID_ATTRIBUTE, "some.command.id", PRIORITY_ATTRIBUTE, "invalid", ICON_ELEMENT,
+                SharedImages.IMG_OBJ_ELEMENT));
+
+        IConfigurationElement secondConfigElement = mockConfigElement(CONTRIBUTION_ELEMENT, ImmutableMap.of(
+                PREF_PAGE_ID_ATTRIBUTE, "some.pref.page.id", LABEL_ATTRIBUTE, "<a>other Label</a>",
+                COMMAND_ID_ATTRIBUTE, "other.command.id", PRIORITY_ATTRIBUTE, "10"));
+
+        ContributionExtensionReader sut = new ContributionExtensionReader("some.pref.page.id");
+        sut.readContributionLinks(firstConfigElement, secondConfigElement);
+
+        List<ContributionLink> links = sut.getContributionLinks();
+
+        assertThat(links.size(), is(1));
+
+        ContributionLink link = getOnlyElement(links);
+
+        assertThat(link.getPriority(), is(10));
+    }
+
+    @Test
+    public void testLinkExtensionWithNonExistentIcon() {
+        IConfigurationElement configElement = mockConfigElement(CONTRIBUTION_ELEMENT, ImmutableMap.of(
+                PREF_PAGE_ID_ATTRIBUTE, "some.pref.page.id", LABEL_ATTRIBUTE, "<a>some Label</a>",
+                COMMAND_ID_ATTRIBUTE, "some.command.id", ICON_ELEMENT, "invalid"));
+
+        ContributionExtensionReader sut = new ContributionExtensionReader("some.pref.page.id");
+        sut.readContributionLinks(configElement);
+
+        List<ContributionLink> links = sut.getContributionLinks();
+
+        assertThat(links.size(), is(0));
+    }
+
+    @Test
+    public void testMultipleLinksContribution() {
+        IConfigurationElement firstConfigElement = mockConfigElement(CONTRIBUTION_ELEMENT, ImmutableMap.of(
+                PREF_PAGE_ID_ATTRIBUTE, "some.pref.page.id", LABEL_ATTRIBUTE, "<a>first Label</a>",
+                COMMAND_ID_ATTRIBUTE, "first.command.id", PRIORITY_ATTRIBUTE, "0", ICON_ELEMENT,
+                SharedImages.IMG_OBJ_ELEMENT));
+
+        IConfigurationElement secondConfigElement = mockConfigElement(CONTRIBUTION_ELEMENT, ImmutableMap.of(
+                PREF_PAGE_ID_ATTRIBUTE, "some.pref.page.id", LABEL_ATTRIBUTE, "<a>second Label</a>",
+                COMMAND_ID_ATTRIBUTE, "second.command.id", PRIORITY_ATTRIBUTE, "0", ICON_ELEMENT,
+                SharedImages.IMG_OBJ_ELEMENT));
+
+        ContributionExtensionReader firstPrefPageContribution = new ContributionExtensionReader("some.pref.page.id");
+        firstPrefPageContribution.readContributionLinks(firstConfigElement, secondConfigElement);
+        List<ContributionLink> links = firstPrefPageContribution.getContributionLinks();
+
+        assertThat(links.size(), is(2));
+    }
+
+    @Test
+    public void testLinkContributionForDifferentPreferencePages() {
+        IConfigurationElement firstConfigElement = mockConfigElement(CONTRIBUTION_ELEMENT, ImmutableMap.of(
+                PREF_PAGE_ID_ATTRIBUTE, "first.pref.page.id", LABEL_ATTRIBUTE, "<a>first Label</a>",
+                COMMAND_ID_ATTRIBUTE, "first.command.id", PRIORITY_ATTRIBUTE, "0", ICON_ELEMENT,
+                SharedImages.IMG_OBJ_ELEMENT));
+
+        IConfigurationElement secondConfigElement = mockConfigElement(CONTRIBUTION_ELEMENT, ImmutableMap.of(
+                PREF_PAGE_ID_ATTRIBUTE, "second.pref.page.id", LABEL_ATTRIBUTE, "<a>second Label</a>",
+                COMMAND_ID_ATTRIBUTE, "second.command.id", PRIORITY_ATTRIBUTE, "1", ICON_ELEMENT,
+                SharedImages.IMG_OBJ_ELEMENT));
+
+        ContributionExtensionReader firstPrefPageContribution = new ContributionExtensionReader("first.pref.page.id");
+        firstPrefPageContribution.readContributionLinks(firstConfigElement, secondConfigElement);
+        List<ContributionLink> links = firstPrefPageContribution.getContributionLinks();
+
+        assertThat(links.size(), is(1));
+
+        ContributionExtensionReader secondPrefPageContribution = new ContributionExtensionReader("second.pref.page.id");
+        secondPrefPageContribution.readContributionLinks(firstConfigElement, secondConfigElement);
+        links = secondPrefPageContribution.getContributionLinks();
+
+        assertThat(links.size(), is(1));
+    }
+}
