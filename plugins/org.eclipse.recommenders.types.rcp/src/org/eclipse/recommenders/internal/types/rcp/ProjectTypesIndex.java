c342d6c48ec0080a24dc7ccf27faa99ae020822a
Marcel Bruch <marcel.bruch@codetrails.com>
2015-04-21 08:18:33 +0200

    [types] Trigger reindexing after completion events only.
    Classpath changes occurr more often than expected and devs work in many
    projects. Thus there are many indexes open at a time. Reindexing all of
    them on classpath change leads to high cpu cost. This change computes
    the updates only after a code completion was triggered.
    
    We risk to make wrong recommendations once but that's okay for this use
    case IMHO.

--- plugins/org.eclipse.recommenders.types.rcp/src/org/eclipse/recommenders/internal/types/rcp/ProjectTypesIndex.java
+++ plugins/org.eclipse.recommenders.types.rcp/src/org/eclipse/recommenders/internal/types/rcp/ProjectTypesIndex.java
@@ -230,7 +230,7 @@ public class ProjectTypesIndex extends AbstractIdleService {
         try {
             return subtypes(Names.vm2srcQualifiedType(expected), prefix);
         } catch (Exception e) {
-            // temporary workaround for 
+            // temporary workaround for
             // https://bugs.eclipse.org/bugs/show_bug.cgi?id=464925
             log(LogMessages.ERROR_ACCESSING_SEARCHINDEX_FAILED, e);
             return ImmutableSet.of();
@@ -242,6 +242,12 @@ public class ProjectTypesIndex extends AbstractIdleService {
         if (!isRunning() || isBlank(type)) {
             return b.build();
         }
+
+        if (isRebuildAfterNextAccess()) {
+            setRebuildAfterNextAccess(false);
+            rebuild();
+        }
+
         IndexSearcher searcher = getSearcher();
         BooleanQuery query = new BooleanQuery();
         query.add(new TermQuery(new Term(F_INSTANCEOF, type)), Occur.MUST);
@@ -298,6 +304,8 @@ public class ProjectTypesIndex extends AbstractIdleService {
     }
 
     private JobFuture active = null;
+
+    private boolean rebuildAfterNextAccess;
 
     public ListenableFuture<IStatus> rebuild() {
         if (!(active == null || active.isDone() || active.isCancelled())) {
@@ -570,5 +578,13 @@ public class ProjectTypesIndex extends AbstractIdleService {
             return false;
         }
 
+    }
+
+    public void setRebuildAfterNextAccess(boolean value) {
+        rebuildAfterNextAccess = value;
+    }
+
+    public boolean isRebuildAfterNextAccess() {
+        return rebuildAfterNextAccess;
     }
 }
