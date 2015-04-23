7b00c97571d322d1a1435694ed81323c41046b64
PCNowak <siersciotluk@gmail.com>
2015-04-23 12:19:05 +0200

    Fixed strings to match Eclipse naming convetions, added valid license.
    Change-Id: I0691947ab65ea250c2b9f11430a9e72b94b319c7
    Signed-off-by: PCNowak <siersciotluk@gmail.com>

--- plugins/org.eclipse.recommenders.news.rcp/src/org/eclipse/recommenders/internal/news/rcp/Messages.java
+++ plugins/org.eclipse.recommenders.news.rcp/src/org/eclipse/recommenders/internal/news/rcp/Messages.java
@@ -1,13 +1,10 @@
 /**
- * Copyright (c) 2010, 2012 Darmstadt University of Technology.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Olav Lenz - externalize Strings.
- */
+* Copyright (c) 2015 Pawel Nowak.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*/
 package org.eclipse.recommenders.internal.news.rcp;
 
 import org.eclipse.osgi.util.NLS;
