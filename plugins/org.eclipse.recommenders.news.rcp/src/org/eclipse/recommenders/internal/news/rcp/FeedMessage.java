8555f8a71b68bcb291f9c7fa37c9b41d11fdbb9c
PCNowak <siersciotluk@gmail.com>
2015-05-04 02:55:32 +0200

    [news] implemented rss service
    Change-Id: I6749337a1c731d2e3e2757f1fd94af651e38b47c
    Signed-off-by: PCNowak <siersciotluk@gmail.com>
    Signed-off-by: Johannes Dorn <johannes.dorn@codetrails.com>

---
+++ plugins/org.eclipse.recommenders.news.rcp/src/org/eclipse/recommenders/internal/news/rcp/FeedMessage.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2015 Codetrails GmbH. All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Johannes Dorn - initial API and implementation.
+ */
+package org.eclipse.recommenders.internal.news.rcp;
+
+import java.net.URL;
+import java.util.Date;
+
+import org.eclipse.recommenders.news.rcp.IFeedMessage;
+
+public class FeedMessage implements IFeedMessage {
+
+    @Override
+    public String getId() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Date getDate() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public URL getUrl() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
