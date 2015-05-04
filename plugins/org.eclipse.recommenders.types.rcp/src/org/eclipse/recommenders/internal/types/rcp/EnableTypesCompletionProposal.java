f20a18d188b343093f4751d087b7bf8aef89f12d
Simon Laffoy <simon.laffoy@codetrails.com>
2015-05-04 12:12:18 +0200

    [completion] In Content Assist, prompt user to enable Types Completion
    Bug: 466277
    Change-Id: Ia20719620ceab4a6f66a656c6da377eb2bd0fc42
    Signed-off-by: Simon Laffoy <simon.laffoy@codetrails.com>

--- plugins/org.eclipse.recommenders.types.rcp/src/org/eclipse/recommenders/internal/types/rcp/EnableTypesCompletionProposal.java
+++ plugins/org.eclipse.recommenders.types.rcp/src/org/eclipse/recommenders/internal/types/rcp/EnableTypesCompletionProposal.java
@@ -1,24 +1,16 @@
-/**
- * Copyright (c) 2010, 2014 Darmstadt University of Technology. All rights reserved. This program and the accompanying
- * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors: Johannes Dorn - initial API and implementation.
- */
-package org.eclipse.recommenders.internal.subwords.rcp;
+package org.eclipse.recommenders.internal.types.rcp;
 
-import static org.eclipse.jdt.internal.ui.JavaPlugin.getActiveWorkbenchShell;
 import static org.eclipse.jface.viewers.StyledString.DECORATIONS_STYLER;
-import static org.eclipse.recommenders.internal.subwords.rcp.Constants.*;
-import static org.eclipse.recommenders.rcp.utils.PreferencesHelper.createLinkLabelToPreferencePage;
-import static org.eclipse.ui.dialogs.PreferencesUtil.createPreferenceDialogOn;
+import static org.eclipse.recommenders.internal.types.rcp.Constants.SESSION_PROCESSOR_ID;
 
 import java.text.MessageFormat;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.text.AbstractInformationControl;
 import org.eclipse.jface.text.IInformationControl;
@@ -31,6 +23,7 @@ import org.eclipse.recommenders.internal.completion.rcp.CompletionRcpPreferences
 import org.eclipse.recommenders.rcp.SharedImages;
 import org.eclipse.recommenders.rcp.SharedImages.Images;
 import org.eclipse.recommenders.rcp.utils.BrowserUtils;
+import org.eclipse.recommenders.utils.names.ITypeName;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -42,25 +35,23 @@ import org.eclipse.swt.widgets.Shell;
 import com.google.common.collect.ImmutableList;
 
 @SuppressWarnings("restriction")
-public class EnableSubwordsCompletionProposal extends AbstractCompletionTipProposal {
+public class EnableTypesCompletionProposal extends AbstractCompletionTipProposal {
 
     private static final List<SessionProcessorDescriptor> NONE = Collections.<SessionProcessorDescriptor>emptyList();
-    private static final String ABOUT_PREFERENCES = "about:preferences"; //$NON-NLS-1$
     private static final String ABOUT_ENABLE = "about:enable"; //$NON-NLS-1$
     private static final String HTTP_MANUAL = "https://www.eclipse.org/recommenders/manual/#completion-engines"; //$NON-NLS-1$
-    private static final String PAGE_NAME = createLinkLabelToPreferencePage(SUBWORDS_COMPLETION_PREFERENCE_PAGE_ID);
 
-    private static final String INFO = MessageFormat.format(Messages.PROPOSAL_TOOLTIP_ENABLE_SUBWORDS_COMPLETION,
-            PAGE_NAME, ABOUT_PREFERENCES, ABOUT_ENABLE, HTTP_MANUAL);
+    private static final String INFO = MessageFormat.format(Messages.PROPOSAL_TOOLTIP_ENABLE_TYPES_COMPLETION,
+            ABOUT_ENABLE, HTTP_MANUAL);
 
     private final CompletionRcpPreferences preferences;
 
     @Inject
-    public EnableSubwordsCompletionProposal(SharedImages images, CompletionRcpPreferences completionPreferences) {
+    public EnableTypesCompletionProposal(SharedImages images, CompletionRcpPreferences completionPreferences) {
         this.preferences = completionPreferences;
         Image image = images.getImage(Images.OBJ_LIGHTBULB);
         setImage(image);
-        StyledString text = new StyledString(Messages.PROPOSAL_LABEL_ENABLE_SUBWORDS_COMPLETION, DECORATIONS_STYLER);
+        StyledString text = new StyledString(Messages.PROPOSAL_LABEL_ENABLE_TYPES_COMPLETION, DECORATIONS_STYLER);
         setStyledDisplayString(text);
         setSortString(text.getString());
     }
@@ -68,19 +59,32 @@ public class EnableSubwordsCompletionProposal extends AbstractCompletionTipPropo
     @Override
     public boolean isApplicable(IRecommendersCompletionContext context) {
         SessionProcessorDescriptor descriptor = preferences.getSessionProcessorDescriptor(SESSION_PROCESSOR_ID);
-        return descriptor != null && !preferences.isEnabled(descriptor);
+        if (descriptor == null) {
+            return false;
+        }
+
+        if (preferences.isEnabled(descriptor)) {
+            return false;
+        }
+
+        Set<ITypeName> expectedTypes = context.getExpectedTypeNames();
+        if (expectedTypes.isEmpty()) {
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public void apply(ITextViewer viewer, char trigger, int stateMask, int offset) {
+        enableTypesCompletion();
+    }
+
+    private void enableTypesCompletion() {
         SessionProcessorDescriptor descriptor = preferences.getSessionProcessorDescriptor(SESSION_PROCESSOR_ID);
         if (descriptor != null) {
             preferences.setSessionProcessorEnabled(ImmutableList.of(descriptor), NONE);
         }
-    }
-
-    private void openPreferencePage() {
-        createPreferenceDialogOn(getActiveWorkbenchShell(), SUBWORDS_COMPLETION_PREFERENCE_PAGE_ID, null, null).open();
     }
 
     @Override
@@ -112,14 +116,8 @@ public class EnableSubwordsCompletionProposal extends AbstractCompletionTipPropo
                 public void widgetSelected(SelectionEvent e) {
                     dispose();
                     if (ABOUT_ENABLE.equals(e.text)) {
-                        SessionProcessorDescriptor descriptor = preferences
-                                .getSessionProcessorDescriptor(SESSION_PROCESSOR_ID);
-                        if (descriptor != null) {
-                            preferences.setSessionProcessorEnabled(ImmutableList.of(descriptor), NONE);
-                        }
-                    } else if (ABOUT_PREFERENCES.equals(e.text)) {
-                        openPreferencePage();
-                    } else if (HTTP_MANUAL.equals(e.text)) {
+                        enableTypesCompletion();
+                    } else if (StringUtils.startsWith(e.text, "http:") || StringUtils.startsWith(e.text, "https:")) {
                         BrowserUtils.openInExternalBrowser(e.text);
                     }
                 }
