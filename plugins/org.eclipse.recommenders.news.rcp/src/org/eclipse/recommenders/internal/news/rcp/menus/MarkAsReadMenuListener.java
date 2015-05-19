fc2d6182de6558df3bcc315a0a9711e9ab50c5a1
Pawel Nowak <siersciotluk@gmail.com>
2015-05-19 12:25:43 +0200

    [news] Implemented menu
    Change-Id: I5f0c41a63d0555eb6b77f2d14523f876ab9612eb
    Signed-off-by: Pawel Nowak <siersciotluk@gmail.com>

---
+++ plugins/org.eclipse.recommenders.news.rcp/src/org/eclipse/recommenders/internal/news/rcp/menus/MarkAsReadMenuListener.java
@@ -0,0 +1,49 @@
+package org.eclipse.recommenders.internal.news.rcp.menus;
+
+import static org.eclipse.recommenders.internal.news.rcp.FeedEvents.createFeedMessageReadEvent;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.recommenders.internal.news.rcp.FeedDescriptor;
+import org.eclipse.recommenders.internal.news.rcp.Messages;
+import org.eclipse.recommenders.news.rcp.IFeedMessage;
+
+import com.google.common.collect.Maps;
+import com.google.common.eventbus.EventBus;
+
+public class MarkAsReadMenuListener implements IMenuListener {
+
+    private final EventBus eventBus;
+    private Map<FeedDescriptor, List<IFeedMessage>> messages = Maps.newHashMap();
+
+    public MarkAsReadMenuListener(EventBus eventBus) {
+        super();
+        this.eventBus = eventBus;
+    }
+
+    @Override
+    public void menuAboutToShow(IMenuManager manager) {
+        Action action = new Action() {
+
+            @Override
+            public void run() {
+                for (Entry<FeedDescriptor, List<IFeedMessage>> entry : messages.entrySet()) {
+                    for (final IFeedMessage message : entry.getValue()) {
+                        eventBus.post(createFeedMessageReadEvent(message.getId()));
+                    }
+                }
+            }
+        };
+        action.setText(Messages.LABEL_MARK_AS_READ);
+        manager.add(action);
+    }
+
+    public void setMessages(Map<FeedDescriptor, List<IFeedMessage>> messages) {
+        this.messages = messages;
+    }
+}
