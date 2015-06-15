b77a9fa470d0f9a229d7a3de1480aa03193fb1a9
Pawel Nowak <siersciotluk@gmail.com>
2015-06-15 18:00:04 +0200

    [news] enabling/disabling a feed takes effect immediately
    Bug: 469683 
    Change-Id: Ia1fe4bdd8b173e84332cd601b652b705167085f0
    Signed-off-by: Johannes Dorn <johannes.dorn@codetrails.com>
    Signed-off-by: Pawel Nowak <siersciotluk@gmail.com>

--- tests/org.eclipse.recommenders.news.rcp.tests/src/org/eclipse/recommenders/internal/news/rcp/PollFeedJobTest.java
+++ tests/org.eclipse.recommenders.news.rcp.tests/src/org/eclipse/recommenders/internal/news/rcp/PollFeedJobTest.java
@@ -7,14 +7,17 @@
  */
 package org.eclipse.recommenders.internal.news.rcp;
 
+import static org.eclipse.recommenders.internal.news.rcp.TestUtils.enabled;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
 import java.net.MalformedURLException;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.Before;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Status;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -22,27 +25,33 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class PollFeedJobTest {
 
-    private static final String FIRST_ELEMENT = "first";
-    private static final String SECOND_ELEMENT = "second";
-
-    private Set<FeedDescriptor> feeds;
-
-    @Before
-    public void setup() {
-        feeds = new HashSet<>();
+    @Test
+    public void testJobBelongsToGivenTheSameFamily() throws MalformedURLException {
+        Set<FeedDescriptor> feeds = new HashSet<>();
+        feeds.add(enabled("test"));
+        assertThat(new PollFeedJob(feeds).belongsTo(Constants.POLL_FEED_JOB_FAMILY), is(true));
     }
 
     @Test
-    public void testFeedsWithSameIdBelongsTo() throws MalformedURLException {
-        assertThat(new PollFeedJob(FIRST_ELEMENT, feeds).belongsTo(new PollFeedJob(FIRST_ELEMENT, feeds)), is(true));
-        assertThat(new PollFeedJob(FIRST_ELEMENT, feeds).belongsTo(new PollFeedJob(FIRST_ELEMENT, feeds)), is(true));
+    public void testJobBelongsToGivenDifferentFamily() throws MalformedURLException {
+        Set<FeedDescriptor> feeds = new HashSet<>();
+        feeds.add(enabled("test"));
+        assertThat(new PollFeedJob(feeds).belongsTo("rndm"), is(false));
     }
 
     @Test
-    public void testFeedsWithDifferentIdDoesntBelongsTo() {
-        PollFeedJob firstJob = new PollFeedJob(FIRST_ELEMENT, feeds);
-        PollFeedJob secondJob = new PollFeedJob(SECOND_ELEMENT, feeds);
-        assertThat(firstJob.belongsTo(secondJob), is(false));
-        assertThat(secondJob.belongsTo(firstJob), is(false));
+    public void testJobWillBeCanceledIfPreferencesAreDisabled() {
+        IProgressMonitor monitor = mock(IProgressMonitor.class);
+        NewsRcpPreferences preferences = mock(NewsRcpPreferences.class);
+        Set<FeedDescriptor> feeds = new HashSet<>();
+        FeedDescriptor feed = mock(FeedDescriptor.class);
+        feeds.add(feed);
+        when(preferences.isEnabled()).thenReturn(false);
+        when(monitor.isCanceled()).thenReturn(true);
+
+        PollFeedJob sut = new PollFeedJob(feeds);
+
+        assertThat(sut.run(monitor), is(Status.CANCEL_STATUS));
+        verifyZeroInteractions(feed);
     }
 }
