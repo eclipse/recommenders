d97378859897e0cd61c4f8f3483fd7095e3d1c41
Pawel Nowak <siersciotluk@gmail.com>
2015-06-23 15:46:19 +0200

    [news] Utils tests
    Change-Id: I5a759752d9ea54bfa558b369fa5c10e82fabf92d
    Signed-off-by: Pawel Nowak <siersciotluk@gmail.com>

--- plugins/org.eclipse.recommenders.news.rcp/src/org/eclipse/recommenders/internal/news/rcp/MessageUtils.java
+++ plugins/org.eclipse.recommenders.news.rcp/src/org/eclipse/recommenders/internal/news/rcp/MessageUtils.java
@@ -19,7 +19,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
-public class Utils {
+public class MessageUtils {
 
     public static boolean containsUnreadMessages(Map<FeedDescriptor, List<IFeedMessage>> map) {
         if (map == null) {
