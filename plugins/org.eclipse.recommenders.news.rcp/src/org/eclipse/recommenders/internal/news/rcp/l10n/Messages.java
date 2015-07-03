5127712e55d6522b76a6380e0da9750914aa13c4
Pawel Nowak <siersciotluk@gmail.com>
2015-07-03 12:07:50 +0200

    [news] changed messages entries prefixes, added prefix for feed entries
    Bug: 471424
    Change-Id: I04c6a81b56b9dc51e202d7bd0ce59e7ede3eb1ed
    Signed-off-by: Pawel Nowak <siersciotluk@gmail.com>

--- plugins/org.eclipse.recommenders.news.rcp/src/org/eclipse/recommenders/internal/news/rcp/l10n/Messages.java
+++ plugins/org.eclipse.recommenders.news.rcp/src/org/eclipse/recommenders/internal/news/rcp/l10n/Messages.java
@@ -43,6 +43,8 @@ public final class Messages extends NLS {
 
     public static String NOTIFICATION_TITLE;
     public static String UNREAD_MESSAGE_PREFIX;
+    public static String READ_MESSAGE_PREFIX;
+    public static String PARTIALLY_READ_FEED;
 
     public static String LOG_ERROR_READING_PROPERTIES;
     public static String LOG_ERROR_WRITING_PROPERTIES;
