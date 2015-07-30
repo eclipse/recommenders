dad588f74135ee23c12a726aaf6617659281cdbb
Pawel Nowak <siersciotluk@gmail.com>
2015-07-30 20:59:11 +0200

    [news] removed recommenders dependencies
    Bug: 473759
    Change-Id: If126755e1691350724853c92f80148231aafd2d8
    Signed-off-by: Pawel Nowak <siersciotluk@gmail.com>

--- tests/org.eclipse.recommenders.news.rcp.uitests/src/org/eclipse/recommenders/internal/news/rcp/NewsPreferencePageUITest.java
+++ tests/org.eclipse.recommenders.news.rcp.uitests/src/org/eclipse/recommenders/internal/news/rcp/NewsPreferencePageUITest.java
@@ -176,6 +176,13 @@ public class NewsPreferencePageUITest {
     }
 
     @Test
+    public void testNotificationLinkLeadsToProperPreferencePage() {
+        bot.link().click();
+
+        assertThat(bot.checkBox("Enable notifications"), is(notNullValue())); //$NON-NLS-1$
+    }
+
+    @Test
     public void testDisableNewsFeed() {
         bot.checkBox(Messages.FIELD_LABEL_NEWS_ENABLED).click();
         applyChangesAndReopenPreferencePage();
@@ -210,20 +217,6 @@ public class NewsPreferencePageUITest {
         applyChangesAndReopenPreferencePage();
 
         assertThat(bot.table().rowCount(), is(equalTo(2)));
-    }
-
-    @Test
-    public void testNotificationLinkLeadsToProperPreferencePage() {
-        bot.link().click();
-
-        assertThat(bot.checkBox("Enable notifications"), is(notNullValue())); //$NON-NLS-1$
-    }
-
-    @Test
-    public void testWebBrowserSettingsinkLeadsToProperPreferencePage() {
-        bot.link(1).click("Web Browser");
-
-        assertThat(bot.radio("Use internal web browser"), is(notNullValue())); //$NON-NLS-1$
     }
 
     private static void openPreferencePage(SWTWorkbenchBot bot) {
