4bbb4609f47bad7f15143b5ac5480a466543d42c
Pawel Nowak <siersciotluk@gmail.com>
2015-08-05 10:38:35 +0200

    [news] added menu when feeds has not been polled
    Bug: 472905
    Change-Id: Idfde774908bd05d83251954db51f16b9779373b3
    Signed-off-by: Pawel Nowak <siersciotluk@gmail.com>

---
+++ plugins/org.eclipse.recommenders.news.rcp/src/org/eclipse/recommenders/internal/news/rcp/menus/PreferenceAction.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2015 Pawel Nowak.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.recommenders.internal.news.rcp.menus;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.recommenders.internal.news.rcp.l10n.Messages;
+import org.eclipse.ui.dialogs.PreferencesUtil;
+
+public class PreferenceAction extends Action {
+    private static PreferenceAction instance = null;
+
+    private PreferenceAction() {
+    }
+
+    public static PreferenceAction getInstance() {
+        if (instance == null) {
+            synchronized (PreferenceAction.class) {
+                if (instance == null) {
+                    instance = new PreferenceAction();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    @Override
+    public void run() {
+        PreferencesUtil.createPreferenceDialogOn(null, "org.eclipse.recommenders.news.rcp.preferencePage", null, null) //$NON-NLS-1$
+                .open();
+    }
+
+    @Override
+    public String getText() {
+        return Messages.LABEL_PREFERENCES;
+    }
+}
