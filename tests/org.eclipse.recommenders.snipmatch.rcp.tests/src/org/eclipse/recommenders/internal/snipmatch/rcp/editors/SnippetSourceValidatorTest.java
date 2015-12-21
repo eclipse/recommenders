af1da270359e209bdb41fded4e26c228e70e83be
Johannes Dorn <johannes.dorn@codetrails.com>
2015-12-21 23:18:11 +0100

    [snipmatch] Evaluate file snippets without Java template variables
    Bug: 484013
    Change-Id: I4b0734f95635b915105833207a5d744bcf6b35d2
    Signed-off-by: Johannes Dorn <johannes.dorn@codetrails.com>

--- tests/org.eclipse.recommenders.snipmatch.rcp.tests/src/org/eclipse/recommenders/internal/snipmatch/rcp/editors/SnippetSourceValidatorTest.java
+++ tests/org.eclipse.recommenders.snipmatch.rcp.tests/src/org/eclipse/recommenders/internal/snipmatch/rcp/editors/SnippetSourceValidatorTest.java
@@ -3,42 +3,51 @@ package org.eclipse.recommenders.internal.snipmatch.rcp.editors;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Test;
+import java.util.LinkedList;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.google.common.collect.Lists;
+
+@RunWith(Parameterized.class)
 public class SnippetSourceValidatorTest {
 
-    @Test
-    public void testEmptySnippetIsValid() {
-        assertThat(SnippetSourceValidator.isSourceValid("").isEmpty(), is(true));
+    private static final boolean VALID = true;
+    private static final boolean INVALID = false;
+
+    private final String templateText;
+    private final boolean isValid;
+
+    public SnippetSourceValidatorTest(String description, String templateText, boolean isValid) {
+        this.templateText = templateText;
+        this.isValid = isValid;
+    }
+
+    @Parameters(name = "{index}: {0}")
+    public static Iterable<Object[]> scenarios() {
+        LinkedList<Object[]> scenarios = Lists.newLinkedList();
+
+        scenarios.add(scenario("Empty snippet", "", VALID));
+        scenarios.add(scenario("Simple snippet", "Lorem ipsum dolor", VALID));
+        scenarios.add(scenario("Snippet with id", "${id}", VALID));
+        scenarios.add(scenario("Snippet with var reference", "${id:var(java.lang.String)}", VALID));
+        scenarios.add(scenario("Snippet with escaped dollar sign", "$$", VALID));
+        scenarios.add(scenario("Snippet with single dollar sign", "$", INVALID));
+        scenarios.add(scenario("Unclosed var reference", "${id:var(java.lang.String)", INVALID));
+
+        return scenarios;
+    }
+
+    private static Object[] scenario(String description, String templateText, boolean isValid) {
+        return new Object[] { description, templateText, isValid };
     }
 
     @Test
-    public void testSimpleSnippet() {
-        assertThat(SnippetSourceValidator.isSourceValid("Lorem ipsum dolor").isEmpty(), is(true));
-    }
-
-    @Test
-    public void testSnippetWithId() {
-        assertThat(SnippetSourceValidator.isSourceValid("${id}").isEmpty(), is(true));
-    }
-
-    @Test
-    public void testSnippetWithVarReference() {
-        assertThat(SnippetSourceValidator.isSourceValid("${id:var(java.lang.String)}").isEmpty(), is(true));
-    }
-
-    @Test
-    public void testSnippetWithEscapedDollarSign() {
-        assertThat(SnippetSourceValidator.isSourceValid("$$").isEmpty(), is(true));
-    }
-
-    @Test
-    public void testSnippetWithSingleDollarSign() {
-        assertThat(SnippetSourceValidator.isSourceValid("$").isEmpty(), is(false));
-    }
-
-    @Test
-    public void testSnippetWithUnclosedVarReference() {
-        assertThat(SnippetSourceValidator.isSourceValid("${id:var(java.lang.String)").isEmpty(), is(false));
+    public void test() {
+        assertThat(JavaSnippetSourceValidator.isSourceValid(templateText).isEmpty(), is(isValid));
+        assertThat(TextSnippetSourceValidator.isSourceValid(templateText).isEmpty(), is(isValid));
     }
 }
