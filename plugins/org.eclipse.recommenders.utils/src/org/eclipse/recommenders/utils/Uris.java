9679c3cddcc1970d4237226fe68d3b5fd941e001
Andreas Sewe <andreas.sewe@codetrails.com>
2016-06-29 17:36:51 +0200

    [models] Percent-encoding usernames or passwords not accepted
    Bug: 497021
    Change-Id: If01924f5ea5959531ef22061b3274656dcada017
    Signed-off-by: Andreas Sewe <andreas.sewe@codetrails.com>

---
+++ plugins/org.eclipse.recommenders.utils/src/org/eclipse/recommenders/utils/Uris.java
@@ -0,0 +1,75 @@
+package org.eclipse.recommenders.utils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Throwables;
+
+public final class Uris {
+
+    private Uris() {
+    }
+
+    public static String toStringWithMaskedPassword(URI uri, char mask) {
+        if (uri.isOpaque()) {
+            return uri.toString();
+        }
+
+        String userInfo = uri.getUserInfo();
+        if (userInfo != null) {
+            int indexOfColon = userInfo.indexOf(':');
+            if (indexOfColon > 0) {
+                userInfo = userInfo.substring(0, indexOfColon + 1)
+                        + StringUtils.repeat(mask, userInfo.length() - indexOfColon - 1);
+            }
+        }
+        return toStringWithUserInfo(uri, userInfo);
+    }
+
+    public static String toStringWithoutUserinfo(URI uri) {
+        if (uri.isOpaque()) {
+            return uri.toString();
+        }
+
+        return toStringWithUserInfo(uri, null);
+    }
+
+    private static String toStringWithUserInfo(URI uri, @Nullable String userInfo) {
+        try {
+            return new URI(uri.getScheme(), userInfo, uri.getHost(), uri.getPort(), uri.getPath(), uri.getQuery(),
+                    uri.getFragment()).toString();
+        } catch (URISyntaxException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    public static Optional<URI> parseURI(String uriString) {
+        try {
+            return Optional.of(new URI(uriString));
+        } catch (URISyntaxException e) {
+            return Optional.absent();
+        }
+    }
+
+    public static URI toUri(String uriString) {
+        try {
+            return new URI(uriString);
+        } catch (URISyntaxException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    public static boolean isUriProtocolSupported(URI uri, List<String> protocols) {
+        for (String protocol : protocols) {
+            if (StringUtils.equalsIgnoreCase(protocol, uri.getScheme())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
