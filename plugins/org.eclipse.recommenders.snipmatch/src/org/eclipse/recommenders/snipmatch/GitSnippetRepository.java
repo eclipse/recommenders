e23d09b71a6485c3bd2b724b85c565be265d5af0
Andreas Sewe <andreas.sewe@codetrails.com>
2016-07-14 11:59:53 +0200

    [models] Exception when using "Add Repository" dialog with malformed URI
    Bug: 497891
    Change-Id: I80a88e3129b2ab6fc943691826be9b363dc3f088
    Signed-off-by: Andreas Sewe <andreas.sewe@codetrails.com>

--- plugins/org.eclipse.recommenders.snipmatch/src/org/eclipse/recommenders/snipmatch/GitSnippetRepository.java
+++ plugins/org.eclipse.recommenders.snipmatch/src/org/eclipse/recommenders/snipmatch/GitSnippetRepository.java
@@ -15,6 +15,7 @@ import static org.eclipse.recommenders.snipmatch.Snippet.FORMAT_VERSION;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.MessageFormat;
 import java.util.Collection;
@@ -55,18 +56,18 @@ public class GitSnippetRepository extends FileSnippetRepository {
     private static final Logger LOG = LoggerFactory.getLogger(GitSnippetRepository.class);
 
     private final File basedir;
-    private final String fetchUrl;
-    private final String pushUrl;
+    private final URI fetchUri;
+    private final URI pushUri;
     private final String pushBranchPrefix;
     private final File gitFile;
 
     private Repository localRepo;
 
-    public GitSnippetRepository(String id, File basedir, String fetchUrl, String pushUrl, String pushBranchPrefix) {
+    public GitSnippetRepository(String id, File basedir, URI uri, URI pushUri, String pushBranchPrefix) {
         super(id, basedir);
         this.basedir = basedir;
-        this.fetchUrl = fetchUrl;
-        this.pushUrl = pushUrl;
+        this.fetchUri = uri;
+        this.pushUri = pushUri;
         this.pushBranchPrefix = pushBranchPrefix;
         this.gitFile = new File(basedir, ".git");
     }
@@ -104,7 +105,7 @@ public class GitSnippetRepository extends FileSnippetRepository {
             } catch (InvalidRemoteException e) {
                 LOG.error("Invalid remote repository.", e);
                 throw createException(updatePossible, MessageFormat
-                        .format("Invalid remote repository \"{0}\". Check the repository's URL.", fetchUrl), e);
+                        .format("Invalid remote repository \"{0}\". Check the repository's URL.", fetchUri), e);
             } catch (TransportException e) {
                 LOG.error("Transport operation failed.", e);
                 throw createException(updatePossible,
@@ -193,7 +194,7 @@ public class GitSnippetRepository extends FileSnippetRepository {
 
         Git git = new Git(localRepo);
         FetchCommand fetch = git.fetch();
-        fetch.setCredentialsProvider(getCredentialsProvider(fetchUrl));
+        fetch.setCredentialsProvider(getCredentialsProvider(fetchUri));
         fetch.call();
         return git;
     }
@@ -265,7 +266,7 @@ public class GitSnippetRepository extends FileSnippetRepository {
         checkout.call();
 
         PullCommand pull = git.pull();
-        pull.setCredentialsProvider(getCredentialsProvider(fetchUrl));
+        pull.setCredentialsProvider(getCredentialsProvider(fetchUri));
         pull.call();
     }
 
@@ -284,7 +285,7 @@ public class GitSnippetRepository extends FileSnippetRepository {
 
     @Override
     public String getRepositoryLocation() {
-        return fetchUrl;
+        return fetchUri;
     }
 
     @Override
@@ -314,6 +315,6 @@ public class GitSnippetRepository extends FileSnippetRepository {
     }
 
     public String getPushUrl() {
-        return pushUrl;
+        return pushUri;
     }
 }
