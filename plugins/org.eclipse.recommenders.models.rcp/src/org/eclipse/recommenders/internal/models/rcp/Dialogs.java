93e742b9aaba7edb124923666ae035e0fd92d831
Johannes Dorn <johannes.dorn@codetrails.com>
2016-11-02 11:59:05 +0100

    [models][preferences] Use secure storage to store model repository usernames and passwords
    Bug: 506887
    Change-Id: I16a3da3d842d64c6f5874115c96a783491ab724e
    Signed-off-by: Johannes Dorn <johannes.dorn@codetrails.com>

--- plugins/org.eclipse.recommenders.models.rcp/src/org/eclipse/recommenders/internal/models/rcp/Dialogs.java
+++ plugins/org.eclipse.recommenders.models.rcp/src/org/eclipse/recommenders/internal/models/rcp/Dialogs.java
@@ -15,11 +15,22 @@ import java.text.MessageFormat;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.recommenders.internal.models.rcp.l10n.Messages;
 import org.eclipse.recommenders.utils.Uris;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 import com.google.common.collect.ImmutableList;
 
@@ -29,6 +40,7 @@ public final class Dialogs {
 
     private Dialogs() {
     }
+
 
     public static InputDialog newModelRepositoryUrlDialog(Shell parent, final String[] remoteUris) {
         return new InputDialog(parent, Messages.DIALOG_TITLE_ADD_MODEL_REPOSITORY, Messages.FIELD_LABEL_REPOSITORY_URI,
