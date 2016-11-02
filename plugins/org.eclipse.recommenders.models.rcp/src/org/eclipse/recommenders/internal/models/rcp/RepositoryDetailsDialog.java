071eeaa82d02639d814be684deb46794f78d6397
Johannes Dorn <johannes.dorn@codetrails.com>
2016-11-02 11:59:05 +0100

    [models][preferences] Use secure storage to store model repository usernames and passwords
    Bug: 506887
    Change-Id: I16a3da3d842d64c6f5874115c96a783491ab724e
    Signed-off-by: Johannes Dorn <johannes.dorn@codetrails.com>

---
+++ plugins/org.eclipse.recommenders.models.rcp/src/org/eclipse/recommenders/internal/models/rcp/RepositoryDetailsDialog.java
@@ -0,0 +1,200 @@
+package org.eclipse.recommenders.internal.models.rcp;
+
+import java.net.URI;
+import java.text.MessageFormat;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.recommenders.internal.models.rcp.l10n.Messages;
+import org.eclipse.recommenders.utils.Uris;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import com.google.common.collect.ImmutableList;
+
+public class RepositoryDetailsDialog extends TitleAreaDialog {
+
+    private static final List<String> SUPPORTED_PROTOCOLS = ImmutableList.of("file", "http", "https"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+
+    private Text repositoryUrlText;
+    private Text repositoryUsernameText;
+    private Text repositoryPasswordText;
+
+    private String repositoryUrl;
+    private final List<String> remoteUris;
+
+    private final ModelsRcpPreferences preferences;
+
+    public RepositoryDetailsDialog(Shell parentShell, String repositoryUrl, List<String> remoteUris,
+            ModelsRcpPreferences preferences) {
+        super(parentShell);
+        this.repositoryUrl = repositoryUrl;
+        this.remoteUris = remoteUris;
+        this.preferences = preferences;
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        Composite fieldArea = new Composite((Composite) super.createDialogArea(parent), SWT.NONE);
+
+        final boolean noRepository = repositoryUrl == null;
+        String title = noRepository ? "Add Repository" : "Edit Repository";
+
+        getShell().setText(title);
+        setTitle(title);
+
+        setMessage(noRepository ? "Enter repository details." : "Edit repository details.");
+
+        GridLayoutFactory.swtDefaults().numColumns(2).applyTo(fieldArea);
+        GridDataFactory.fillDefaults().grab(true, true).applyTo(fieldArea);
+
+        repositoryUrlText = createLabelledText(fieldArea, "Repository URL:",
+                noRepository ? "http://download.eclipse.org/recommenders/models/<version>" : repositoryUrl, SWT.NONE);
+
+        repositoryUsernameText = createLabelledText(fieldArea, "Username:",
+                preferences.getServerUsername(repositoryUrl).or(""), SWT.NONE); //$NON-NLS-1$
+        repositoryUsernameText.setMessage("Optional");
+
+        repositoryPasswordText = createLabelledText(fieldArea, "Password:", //$NON-NLS-1$
+                preferences.getServerPassword(repositoryUrl).or(""), SWT.PASSWORD);
+
+        repositoryPasswordText.setMessage("Optional");
+
+        repositoryUrlText
+                .addModifyListener(new ServerRepositoryUrlListener(repositoryUsernameText, repositoryPasswordText));
+
+        Dialog.applyDialogFont(fieldArea);
+
+        return fieldArea;
+    }
+
+    private Text createLabelledText(Composite parent, String labelText, String initialText, int additionalStyle) {
+        Label label = new Label(parent, SWT.NONE);
+
+        label.setText(labelText);
+        GridDataFactory.swtDefaults().applyTo(label);
+
+        Text text = new Text(parent, SWT.BORDER | additionalStyle);
+        text.setText(initialText);
+        text.addModifyListener(new ModifyListener() {
+
+            @Override
+            public void modifyText(ModifyEvent e) {
+                updatePageComplete();
+            }
+        });
+        GridDataFactory.fillDefaults().grab(true, false).applyTo(text);
+
+        return text;
+    }
+
+    private void updatePageComplete() {
+        setErrorMessage(null);
+        boolean hasError = false;
+
+        try {
+            URI uri = Uris.parseURI(repositoryUrlText.getText()).orNull();
+            if (uri == null) {
+                hasError = true;
+                setErrorMessage(Messages.DIALOG_MESSAGE_INVALID_URI);
+                return;
+            }
+            if (!uri.isAbsolute()) {
+                hasError = true;
+                setErrorMessage(Messages.DIALOG_MESSAGE_NOT_ABSOLUTE_URI);
+                return;
+            }
+            if (isUriAlreadyAdded(uri)) {
+                hasError = true;
+                setErrorMessage(Messages.DIALOG_MESSAGE_URI_ALREADY_ADDED);
+                return;
+            }
+
+            if (!Uris.isUriProtocolSupported(uri, SUPPORTED_PROTOCOLS)) {
+                hasError = true;
+                setErrorMessage(MessageFormat.format(Messages.DIALOG_MESSAGE_UNSUPPORTED_PROTOCOL, uri.getScheme(),
+                        StringUtils.join(SUPPORTED_PROTOCOLS, Messages.LIST_SEPARATOR)));
+                return;
+            }
+
+            if (!isUsernameValid()) {
+                hasError = true;
+                setErrorMessage("You must specify a username if a password is set.");
+                return;
+            }
+        } finally {
+            getButton(IDialogConstants.OK_ID).setEnabled(!hasError);
+        }
+    }
+
+    private boolean isUriAlreadyAdded(URI uri) {
+        if (repositoryUrl != null) {
+            URI repositoryUri = Uris.parseURI(repositoryUrl).orNull();
+            if (uri.equals(repositoryUri)) {
+                return false;
+            }
+        }
+
+        String mangledUri = Uris.mangle(uri);
+        for (String remoteUri : remoteUris) {
+            if (Uris.mangle(Uris.toUri(remoteUri)).equals(mangledUri)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isUsernameValid() {
+        return !repositoryUsernameText.getText().isEmpty() || repositoryPasswordText.getText().isEmpty();
+    }
+
+    @Override
+    protected void okPressed() {
+        repositoryUrl = repositoryUrlText.getText();
+
+        preferences.setServerUsername(repositoryUrl, repositoryUsernameText.getText());
+        preferences.setServerPassword(repositoryUrl, repositoryPasswordText.getText());
+
+        super.okPressed();
+    }
+
+    public String getRepositoryUrl() {
+        return repositoryUrl;
+    }
+
+    private final class ServerRepositoryUrlListener implements ModifyListener {
+
+        private final Text usernameField;
+        private final Text passwordField;
+
+        public ServerRepositoryUrlListener(Text usernameField, Text passwordField) {
+            this.usernameField = usernameField;
+            this.passwordField = passwordField;
+        }
+
+        @Override
+        public void modifyText(ModifyEvent e) {
+            Text urlField = (Text) e.widget;
+            String serverUri = urlField.getText();
+            String username = preferences.getServerUsername(serverUri).orNull();
+            if (username == null) {
+                return;
+            }
+            usernameField.setText(username);
+
+            String password = preferences.getServerPassword(serverUri).or(""); // Can't set null password
+            passwordField.setText(password);
+        }
+    }
+}
