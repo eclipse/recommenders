7c162805799a5e61ea7468a4044cea1e35559418
Andreas Sewe <andreas.sewe@codetrails.com>
2017-08-30 13:19:27 +0200

    [news] Limit the number of items shown
    Bug: 521591
    Change-Id: I24b920b12dc3bdfa9832202c5c2813d03bd9d589
    Signed-off-by: Andreas Sewe <andreas.sewe@codetrails.com>

---
+++ plugins/org.eclipse.recommenders.news.rcp/src/org/eclipse/recommenders/internal/news/rcp/PollingResults.java
@@ -0,0 +1,41 @@
+package org.eclipse.recommenders.internal.news.rcp;
+
+import static java.lang.Math.min;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.recommenders.news.api.NewsItem;
+import org.eclipse.recommenders.news.api.poll.PollingResult;
+
+public final class PollingResults {
+
+    private PollingResults() {
+        throw new AssertionError();
+    }
+
+    public static List<NewsItem> getAllNewsItems(PollingResult result, int limit) {
+        List<NewsItem> allNewsItems = result.getAllNewsItems();
+        List<NewsItem> limitedNewsItems = new ArrayList<>(min(allNewsItems.size(), limit));
+        for (NewsItem item : allNewsItems) {
+            if (limitedNewsItems.size() >= limit) {
+                break;
+            } else {
+                limitedNewsItems.add(item);
+            }
+        }
+        return limitedNewsItems;
+    }
+
+    public static List<NewsItem> getNewNewsItems(PollingResult result, int limit) {
+        List<NewsItem> allNewsItems = getAllNewsItems(result, limit);
+        List<NewsItem> newNewsItems = result.getNewNewsItems();
+        List<NewsItem> limitedNewsItems = new ArrayList<>(min(newNewsItems.size(), limit));
+        for (NewsItem item : allNewsItems) {
+            if (newNewsItems.contains(item)) {
+                limitedNewsItems.add(item);
+            }
+        }
+        return limitedNewsItems;
+    }
+}
