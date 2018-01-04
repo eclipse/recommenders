785477b48fced79bdd2865d37488931f675c252f
Andreas Sewe <andreas.sewe@codetrails.com>
2018-01-04 13:36:17 +0100

    [completion] UnsupportedOperationException w/o "Fill Method Arguments"
    Bug: 528937
    Change-Id: I26cd2b73f541a30db10fd2df1028f704034a3c25
    Signed-off-by: Andreas Sewe <andreas.sewe@codetrails.com>

--- plugins/org.eclipse.recommenders.completion.rcp/src/org/eclipse/recommenders/completion/rcp/processable/ProcessableProposalFactory.java
+++ plugins/org.eclipse.recommenders.completion.rcp/src/org/eclipse/recommenders/completion/rcp/processable/ProcessableProposalFactory.java
@@ -16,12 +16,8 @@ import static org.eclipse.recommenders.utils.Logs.log;
 
 import java.lang.reflect.Method;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.CompletionProposal;
 import org.eclipse.jdt.core.IField;
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IMember;
-import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.internal.ui.text.java.AbstractJavaCompletionProposal;
 import org.eclipse.jdt.internal.ui.text.java.AnonymousTypeCompletionProposal;
@@ -47,7 +43,6 @@ import org.eclipse.recommenders.internal.completion.rcp.l10n.LogMessages;
 import org.eclipse.recommenders.utils.Checks;
 import org.eclipse.recommenders.utils.Logs;
 import org.eclipse.recommenders.utils.Reflections;
-import org.eclipse.recommenders.utils.Throws;
 
 import com.google.common.base.Throwables;
 
@@ -299,7 +294,8 @@ public class ProcessableProposalFactory implements IProcessableProposalFactory {
      * {@link AbstractJavaCompletionProposal#getProposalInfo} as that method is {@code protected} and hence not visible
      * to a wrapper.
      */
-    private static void copyProposalInfo(IJavaCompletionProposal originalProposal, IProcessableProposal processableProposal) {
+    private static void copyProposalInfo(IJavaCompletionProposal originalProposal,
+            IProcessableProposal processableProposal) {
         // XXX this method should under no circumstances throw any exception
         if (Checks.anyIsNull(proposalInfoMethod, processableProposal, originalProposal)) {
             return;
@@ -405,22 +401,7 @@ public class ProcessableProposalFactory implements IProcessableProposalFactory {
     @Override
     public IProcessableProposal newMethodDeclarationCompletionProposal(CompletionProposal coreProposal,
             MethodDeclarationCompletionProposal uiProposal, JavaContentAssistInvocationContext context) {
-        try {
-            IJavaElement enclosingElement = context.getCoreContext().getEnclosingElement();
-            IType type = null;
-            if (enclosingElement instanceof IType) {
-                type = (IType) enclosingElement;
-            } else if (enclosingElement instanceof IMember) {
-                type = ((IMember) enclosingElement).getDeclaringType();
-            }
-            if (type == null) {
-                throw Throws.throwIllegalArgumentException("No type found for enclosing element %s", enclosingElement); //$NON-NLS-1$
-            }
-            return postConstruct(ProcessableMethodDeclarationCompletionProposal.newProposal(coreProposal, type,
-                    uiProposal.getRelevance()), uiProposal);
-        } catch (CoreException e) {
-            throw Throwables.propagate(e);
-        }
+        return postConstruct(ProcessableMethodDeclarationCompletionProposal.toProcessableProposal(uiProposal, coreProposal), uiProposal);
     }
 
     @Override
